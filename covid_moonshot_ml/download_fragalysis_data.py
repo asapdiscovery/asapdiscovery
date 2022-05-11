ef054a8734e51930c9f2a25d635f79e343a7c054
kaminow <benjamin.kaminow@choderalab.org>
2022-05-11 12:19:13 -0400

    Add functionality for extracting the zip file once downloaded.

--- covid_moonshot_ml/download_fragalysis_data.py
+++ covid_moonshot_ml/download_fragalysis_data.py
@@ -2,7 +2,10 @@ import argparse
 import os
 import requests
 import time
+from  zipfile import ZipFile
 
+BASE_URL = 'https://fragalysis.diamond.ac.uk/api/download_structures/'
+## Info for the POST call
 MPRO_API_CALL = {
     'target_name': 'Mpro',
     'proteins': '',
@@ -25,29 +28,35 @@ MPRO_API_CALL = {
     'file_url': ''
 }
 
-def download(fn_out):
-    url = 'https://fragalysis.diamond.ac.uk/api/download_structures/'
+def download(out_fn, extract=True):
     ## First send POST request to prepare the download file and get its URL
-    r = requests.post(url, data=MPRO_API_CALL)
+    r = requests.post(BASE_URL, data=MPRO_API_CALL)
     url_dl = r.text.split(':"')[1].strip('"}')
     ## Send GET request for the zip archive
-    r_dl = requests.get(url, params={'file_url': url_dl})
+    r_dl = requests.get(BASE_URL, params={'file_url': url_dl})
     ## Full archive stored in r_dl.content, so write to zip file
-    with open(fn_out, 'wb') as fp:
+    with open(out_fn, 'wb') as fp:
         fp.write(r_dl.content)
+
+    ## Extract files if requested
+    if extract:
+        zf = ZipFile(out_fn)
+        zf.extractall(path=os.path.dirname(out_fn))
 
 ################################################################################
 def get_args():
     parser = argparse.ArgumentParser(description='')
 
-    parser.add_argument('-o', required=True, help='Output zip file.')
+    parser.add_argument('-o', required=True, help='Output file name.')
+    parser.add_argument('-x', action='store_true',
+        help='Extract file after downloading it.')
 
     return(parser.parse_args())
 
 def main():
     args = get_args()
 
-    download(args.o)
+    download(args.o, args.x)
 
 if __name__ == '__main__':
     main()
