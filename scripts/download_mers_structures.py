ca20a65e7d73f9e89e2966245e7614cb54f810a5
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-07-22 11:04:39 -0400

    move mers-structures.yaml to scripts directory

--- scripts/download_mers_structures.py
+++ scripts/download_mers_structures.py
@@ -2,14 +2,15 @@ import argparse
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(repo_path)
 from covid_moonshot_ml.datasets import pdb
 
 ################################################################################
 def get_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-d', help='Directory name to put the structures')
-    parser.add_argument('-y', help='MERS structures yaml file')
+    parser.add_argument('-y', default="mers-structures.yaml", help='MERS structures yaml file')
     parser.add_argument('-r', default=None, help='Path to pdb reference file to align to')
     parser.add_argument('-n', default=None, help='Name of reference')
     return(parser.parse_args())
