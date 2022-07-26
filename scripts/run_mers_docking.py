cdab3fefcf12d690164820553fe69f70d7994132
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-07-26 14:20:49 -0400

    add script identifying which exp_ligands have SARS2 structures

--- scripts/run_mers_docking.py
+++ scripts/run_mers_docking.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import pandas as pd
 
 repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(repo_path)
@@ -10,26 +11,39 @@ from covid_moonshot_ml.docking.docking import build_docking_systems,\
 
 
 ################################################################################
-def get_args():
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('-exp', required=True,
-                        help='CSV file with experimental data.')
-    parser.add_argument('-x', required=True,
-                        help='CSV file with crystal compound information.')
-    parser.add_argument('-x_dir', required=True,
-                        help='Directory with crystal structures.')
-    parser.add_argument('-d', help='Directory name to put the structures')
-    parser.add_argument('-y', default="mers-structures.yaml",
-                        help='MERS structures yaml file')
-    parser.add_argument('-r', default=None,
-                        help='Path to pdb reference file to align to')
-    parser.add_argument('-n', default=None, help='Name of reference')
-    return (parser.parse_args())
+# def get_args():
+#     parser = argparse.ArgumentParser(description='')
+#     parser.add_argument('-exp', required=True,
+#                         help='CSV file with experimental data.')
+#     parser.add_argument('-x', required=True,
+#                         help='CSV file with crystal compound information.')
+#     parser.add_argument('-x_dir', required=True,
+#                         help='Directory with crystal structures.')
+#     parser.add_argument('-d', help='Directory name to put the structures')
+#     parser.add_argument('-y', default="mers-structures.yaml",
+#                         help='MERS structures yaml file')
+#     parser.add_argument('-r', default=None,
+#                         help='Path to pdb reference file to align to')
+#     parser.add_argument('-n', default=None, help='Name of reference')
+#     return (parser.parse_args())
 
 
 def main():
-    args = get_args()
+    # args = get_args()
+    exp_data_fn = "COVID_Moonshot_Takeda_panCorona_enzyme_measurements.csv"
+    sars2_cmpds_fn = "/Users/alexpayne/lilac-mount-point/fragalysis/extra_files/Mpro_compound_tracker_csv.csv"
 
+    exp_data = pd.read_csv(exp_data_fn).fillna("")
+    sars2_structures = pd.read_csv(sars2_cmpds_fn)
+    sars2_filtered = sars2_structures[sars2_structures['Compound ID'].isin(exp_data['External ID'])]
+
+    mols_wo_sars2_xtal = sars2_filtered[sars2_filtered["Dataset"].isna()][["Compound ID", "SMILES"]]
+    mols_w_sars2_xtal = sars2_filtered[~sars2_filtered["Dataset"].isna()][["Compound ID", "SMILES", "Dataset"]]
+
+    mols_w_sars2_xtal.to_csv("mers_ligands_with_SARS2_structures.csv",
+                             index=False)
+    mols_wo_sars2_xtal.to_csv("mers_ligands_without_SARS2_structures.csv",
+                              index=False)
 
 if __name__ == '__main__':
     main()
