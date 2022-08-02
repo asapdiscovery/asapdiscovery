2764794eb197bf08503755b862a1bdb2a06f008a
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-08-02 17:37:16 -0400

    add DockingDataset class and start writing code to calc RMSD

--- covid_moonshot_ml/datasets/utils.py
+++ covid_moonshot_ml/datasets/utils.py
@@ -751,3 +751,19 @@ def parse_fragalysis_data(frag_fn,
         print(xtal.compound_id, xtal.dataset)
 
     return sars_xtals
+
+def load_openeye_pdb(pdb_fn):
+    ifs = oechem.oemolistream()
+    ifs.SetFlavor(
+        oechem.OEFormat_PDB,
+        oechem.OEIFlavor_PDB_Default | oechem.OEIFlavor_PDB_DATA,
+    )
+    ifs.open(pdb_fn)
+    in_mol = oechem.OEGraphMol()
+    oechem.OEReadMolecule(ifs, in_mol)
+    ifs.close()
+
+    return in_mol
+
+def get_ligand_rmsd(mobile, ref):
+    return oechem.OERMSD(mobile, ref)
