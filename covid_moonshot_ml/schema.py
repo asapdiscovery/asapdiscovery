77be0dc6c1b213f834a79568e4dc59c16cddecea
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-08-04 14:52:17 -0400

    make DockingDataset no longer a pydantic object and basic reading and csv saving code

--- covid_moonshot_ml/schema.py
+++ covid_moonshot_ml/schema.py
@@ -122,13 +122,13 @@ class DockingDataset():
         pass
 
     def organize_docking_results(self):
-
         ## Make lists we want to use to collect information about docking results
         cmp_ids = []
         xtal_ids = []
         chain_ids = []
         mcss_ranks = []
         sdf_fns = []
+        cmplx_ids = []
 
         ## dictionary of reference sdf files for each compound id
         ref_fn_dict = {}
@@ -159,7 +159,7 @@ class DockingDataset():
                 ## the ligand re-docked to their original xtal have no mcss rank, so this will fail
                 try:
                     mcss_rank = info[9]
-                    # print(xtal, chain, cmp_id, mcss_rank)
+
                 except:
 
                     ## however its a convenient way of identifying which is the original xtal
@@ -176,20 +176,28 @@ class DockingDataset():
                 chain_ids.append(chain)
                 mcss_ranks.append((mcss_rank))
                 sdf_fns.append(fn)
+                cmplx_ids.append(f"{cmp_id}_{xtal}_{chain}_{mcss_rank}")
+
+        ref_sdf_fns = [ref_fn_dict[cmp_id] for cmp_id in cmp_ids]
+
         self.df = pd.DataFrame(
-            {"Compound_ID": cmp_ids,
-             "Crystal ID": xtal_ids,
-             "Chain ID": chain_ids,
-             "MCSS Rank": mcss_ranks,
-             "SDF Filename": sdf_fns,
+            {"Complex_ID": cmplx_ids,
+                "Compound_ID": cmp_ids,
+             "Crystal_ID": xtal_ids,
+             "Chain_ID": chain_ids,
+             "MCSS_Rank": mcss_ranks,
+             "SDF_Filename": sdf_fns,
+             "Reference_SDF": ref_sdf_fns
              }
         )
 
-    def write_csv(self):
-        self.df.to_csv("docking_results.csv")
+    def write_csv(self, output_csv_fn):
+        self.df.to_csv(output_csv_fn, index=False)
 
-    def analyze_docking_results(self, fragalysis_directory):
+    def analyze_docking_results(self, fragalysis_directory, output_csv_fn):
         self.organize_docking_results()
-        self.write_csv()
+        # self.to_df()
+        self.write_csv(output_csv_fn=output_csv_fn)
+
 
 
