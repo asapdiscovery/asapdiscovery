de83e7895e77aab60b1675ec91c7f186fa3309e2
kaminow <benjamin.kaminow@choderalab.org>
2022-08-05 17:03:48 -0400

    Updated script to support multiprocessing.

--- scripts/hallucinate_fragalysis.py
+++ scripts/hallucinate_fragalysis.py
@@ -27,6 +27,44 @@ from covid_moonshot_ml.docking.docking import (
 from covid_moonshot_ml.modeling import du_to_complex, make_du_from_new_lig
 
 
+def mp_func(apo_prot, lig, ref_prot, out_dir, holo_name, save_du=False):
+    out_base = f"{out_dir}/{holo_name}/"
+    os.makedirs(out_base, exist_ok=True)
+    out_fn = f"{out_base}/predocked"
+    ## Make design unit and prep the receptor
+    du = make_du_from_new_lig(apo_prot, lig, ref_prot, False, False)
+    oedocking.OEMakeReceptor(du)
+
+    ## Save if desired
+    if save_du:
+        oechem.OEWriteDesignUnit(f"{out_fn}.oedu", du)
+
+    ## Get protein+lig complex in molecule form and save
+    complex_mol = du_to_complex(du)
+    save_openeye_pdb(complex_mol, f"{out_fn}.pdb")
+
+    ## Run docking with kinoml
+    dock_lig = oechem.OEGraphMol()
+    du.GetLigand(dock_lig)
+    docked_mol = pose_molecules(du, [dock_lig], score_pose=True)[0]
+
+    save_openeye_sdf(docked_mol, f"{out_base}/docked.sdf")
+
+    ## Calculate RMSD
+    orig_mol = oechem.OEGraphMol()
+    du.GetLigand(orig_mol)
+    rmsd = oechem.OERMSD(orig_mol, docked_mol)
+
+    return (
+        name,
+        holo_name,
+        f"{out_base}/docked.sdf",
+        rmsd,
+        float(oechem.OEGetSDData(docked_mol, "POSIT::Probability")),
+        float(oechem.OEGetSDData(docked_mol, "Chemgauss4")),
+    )
+
+
 ################################################################################
 def get_args():
     parser = argparse.ArgumentParser(description="")
@@ -53,7 +91,7 @@ def get_args():
 
     ## Performance arguments
     parser.add_argument(
-        "-n", default=1, type=int, help="Number of processors to use."
+        "-n", default=10, type=int, help="Number of processors to use."
     )
 
     ## Output arguments
@@ -92,6 +130,8 @@ def main():
         os.path.splitext(os.path.basename(fn))[0] for fn in all_holo_fns
     ]
 
+    print(args.apo, args.holo)
+
     print(f"{len(all_apo_fns)} apo structures")
     print(f"{len(all_holo_fns)} ligands to dock", flush=True)
 
@@ -113,37 +153,18 @@ def main():
     else:
         cache_dir = args.cache
 
-    results_cols = [
-        "MERS_structure",
-        "SARS_structure",
-        "docked_file",
-        "docked_RMSD",
-        "POSIT_prob",
-        "chemgauss4_score",
-    ]
-    results_df = []
-    ## Make all design unit objects (optionally saving them), and save complex
-    ##  PDB file
+
+    mp_args = []
+    ## Construct all args for mp_func
     for name, fn in zip(all_apo_names, all_apo_fns):
         out_dir = f"{args.o}/{name}/"
         os.makedirs(out_dir, exist_ok=True)
         ## Load and parse apo protein
         apo_prot = split_openeye_mol(load_openeye_pdb(fn))["pro"]
         for holo_name, lig in zip(all_holo_names, all_ligs):
-            out_base = f"{out_dir}/{holo_name}/"
-            os.makedirs(out_base, exist_ok=True)
-            out_fn = f"{out_base}/predocked"
-            ## Make design unit and prep the receptor
-            du = make_du_from_new_lig(apo_prot, lig, ref_prot, False, False)
-            oedocking.OEMakeReceptor(du)
-
-            ## Save if desired
-            if args.du:
-                oechem.OEWriteDesignUnit(f"{out_fn}.oedu", du)
-
-            ## Get protein+lig complex in molecule form and save
-            complex_mol = du_to_complex(du)
-            save_openeye_pdb(complex_mol, f"{out_fn}.pdb")
+            mp_args.append(
+                (apo_prot, lig, ref_prot, out_dir, holo_name, args.du)
+            )
 
             # reloaded_mol = load_openeye_pdb(f"{out_fn}.pdb")
 
@@ -192,12 +213,6 @@ def main():
             #     cache_dir, out_base, args.loop, 1, [docking_system]
             # )
 
-            ## Run docking with kinoml
-            dock_lig = oechem.OEGraphMol()
-            du.GetLigand(dock_lig)
-            docked_mol = pose_molecules(du, [dock_lig], score_pose=True)[0]
-
-            save_openeye_sdf(docked_mol, f"{out_base}/docked.sdf")
             # print(
             #     "POSIT probability:",
             #     oechem.OEGetSDData(docked_mol, "POSIT::Probability"),
@@ -208,22 +223,6 @@ def main():
             #     oechem.OEGetSDData(docked_mol, "Chemgauss4"),
             #     flush=True
             # )
-
-            ## Calculate RMSD?
-            orig_mol = oechem.OEGraphMol()
-            du.GetLigand(orig_mol)
-            rmsd = oechem.OERMSD(orig_mol, docked_mol)
-
-            results_df.append(
-                (
-                    name,
-                    holo_name,
-                    f"{out_base}/docked.sdf",
-                    rmsd,
-                    float(oechem.OEGetSDData(docked_mol, "POSIT::Probability")),
-                    float(oechem.OEGetSDData(docked_mol, "Chemgauss4")),
-                )
-            )
 
             # print(
             #     "Docking score:",
@@ -238,8 +237,21 @@ def main():
             #     flush=True,
             # )
 
+    results_cols = [
+        "MERS_structure",
+        "SARS_structure",
+        "docked_file",
+        "docked_RMSD",
+        "POSIT_prob",
+        "chemgauss4_score",
+    ]
+    nprocs = min(mp.cpu_count(), len(mp_args), args.n)
+    print(f"Running {len(mp_args)} docking runs over {nprocs} cores.")
+    with mp.Pool(processes=nprocs) as pool:
+        results_df = pool.starmap(mp_func, mp_args)
     results_df = pandas.DataFrame(results_df, columns=results_cols)
-    print(results_df)
+
+    results_df.to_csv(f"{args.o}/all_results.csv")
 
 
 if __name__ == "__main__":
