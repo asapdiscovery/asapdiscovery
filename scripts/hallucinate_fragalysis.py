45b69fcbfe88121d59f1ece70f5fd2d732067108
kaminow <benjamin.kaminow@choderalab.org>
2022-08-17 11:13:10 -0400

    Flip apo/holo in output directory, and replace structure name with lig name.

--- scripts/hallucinate_fragalysis.py
+++ scripts/hallucinate_fragalysis.py
@@ -15,8 +15,10 @@ from kinoml.docking.OEDocking import pose_molecules
 
 sys.path.append(f"{os.path.dirname(os.path.abspath(__file__))}/../")
 from covid_moonshot_ml.datasets.utils import (
+    get_compound_id_xtal_dicts,
     load_openeye_pdb,
     load_openeye_sdf,
+    parse_fragalysis_data,
     save_openeye_pdb,
     save_openeye_sdf,
     split_openeye_mol,
@@ -29,11 +31,6 @@ from covid_moonshot_ml.modeling import du_to_complex, make_du_from_new_lig
 
 
 def add_compound_id(in_fn, frag_xtal_fn, frag_xtal_dir, out_fn=None):
-    from covid_moonshot_ml.datasets.utils import (
-        parse_fragalysis_data,
-        get_compound_id_xtal_dicts,
-    )
-
     compound_id_dict = get_compound_id_xtal_dicts(
         parse_fragalysis_data(frag_xtal_fn, frag_xtal_dir).values()
     )[1]
@@ -90,13 +87,13 @@ def check_output(d):
 
 
 def mp_func(
-    apo_prot, lig, ref_prot, out_dir, apo_name, holo_name, save_du=False
+    apo_prot, lig, ref_prot, out_dir, apo_name, lig_name, save_du=False
 ):
 
-    out_base = f"{out_dir}/{holo_name}/"
+    out_base = f"{out_dir}/{apo_name}/"
     ## First check if this combo has already been run
     if check_output(out_base):
-        print(f"Results found for {apo_name}_{holo_name}", flush=True)
+        print(f"Results found for {lig_name}_{apo_name}", flush=True)
         return pkl.load(open(f"{out_base}/results.pkl", "rb"))
 
     ## Make output directory if necessary
@@ -107,8 +104,8 @@ def mp_func(
     try:
         du = make_du_from_new_lig(apo_prot, lig, ref_prot, False, False)
     except AssertionError:
-        print(f"Design unit generation failed for {apo_name}/{holo_name}")
-        results = (apo_name, holo_name, None, -1.0, -1.0, -1.0)
+        print(f"Design unit generation failed for {lig_name}/{apo_name}")
+        results = (lig_name, apo_name, None, -1.0, -1.0, -1.0)
         pkl.dump(results, open(f"{out_base}/results.pkl", "wb"))
         return results
     oedocking.OEMakeReceptor(du)
@@ -127,8 +124,8 @@ def mp_func(
     try:
         docked_mol = pose_molecules(du, [dock_lig], score_pose=True)[0]
     except TypeError:
-        print(f"Pose generation failed for {apo_name}/{holo_name}")
-        results = (apo_name, holo_name, None, -1.0, -1.0, -1.0)
+        print(f"Pose generation failed for {lig_name}/{apo_name}")
+        results = (lig_name, apo_name, None, -1.0, -1.0, -1.0)
         pkl.dump(results, open(f"{out_base}/results.pkl", "wb"))
         return results
 
@@ -140,8 +137,8 @@ def mp_func(
     rmsd = oechem.OERMSD(orig_mol, docked_mol)
 
     results = (
+        lig_name,
         apo_name,
-        holo_name,
         f"{out_base}/docked.sdf",
         rmsd,
         float(oechem.OEGetSDData(docked_mol, "POSIT::Probability")),
@@ -174,6 +171,9 @@ def get_args():
         ),
     )
     parser.add_argument("-loop", required=True, help="Spruce loop_db file.")
+    parser.add_argument(
+        "-x", help="Fragalysis crystal structure compound tracker CSV file."
+    )
 
     ## Performance arguments
     parser.add_argument(
@@ -216,7 +216,17 @@ def main():
         os.path.splitext(os.path.basename(fn))[0] for fn in all_holo_fns
     ]
 
-    # print(args.apo, args.holo)
+    if args.x is not None:
+        ## Need to go up one level of directory
+        frag_dir = os.path.dirname(os.path.dirname(args.holo))
+        compound_id_dict = get_compound_id_xtal_dicts(
+            parse_fragalysis_data(args.x, frag_dir).values()
+        )[1]
+
+        ## Map all holo structure names to their ligand name
+        all_holo_names = [
+            compound_id_dict[n.split("_")[0]] for n in all_holo_names
+        ]
 
     print(f"{len(all_apo_fns)} apo structures")
     print(f"{len(all_holo_fns)} ligands to dock", flush=True)
@@ -224,6 +234,11 @@ def main():
     ## Get ligands from all holo structures
     all_ligs = [
         split_openeye_mol(load_openeye_pdb(fn))["lig"] for fn in all_holo_fns
+    ]
+
+    ## Get proteins from apo structures
+    all_prots = [
+        split_openeye_mol(load_openeye_pdb(fn))["pro"] for fn in all_apo_fns
     ]
 
     ## Parse reference
@@ -235,96 +250,24 @@ def main():
     ## Figure out cache dir for docking
     if args.cache is None:
         cache_dir = f"{args.o}/.cache/"
-        os.makedirs(cache_dir, exist_ok=True)
     else:
         cache_dir = args.cache
+    os.makedirs(cache_dir, exist_ok=True)
 
     mp_args = []
     ## Construct all args for mp_func
-    for name, fn in zip(all_apo_names, all_apo_fns):
-        out_dir = f"{args.o}/{name}/"
+    for lig_name, lig in zip(all_holo_names, all_ligs):
+        out_dir = f"{args.o}/{lig_name}/"
         os.makedirs(out_dir, exist_ok=True)
         ## Load and parse apo protein
-        apo_prot = split_openeye_mol(load_openeye_pdb(fn))["pro"]
-        for holo_name, lig in zip(all_holo_names, all_ligs):
+        for prot_name, apo_prot in zip(all_apo_names, all_prots):
             mp_args.append(
-                (apo_prot, lig, ref_prot, out_dir, name, holo_name, args.du)
+                (apo_prot, lig, ref_prot, out_dir, prot_name, lig_name, args.du)
             )
 
-            # reloaded_mol = load_openeye_pdb(f"{out_fn}.pdb")
-
-            # ## Build docking system and run docking
-            # docking_system = build_docking_system_direct(
-            #     reloaded_mol,
-            #     oechem.OEMolToSmiles(lig),
-            #     f"{name}_{holo_name}",
-            #     holo_name,
-            # )
-
-            # from kinoml.modeling.OEModeling import prepare_structure
-            # from openeye import oespruce
-
-            # print("complex_mol", flush=True)
-            # print(
-            #     # prepare_structure(
-            #     #     complex_mol, has_ligand=True, loop_db=args.loop
-            #     # )
-            #     list(oespruce.OEMakeDesignUnits(complex_mol))
-            # )
-
-            # print("reloaded_mol", flush=True)
-            # print(
-            #     # prepare_structure(
-            #     #     reloaded_mol, has_ligand=True, loop_db=args.loop
-            #     # )
-            #     list(oespruce.OEMakeDesignUnits(reloaded_mol))
-            # )
-
-            # print("docking_system", flush=True)
-            # print(
-            #     # prepare_structure(
-            #     #     docking_system.protein.molecule,
-            #     #     has_ligand=True,
-            #     #     loop_db=args.loop,
-            #     # )
-            #     list(
-            #         oespruce.OEMakeDesignUnits(docking_system.protein.molecule)
-            #     )
-            # )
-            # return
-
-            # ## Run docking
-            # docking_system = run_docking(
-            #     cache_dir, out_base, args.loop, 1, [docking_system]
-            # )
-
-            # print(
-            #     "POSIT probability:",
-            #     oechem.OEGetSDData(docked_mol, "POSIT::Probability"),
-            #     flush=True
-            # )
-            # print(
-            #     "Docking score:",
-            #     oechem.OEGetSDData(docked_mol, "Chemgauss4"),
-            #     flush=True
-            # )
-
-            # print(
-            #     "Docking score:",
-            #     docking_system.featurizations["last"]._topology.docking_score,
-            #     flush=True,
-            # )
-            # print(
-            #     "POSIT probability:",
-            #     docking_system.featurizations[
-            #         "last"
-            #     ]._topology.posit_probability,
-            #     flush=True,
-            # )
-
     results_cols = [
+        "SARS_ligand",
         "MERS_structure",
-        "SARS_structure",
         "docked_file",
         "docked_RMSD",
         "POSIT_prob",
