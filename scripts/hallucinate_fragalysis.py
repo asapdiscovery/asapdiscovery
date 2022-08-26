3b821abd65e296d491466cb007868dd8226e688c
kaminow <benjamin.kaminow@choderalab.org>
2022-08-26 17:46:39 -0400

    Change from using kinoml pose_molecules function to using pure OpenEye for docking ligands.

--- scripts/hallucinate_fragalysis.py
+++ scripts/hallucinate_fragalysis.py
@@ -11,8 +11,6 @@ import pandas
 import pickle as pkl
 import sys
 
-from kinoml.docking.OEDocking import pose_molecules
-
 sys.path.append(f"{os.path.dirname(os.path.abspath(__file__))}/../")
 from covid_moonshot_ml.datasets.utils import (
     get_compound_id_xtal_dicts,
@@ -121,12 +119,38 @@ def mp_func(
     complex_mol = du_to_complex(du)
     save_openeye_pdb(complex_mol, f"{out_fn}.pdb")
 
-    ## Run docking with kinoml
+    ## Set up POSIT docking options
+    opts = oedocking.OEPositOptions()
+    ## kinoml has the below option set, but the accompanying comment implies
+    ##  that we should be ignoring N stereochemistry, which, paradoxically,
+    ##  corresponds to a False option (the default)
+    # opts.SetIgnoreNitrogenStereo(True)
+    opts.SetPositMethods(
+        oedocking.OEPositMethod_FRED
+        | oedocking.OEPositMethod_HYBRID
+        | oedocking.OEPositMethod_SHAPEFIT
+    )
+
+    ## Set up poser object
+    poser = oedocking.OEPosit(opts)
+    poser.AddReceptor(du)
+
+    ## Run posing
     dock_lig = oechem.OEGraphMol()
     du.GetLigand(dock_lig)
-    try:
-        docked_mol = pose_molecules(du, [dock_lig], score_pose=True)[0]
-    except TypeError:
+    pose_res = oedocking.OESinglePoseResult()
+    ret_code = poser.Dock(pose_res, dock_lig)
+
+    ## Check results
+    if ret_code == oedocking.OEDockingReturnCode_Success:
+        posed_mol = pose_res.GetPose()
+        posit_prob = pose_res.GetProbability()
+
+        ## Get the Chemgauss4 score (adapted from kinoml)
+        pose_scorer = oedocking.OEScore(oedocking.OEScoreType_Chemgauss4)
+        pose_scorer.Initialize(du)
+        chemgauss_score = pose_scorer.ScoreLigand(posed_mol)
+    else:
         print(f"Pose generation failed for {lig_name}/{apo_name}")
         results = (lig_name, apo_name, None, -1.0, -1.0, -1.0)
         pkl.dump(results, open(f"{out_base}/results.pkl", "wb"))
@@ -147,7 +171,7 @@ def mp_func(
         apo_name,
         f"{out_base}/docked.sdf",
         rmsd,
-        float(oechem.OEGetSDData(docked_mol, "POSIT::Probability")),
+        float(posit_prob),
         float(oechem.OEGetSDData(docked_mol, "Chemgauss4")),
     )
     pkl.dump(results, open(f"{out_base}/results.pkl", "wb"))
