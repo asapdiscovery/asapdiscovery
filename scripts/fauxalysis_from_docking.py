fe00c0fea0d471fa1602bf5f7c9135612c094471
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-09-16 20:41:04 +0200

    change shutil function to copy the whole directory

--- scripts/fauxalysis_from_docking.py
+++ scripts/fauxalysis_from_docking.py
@@ -26,11 +26,11 @@ def main():
     docking_results = DockingResults(args.input_csv)
 
     for index, values in docking_results.df.to_dict(orient='index').items():
-        print(values)
+        # print(values)
         input_dir_path = os.path.dirname(values["Docked_File"])
-        print(input_dir_path)
+        # print(input_dir_path)
         output_dir_path = os.path.join(args.output_dir, values["Complex_ID"])
-        shutil.copy2(input_dir_path, output_dir_path)
+        shutil.copytree(input_dir_path, output_dir_path)
 
 if __name__ == '__main__':
     main()
