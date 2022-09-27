565dc11b1782374816e31be04629592d251f38ab
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-09-27 15:38:10 -0400

    enable use of yaml file for alignment selections

--- scripts/download_and_align_pdbs.py
+++ scripts/download_and_align_pdbs.py
@@ -1,10 +1,9 @@
-import argparse
-import os
-import sys
+import argparse, os, yaml, sys
 
 repo_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(repo_path)
 from covid_moonshot_ml.datasets import pdb
+
 
 ################################################################################
 def get_args():
@@ -13,8 +12,8 @@ def get_args():
         "-d", "--pdb_dir_path", help="Directory name to put the structures"
     )
     parser.add_argument(
-        "-y",
-        "--yaml_path",
+        "-p",
+        "--pdb_yaml_path",
         default="../data/mers-structures.yaml",
         help="MERS structures yaml file",
     )
@@ -22,16 +21,33 @@ def get_args():
         "-r", "--ref_path", default=None, help="Path to pdb reference file to align to"
     )
     parser.add_argument("-n", "--ref_name", default=None, help="Name of reference")
+    parser.add_argument(
+        "-s",
+        "--sel_dict_yaml_path",
+        default=None,
+        help="Path to yaml file containing selection dictionary",
+    )
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    pdb_list = pdb.load_pdbs_from_yaml(args.y)
-    pdb.download_PDBs(pdb_list, args.d)
-    sel_dict = {"chainA_protein": "chain A and polymer.protein"}
+    pdb_list = pdb.load_pdbs_from_yaml(args.pdb_yaml_path)
+    pdb.download_PDBs(pdb_list, args.pdb_dir_path)
+    if args.sel_dict_yaml_path:
+        print(f"Loading selection dictionary from {args.sel_dict_yaml_path}...")
+        with open(args.sel_dict_yaml_path, "r") as f:
+            sel_dict = yaml.safe_load(f)
+    else:
+        sel_dict = None
     pdb.align_all_pdbs(
-        pdb_list, args.pdb_dir_path, args.ref_path, args.ref_name, sel_dict
+        pdb_list,
+        args.pdb_dir_path,
+        args.ref_path,
+        args.ref_name,
+        mobile_chain_id="A",
+        ref_chain_id="A",
+        sel_dict=sel_dict,
     )
 
 
