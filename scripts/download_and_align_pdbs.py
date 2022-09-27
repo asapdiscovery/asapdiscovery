f1c6a629a9b627fed8f99d6917320894405663e9
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-09-27 15:31:46 -0400

    use easier to read arguments in pdb_downloading script

--- scripts/download_and_align_pdbs.py
+++ scripts/download_and_align_pdbs.py
@@ -9,14 +9,19 @@ from covid_moonshot_ml.datasets import pdb
 ################################################################################
 def get_args():
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("-d", help="Directory name to put the structures")
     parser.add_argument(
-        "-y", default="mers-structures.yaml", help="MERS structures yaml file"
+        "-d", "--pdb_dir_path", help="Directory name to put the structures"
     )
     parser.add_argument(
-        "-r", default=None, help="Path to pdb reference file to align to"
+        "-y",
+        "--yaml_path",
+        default="../data/mers-structures.yaml",
+        help="MERS structures yaml file",
     )
-    parser.add_argument("-n", default=None, help="Name of reference")
+    parser.add_argument(
+        "-r", "--ref_path", default=None, help="Path to pdb reference file to align to"
+    )
+    parser.add_argument("-n", "--ref_name", default=None, help="Name of reference")
     return parser.parse_args()
 
 
@@ -25,7 +30,9 @@ def main():
     pdb_list = pdb.load_pdbs_from_yaml(args.y)
     pdb.download_PDBs(pdb_list, args.d)
     sel_dict = {"chainA_protein": "chain A and polymer.protein"}
-    pdb.align_all_pdbs(pdb_list, args.d, args.r, args.n, sel_dict)
+    pdb.align_all_pdbs(
+        pdb_list, args.pdb_dir_path, args.ref_path, args.ref_name, sel_dict
+    )
 
 
 if __name__ == "__main__":
