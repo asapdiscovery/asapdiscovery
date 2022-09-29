440dcfe19be3367171c3dcc7c8e932137769e89f
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-09-29 14:01:16 -0400

    implement filtering a dictionary of CrystalCompoundData

--- scripts/test-ligand-filter.py
+++ scripts/test-ligand-filter.py
@@ -28,24 +28,44 @@ def get_args():
         type=str,
         help="Path to csv file containing compound info.",
     )
+    parser.add_argument(
+        "-s",
+        "--smarts_queries",
+        default="../data/smarts_queries.csv",
+        type=str,
+        help="Path to csv file containing smarts queries.",
+    )
 
     return parser.parse_args()
 
 
 def main():
     args = get_args()
+
+    # First, parse the fragalysis directory and
     sars_xtals = utils.parse_fragalysis_data(args.csv_file, args.fragalysis_dir)
+
+    # For the compounds for which we have smiles strings, get a dictionary mapping the Compound_ID to the smiles
     cmp_to_smiles_dict = {
         compound_id: data.smiles
         for compound_id, data in sars_xtals.items()
         if data.smiles
     }
-    # print(sars_xtals)
+    # Filter based on the smiles using this OpenEye function
     filtered_inputs = utils.filter_docking_inputs(
-        smarts_queries="../data/smarts_queries.csv",
+        smarts_queries=args.smarts_queries,
         docking_inputs=cmp_to_smiles_dict,
     )
+
+    # Get a new dictionary of sars xtals based on the filtered inputs
     print(filtered_inputs)
+    sars_xtals_filtered = {
+        compound_id: data
+        for compound_id, data in sars_xtals.items()
+        if compound_id in filtered_inputs
+    }
+    print(sars_xtals_filtered)
+    print(len(sars_xtals_filtered))
 
 
 if __name__ == "__main__":
