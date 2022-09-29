63e2a00d2d88cd4dabebd60af4eeacd848638ca2
kaminow <benjamin.kaminow@choderalab.org>
2022-09-29 16:28:30 -0400

    Remove default argument for `--smarts_queries` to avoid accidental filtering.

--- scripts/hallucinate_fragalysis.py
+++ scripts/hallucinate_fragalysis.py
@@ -407,7 +407,6 @@ def get_args():
     parser.add_argument(
         "-s",
         "--smarts_queries",
-        default="../data/smarts_queries.csv",
         type=str,
         help="Path to csv file containing smarts queries.",
     )
