b5a7e8df0ee0f6e80ffa5aef62df7b7155adba1a
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-10-05 15:57:26 -0400

    correctly handle "_bound" addition or deletion

--- scripts/fauxalysis_from_docking.py
+++ scripts/fauxalysis_from_docking.py
@@ -176,23 +176,22 @@ def write_fragalysis_output(
         oechem.OEWriteMolecule(ofs, lig)
 
         if frag_dir:
-            # TODO: Map the compound_id to the crystal structure and use that to build the path
-            ## the compound ids used so far include a chain A vs chain B split, let's remove that
-
             if "_bound" in compound_id:
                 frag_structure = compound_id
+                bound_str = ""
             else:
                 compound_id_without_chain, chain = compound_id.split("_")
                 frag_structure = (
                     f"{cmpd_to_frag_dict[compound_id_without_chain]}_{chain}"
                 )
+                bound_str = "_bound"
 
             frag_compound_path = os.path.join(frag_dir, frag_structure)
             bound_pdb_path = os.path.join(
-                frag_compound_path, f"{frag_structure}_bound.pdb"
+                frag_compound_path, f"{frag_structure}{bound_str}.pdb"
             )
             copied_bound_pdb_path = os.path.join(
-                compound_out_dir, f"fragalysis_{compound_id}_bound.pdb"
+                compound_out_dir, f"fragalysis_{compound_id}{bound_str}.pdb"
             )
             if os.path.exists(bound_pdb_path):
                 print(
