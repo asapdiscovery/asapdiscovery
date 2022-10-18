b3fa6f20997f1f818be4821bc68866887df526dd
kaminow <benjamin.kaminow@choderalab.org>
2022-10-18 10:53:54 -0400

    Start adding OpenEye docking function to module.

--- covid_moonshot_ml/docking/docking.py
+++ covid_moonshot_ml/docking/docking.py
@@ -135,3 +135,151 @@ def run_docking(cache_dir, output_dir, loop_db, n_procs, docking_systems):
     docking_systems = featurizer.featurize(docking_systems)
 
     return docking_systems
+
+
+def run_docking_oe(du, dock_lig, dock_sys, relax="none", hybrid=False):
+    """
+    Run docking using OpenEye.
+
+    Parameters
+    ----------
+    du : oechem.OEDesignUnit
+        DesignUnit receptor to dock to
+    dock_lig : oechem.OEMol
+        Mol object to dock
+    dock_sys : str
+        Which docking system to use ["posit", "hybrid"]
+    relax : str, default="none"
+        When to check for relaxation ["clash", "all", "none"]
+    hybrid : bool, default=False
+        Set POSIT methods to only use Hybrid
+
+    Returns
+    -------
+    bool
+        If docking succeeded
+    float
+        RMSD
+    float
+        POSIT prob
+    float
+        Chemgauss4 score
+    int
+        Whether clash correction was disabled, enabled and no clash was found,
+        or enables and clash was found ([-1, 0, 1] respectively)
+    """
+    from openeye import oechem, oedocking
+
+    ## Keep track of if there's a clash (-1 if not using POSIT, 0 if no clash,
+    ##  1 if there was a clash that couldn't be resolved)
+    clash = -1
+
+    ## Get ligand to dock
+    if dock_sys == "posit":
+        ## Set up POSIT docking options
+        opts = oedocking.OEPositOptions()
+        ## kinoml has the below option set, but the accompanying comment implies
+        ##  that we should be ignoring N stereochemistry, which, paradoxically,
+        ##  corresponds to a False option (the default)
+        opts.SetIgnoreNitrogenStereo(True)
+        ## Set the POSIT methods to only be hybrid (otherwise leave as default
+        ##  of all)
+        if hybrid:
+            opts.SetPositMethods(oedocking.OEPositMethod_HYBRID)
+
+        ## Set up pose relaxation
+        if relax == "clash":
+            clash = 0
+            opts.SetPoseRelaxMode(oedocking.OEPoseRelaxMode_CLASHED)
+        elif relax == "all":
+            clash = 0
+            opts.SetPoseRelaxMode(oedocking.OEPoseRelaxMode_ALL)
+        elif relax != "none":
+            ## Don't need to do anything for none bc that's already the default
+            raise ValueError(f'Unknown arg for relaxation "{relax}"')
+
+        ## Set up poser object
+        poser = oedocking.OEPosit(opts)
+        poser.AddReceptor(du)
+
+        ## Run posing
+        pose_res = oedocking.OESinglePoseResult()
+        ret_code = poser.Dock(pose_res, dock_lig)
+    elif dock_sys == "hybrid":
+        print("Running Hybrid docking", flush=True)
+
+        ## Set up poser object
+        poser = oedocking.OEHybrid()
+        poser.Initialize(du)
+
+        ## Run posing
+        posed_mol = oechem.OEMol()
+        ret_code = poser.DockMultiConformerMolecule(posed_mol, dock_lig)
+
+        posit_prob = -1.0
+    else:
+        raise ValueError(f'Unknown docking system "{dock_sys}"')
+
+    if ret_code == oedocking.OEDockingReturnCode_NoValidNonClashPoses:
+        ## For POSIT with clash removal, if no non-clashing pose can be found,
+        ##  re-run with no clash removal
+        opts.SetPoseRelaxMode(oedocking.OEPoseRelaxMode_NONE)
+        clash = 1
+
+        print(
+            f"Re-running POSIT {'hybrid' if hybrid else 'all'} docking with "
+            f"no relaxation for {lig_name}/{apo_name}",
+            flush=True,
+        )
+
+        ## Set up poser object
+        poser = oedocking.OEPosit(opts)
+        poser.AddReceptor(du)
+
+        ## Run posing
+        pose_res = oedocking.OESinglePoseResult()
+        ret_code = poser.Dock(pose_res, dock_lig)
+
+    ## Check results
+    if ret_code == oedocking.OEDockingReturnCode_Success:
+        if dock_sys == "posit":
+            posed_mol = pose_res.GetPose()
+            posit_prob = pose_res.GetProbability()
+
+        ## Get the Chemgauss4 score (adapted from kinoml)
+        pose_scorer = oedocking.OEScore(oedocking.OEScoreType_Chemgauss4)
+        pose_scorer.Initialize(du)
+        chemgauss_score = pose_scorer.ScoreLigand(posed_mol)
+    else:
+        err_type = oedocking.OEDockingReturnCodeGetName(ret_code)
+        print(
+            f"Pose generation failed for {lig_name}/{apo_name} ({err_type})",
+            flush=True,
+        )
+        return False, -1.0, -1.0, -1.0, clash
+
+    ## Calculate RMSD
+    oechem.OECanonicalOrderAtoms(dock_lig)
+    oechem.OECanonicalOrderBonds(dock_lig)
+    oechem.OECanonicalOrderAtoms(posed_mol)
+    oechem.OECanonicalOrderBonds(posed_mol)
+    ## Get coordinates, filtering out Hs
+    predocked_coords = [
+        c
+        for a in dock_lig.GetAtoms()
+        for c in dock_lig.GetCoords()[a.GetIdx()]
+        if a.GetAtomicNum() != 1
+    ]
+    docked_coords = [
+        c
+        for a in posed_mol.GetAtoms()
+        for c in posed_mol.GetCoords()[a.GetIdx()]
+        if a.GetAtomicNum() != 1
+    ]
+    rmsd = oechem.OERMSD(
+        oechem.OEDoubleArray(predocked_coords),
+        oechem.OEDoubleArray(docked_coords),
+        len(predocked_coords) // 3,
+    )
+
+    return True, rmsd, posit_prob, chemgauss_score, clash
