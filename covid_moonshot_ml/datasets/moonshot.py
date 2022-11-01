a8c040b18dea12b10da5bd9df26a6c90fdd831b2
kaminow <benjamin.kaminow@choderalab.org>
2022-11-01 16:33:38 -0400

    Add more general download_molecules function.

--- covid_moonshot_ml/datasets/moonshot.py
+++ covid_moonshot_ml/datasets/moonshot.py
@@ -125,3 +125,66 @@ def download_achiral(header, fn_out=None):
         achiral_df.to_csv(fn_out, index=False)
 
     return achiral_df
+
+
+# TODO: Generalize inclusion criteria to something more compact
+def download_molecules(
+    header,
+    smiles_fieldname="suspected_SMILES",
+    fn_out=None,
+    fn_cache=None,
+    **filter_kwargs,
+):
+    """
+    Download all molecules and remove any chiral molecules.
+    Parameters
+    ----------
+    header : dict
+        Header information passed to GET request. Must contain an entry for
+        'X-CDD-token' that gives the user's CDD API token
+    smiles_fieldname : str, default='suspected_SMILES'
+        Field to use to extract SMILES
+    fn_out : str, optional
+        If specified, filename to write CSV to
+    fn_cache : str, optional
+        If specified, file to write unfiltered CSV download to
+    filter_kwargs :
+        Other arguments passed to filter_molecules
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame containing compound information for all achiral molecules
+    """
+    import os
+
+    if fn_cache and os.path.exists(fn_cache):
+        with open(fn_cache, "rt") as infile:
+            content = infile.read()
+    else:
+        ## Download all molecules to start
+        url = VAULT_URL + NONCOVALENT_SMI_SEARCH
+        logging.debug(f"Downloading data from CDD vault from {url}")
+        response = download_url(url, header)
+        content = response.content.decode()
+
+        if fn_cache:
+            with open(fn_cache, "wt") as outfile:
+                outfile.write(content)
+
+    ## Parse into DF
+    mol_df = pandas.read_csv(StringIO(content))
+    logging.debug(f"\n{mol_df}")
+
+    ## Remove chiral molecules
+    logging.debug(f"Filtering dataframe...")
+    from .utils import filter_molecules_dataframe
+
+    filtered_df = filter_molecules_dataframe(mol_df, **filter_kwargs)
+
+    ## Save to CSV as requested
+    if fn_out:
+        logging.debug(f"Generating CSV file {fn_out}")
+        filtered_df.to_csv(fn_out, index=False)
+
+    return filtered_df
