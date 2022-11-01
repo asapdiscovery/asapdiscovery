f61a7184ab90fe433b01ae20ad437715cbfb299a
kaminow <benjamin.kaminow@choderalab.org>
2022-11-01 11:21:04 -0400

    Add TODO for a different branch/PR.

--- covid_moonshot_ml/data/dataset.py
+++ covid_moonshot_ml/data/dataset.py
@@ -1,6 +1,7 @@
 from torch.utils.data import Dataset
 
 
+### TODO: Add smiles/pic50 values here to match GraphDataset
 class DockedDataset(Dataset):
     """
     Class for loading docking results into a dataset to be used for graph
