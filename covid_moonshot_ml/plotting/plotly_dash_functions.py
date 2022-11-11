2411cf7ddb08a3a70d41d1a1e1a627bcbd1b3bab
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-11-11 13:53:24 -0500

    create dash-docking-contour-plot.py as an example for how this logic could work in future iterations

--- covid_moonshot_ml/plotting/plotly_dash_functions.py
+++ covid_moonshot_ml/plotting/plotly_dash_functions.py
@@ -3,3 +3,97 @@ import pandas as pd
 from dash import Dash, dcc, html, Input, Output, dash_table, ctx
 import plotly.express as px
 import json, argparse, os
+
+
+def get_dash_app():
+    app = Dash(__name__)
+    styles = {"pre": {"border": "thin lightgrey solid", "overflowX": "scroll"}}
+    return app, styles
+
+
+# def wrap_plotting_function(app)
+
+
+def get_contour_plot():
+    return html.Div(
+        [
+            dcc.Graph(
+                id="crossfilter-indicator-contour",
+            )
+        ],
+        style={
+            "display": "inline-block",
+            "padding": "0 20",
+            # "float": "right",
+        },
+    )
+
+
+def get_filters(variable_list):
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.H4("X-Axis"),
+                    dcc.Dropdown(
+                        variable_list,
+                        "Chemgauss4",
+                        id="crossfilter-xaxis-column",
+                    ),
+                    html.P("Filter X-Axis:"),
+                    dcc.RangeSlider(
+                        id="x-axis-slider",
+                        min=-50,
+                        max=50,
+                        value=[-50, 50],
+                    ),
+                    dcc.RadioItems(
+                        ["Linear", "Log"],
+                        "Linear",
+                        id="crossfilter-xaxis-type",
+                        labelStyle={
+                            "display": "inline-block",
+                            "marginTop": "5px",
+                        },
+                    ),
+                ],
+                style={
+                    "width": "49%",
+                    "display": "inline-block",
+                },
+            ),
+            html.Div(
+                [
+                    html.H4("Y-Axis"),
+                    dcc.Dropdown(
+                        variable_list,
+                        "RMSD",
+                        id="crossfilter-yaxis-column",
+                    ),
+                    html.P("Filter Y-Axis:"),
+                    dcc.RangeSlider(
+                        id="y-axis-slider",
+                        min=0,
+                        max=30,
+                        value=[0, 30],
+                    ),
+                    dcc.RadioItems(
+                        ["Linear", "Log"],
+                        "Linear",
+                        id="crossfilter-yaxis-type",
+                        labelStyle={
+                            "display": "inline-block",
+                            "marginTop": "5px",
+                        },
+                    ),
+                ],
+                style={
+                    "width": "49%",
+                    "display": "inline-block",
+                },
+            ),
+        ],
+        style={
+            "padding": "10px 5px",
+        },
+    )
--- covid_moonshot_ml/plotting/plotting.py
+++ covid_moonshot_ml/plotting/plotting.py
@@ -1,9 +1,9 @@
 # TODO: Do we need to add plotly to our environment yaml?
-import plotly.express as ex
+import plotly.express as px
 
 
 def plot_poses_auc(poses_df):
-    fig = ex.line(
+    fig = px.line(
         poses_df,
         x="False_Positive",
         y="True_Positive",
@@ -21,7 +21,7 @@ def plot_poses_auc(poses_df):
 
 
 def plot_precision_recall(poses_df):
-    fig = ex.line(
+    fig = px.line(
         poses_df,
         x="True_Positive",
         y="Precision",
@@ -33,4 +33,67 @@ def plot_precision_recall(poses_df):
         scaleanchor="x",
         scaleratio=1,
     )
+    return fig
+
+
+def update_contour(
+    df,
+    xaxis_column_name,
+    yaxis_column_name,
+    xaxis_type,
+    yaxis_type,
+    x_range,
+    y_range,
+):
+    """
+    Make a contour plot of two variables that are each columns in a dataframe
+    Values for column names and range sliders.
+
+    Parameters
+    ----------
+    df
+    xaxis_column_name
+    yaxis_column_name
+    xaxis_type
+    yaxis_type
+    x_range
+    y_range
+
+    Returns
+    -------
+
+    """
+    filtered = df[
+        (df[xaxis_column_name] > x_range[0])
+        & (df[xaxis_column_name] < x_range[1])
+        & (df[yaxis_column_name] > y_range[0])
+        & (df[yaxis_column_name] < y_range[1])
+    ]
+
+    fig = px.density_contour(
+        filtered,
+        x=xaxis_column_name,
+        y=yaxis_column_name,
+        marginal_x="histogram",
+        marginal_y="histogram",
+    )
+    fig.update_traces(
+        contours_coloring="heatmap",
+        selector=dict(type="histogram2dcontour"),
+        colorscale="Peach",
+    )
+    fig.update_xaxes(
+        title=xaxis_column_name,
+        type="linear" if xaxis_type == "Linear" else "log",
+    )
+
+    fig.update_yaxes(
+        title=yaxis_column_name,
+        type="linear" if yaxis_type == "Linear" else "log",
+    )
+
+    fig.update_layout(
+        margin={"l": 40, "b": 40, "t": 40, "r": 40}, hovermode="closest"
+    )
+
     return fig
