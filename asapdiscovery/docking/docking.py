959cda67fee91caf65823fac1b90fcdfe32d12e6
Jenke Scheen <jenke.scheen@choderalab.org>
2022-11-28 11:50:26 +0100

    set OE to inject SMILES into docked lig SD tag

--- asapdiscovery/docking/docking.py
+++ asapdiscovery/docking/docking.py
@@ -249,7 +249,8 @@ def run_docking_oe(
         posed_mol, f"Docking_{docking_id}_Chemgauss4", str(chemgauss_score)
     )
     oechem.OESetSDData(posed_mol, f"Docking_{docking_id}_clash", str(clash))
-
+    oechem.OESetSDData(posed_mol, f"SMILES", oechem.OEMolToSmiles(dock_lig))
+    print(f"############ SETTING SMILES {oechem.OEMolToSmiles(dock_lig)} for LIG {posed_mol}")
     ## Set molecule name if given
     if compound_name:
         posed_mol.SetTitle(compound_name)
