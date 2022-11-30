d7f06be70fe98beee3c12347d1af330e8d256d78
Alexander Matthew Payne <alex.payne@choderalab.org>
2022-11-30 10:06:49 -0500

    fix some module packaging errors

--- cluster_scripts/test_run_oe_docking.sh
+++ cluster_scripts/test_run_oe_docking.sh
@@ -1,17 +1,20 @@
 
 #!/bin/bash
-#BSUB -J run_oe_docking_amines_small
+#BSUB -J test_run_oe_docking
 #BSUB -R span[hosts=1]
-#BSUB -oo log_files/test_run_oe_docking.out
-#BSUB -n 1
+#BSUB -o log_files/test_run_oe_docking.out
+#BSUB -e log_files/test_run_oe_docking.stderr
+#BSUB -n 10
+#BSUB -q cpuqueue
 #BSUB -R rusage[mem=4]
 #BSUB -W 00:20
-conda activate mers-docking2
+source ~/.bashrc
+conda activate mers-docking
 python ~/covid-moonshot-ml/asapdiscovery/docking/scripts/run_docking_oe.py \
 -l ~/asap-datasets/test_run_oe/ligand.sdf \
 -r '~/asap-datasets/full_frag_prepped_dus_seqres/*/prepped_receptor.oedu' \
 -s /lila/data/chodera/asap-datasets/amines_small_mcs/mcs_sort_index.pkl \
--o /lila/data/chodera/asap-datasets/alex_test \
--n 1 \
+-o ~/asap-datasets/test_run_oe/ \
+-n 10 \
 -t 10
 echo done
