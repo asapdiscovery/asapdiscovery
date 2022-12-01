5508905bbf7bee4d4fd735bd974a84fceee5ce00
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-12-01 15:07:41 -0500

    create submission script for fragalysis retrospective

---
+++ cluster_scripts/run_fragalysis_retrospective.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#BSUB -J test_run_oe_docking
+#BSUB -R span[hosts=1]
+#BSUB -o log_files/test_run_oe_docking.out
+#BSUB -e log_files/test_run_oe_docking.stderr
+#BSUB -n 64
+#BSUB -q cpuqueue
+#BSUB -R rusage[mem=4]
+#BSUB -W 72:00
+source ~/.bashrc
+conda activate mers-docking
+python ~/covid-moonshot-ml/asapdiscovery/docking/scripts/run_docking_oe.py \
+-l ~/asap-datasets/mpro_fragalysis_2022_10_12/Mpro_combined.sdf \
+-r '/lila/data/chodera/asap-datasets/prospective/prepped_mpro_structures_fragalysis/*/prepped_receptor.oedu' \
+-s /home/kaminowb/work/stereochemistry_pred/mcs_res/mcs_sort_index.pkl \
+-o ~/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221201/ \
+-n 64 \
+-t 1
+echo Done
+date
