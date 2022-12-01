7a74f185efa8eef26e98e4737d22ea8aa018ee3e
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-12-01 15:27:15 -0500

    test running without mcss pickle

--- cluster_scripts/run_fragalysis_retrospective.sh
+++ cluster_scripts/run_fragalysis_retrospective.sh
@@ -11,9 +11,10 @@ conda activate mers-docking
 python ~/covid-moonshot-ml/asapdiscovery/docking/scripts/run_docking_oe.py \
 -l ~/asap-datasets/mpro_fragalysis_2022_10_12/Mpro_combined.sdf \
 -r '/lila/data/chodera/asap-datasets/prospective/prepped_mpro_structures_fragalysis/*/prepped_receptor.oedu' \
--s /home/kaminowb/work/stereochemistry_pred/mcs_res/mcs_sort_index.pkl \
 -o ~/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221201/ \
 -n 64 \
 -t 1
 echo Done
 date
+
+## -s /home/kaminowb/work/stereochemistry_pred/mcs_res/mcs_sort_index.pkl \
