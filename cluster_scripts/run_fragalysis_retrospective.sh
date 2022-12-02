069ed1390e03088e0d3c816e9eec7f3ba1b17a8b
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-12-02 13:08:49 -0500

    fix cluster script typo

--- cluster_scripts/run_fragalysis_retrospective.sh
+++ cluster_scripts/run_fragalysis_retrospective.sh
@@ -12,6 +12,6 @@ python ~/covid-moonshot-ml/asapdiscovery/docking/scripts/run_docking_oe.py \
 -l ~/asap-datasets/mpro_fragalysis_2022_10_12/2022_12_02_fragalysis_correct_bond_orders_220_P_structures.sdf \
 -r '/lila/data/chodera/asap-datasets/prospective/prepped_mpro_structures_fragalysis/*/prepped_receptor.oedu' \
 -o ~/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221202/ \
--n 64 \
+-n 64
 echo Done
 date
