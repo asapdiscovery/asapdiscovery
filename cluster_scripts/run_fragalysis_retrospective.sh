cc1ceb706f79a11c946451813c01ea95d0384487
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-12-09 14:29:04 -0500

    change paths in cluster script

--- cluster_scripts/run_fragalysis_retrospective.sh
+++ cluster_scripts/run_fragalysis_retrospective.sh
@@ -4,14 +4,14 @@
 #BSUB -e log_files/run_fragalysis_retrospective.stderr
 #BSUB -n 64
 #BSUB -q cpuqueue
-#BSUB -R rusage[mem=4]
-#BSUB -W 72:00
+#BSUB -R rusage[mem=2]
+#BSUB -W 48:00
 source ~/.bashrc
 conda activate mers-docking
 python ~/covid-moonshot-ml/asapdiscovery/docking/scripts/run_docking_oe.py \
--l ~/asap-datasets/mpro_fragalysis_2022_10_12/2022_12_02_fragalysis_correct_bond_orders_220_P_structures.sdf \
+-l /lila/data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/2022_12_02_fragalysis_correct_bond_orders_220_P_structures.sdf \
 -r '/lila/data/chodera/asap-datasets/full_frag_prepped_mpro_12_2022/*/prepped_receptor.oedu' \
--o ~/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221208/ \
+-o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221208/ \
 -n 64
 echo Done
 date
