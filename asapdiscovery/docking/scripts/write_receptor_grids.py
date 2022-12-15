86dbb1245a79b1cd2311264cbc96a0d7f486f180
Alexander Matthew Payne <alexpayne1997@gmail.com>
2022-12-15 17:08:39 -0500

    add script to write out receptor grids

---
+++ asapdiscovery/docking/scripts/write_receptor_grids.py
@@ -0,0 +1,47 @@
+import sys, os, argparse
+from glob import glob
+
+sys.path.append(
+    f"{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}"
+)
+from asapdiscovery.data.openeye import save_receptor_grid
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="")
+
+    ## Input arguments
+    parser.add_argument(
+        "-i",
+        "--input_glob",
+        required=True,
+        type=str,
+        help="Glob of design units to load.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output_dir",
+        required=True,
+        type=str,
+        help="Path to output_dir.",
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+    print(f"{len(glob(args.input_glob))} files found")
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    for du_fn in glob(args.input_glob):
+        ## get name of directory
+        fn_header = os.path.split(os.path.dirname(du_fn))[1]
+        out_fn = os.path.join(args.output_dir, f"{fn_header}.ccp4")
+        print(f"Writing {du_fn} to {out_fn}")
+        save_receptor_grid(du_fn, out_fn)
+
+
+if __name__ == "__main__":
+    main()
