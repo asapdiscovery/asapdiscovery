74f1292627a722a0eba5a0705cde9be5149c55c4
Alexander Matthew Payne <alex.payne@choderalab.org>
2022-12-19 17:11:08 -0500

    cluster script edit

--- cluster_scripts/prep_full_mpro_AP.sh
+++ cluster_scripts/prep_full_mpro_AP.sh
@@ -1,10 +1,9 @@
-
 #!/bin/bash
 #BSUB -J prep_full_mpro_AP
 #BSUB -R span[hosts=1]
 #BSUB -oo log_files/prep_full_mpro_AP.out
 #BSUB -e log_files/prep_full_mpro_AP.stderr
-#BSUB -n 1
+#BSUB -n 32
 #BSUB -R rusage[mem=4]
 #BSUB -W 24:00
 mkdir -p /data/chodera/asap-datasets/full_frag_prepped_mpro_20221219/
@@ -15,7 +14,7 @@ python /data/chodera/paynea/covid-moonshot-ml/asapdiscovery/docking/scripts/prep
 -x /data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/extra_files/Mpro_compound_tracker_csv.csv \
 -o /data/chodera/asap-datasets/full_frag_prepped_mpro_20221219/ \
 -l /data/chocera/asap-datasets/rcsb_spruce.loop_db \
--n 1 \
+-n 32 \
 -s /data/chodera/paynea/covid-moonshot-ml/metadata/mpro_sars2_seqres.yaml \
 --include_non_Pseries
 echo date
