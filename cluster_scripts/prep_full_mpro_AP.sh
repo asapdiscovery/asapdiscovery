fd2b39425512449cbbc3a49538fc4afcca22401e
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-01-11 15:17:25 -0500

    adjust protein prep cluster scrip parameters

--- cluster_scripts/prep_full_mpro_AP.sh
+++ cluster_scripts/prep_full_mpro_AP.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 #BSUB -J prep_full_mpro_AP
-#BSUB -R span[hosts=1]
 #BSUB -oo log_files/prep_full_mpro_AP.out
 #BSUB -e log_files/prep_full_mpro_AP.stderr
-#BSUB -n 16
+#BSUB -n 32
 #BSUB -R rusage[mem=4]
-#BSUB -W 24:00
-mkdir -p /data/chodera/asap-datasets/full_frag_prepped_mpro_20221220/
+#BSUB -W 72:00
+mkdir -p /data/chodera/asap-datasets/full_frag_prepped_mpro_20230111/
 source ~/.bashrc
 conda activate mers-docking
 python /data/chodera/paynea/covid-moonshot-ml/asapdiscovery/docking/scripts/prep_proteins.py \
 -d /data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/aligned/ \
 -x /data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/extra_files/Mpro_compound_tracker_csv.csv \
--o /data/chodera/asap-datasets/full_frag_prepped_mpro_20221220/ \
+-o /data/chodera/asap-datasets/full_frag_prepped_mpro_20230111/ \
 -l /data/chocera/asap-datasets/rcsb_spruce.loop_db \
--n 16 \
+-n 32 \
 -s /data/chodera/paynea/covid-moonshot-ml/metadata/mpro_sars2_seqres.yaml \
 --include_non_Pseries
 echo date
