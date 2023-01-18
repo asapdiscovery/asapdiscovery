fb253385420fb4af4d50a68f2c9caf9013e1fd3a
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-01-18 17:51:05 -0500

    finish re-running mers dimers prep

--- cluster_scripts/prep_full_mpro_AP_testing.sh
+++ cluster_scripts/prep_full_mpro_AP_testing.sh
@@ -2,9 +2,9 @@
 #BSUB -J prep_full_mpro_AP_test
 #BSUB -oo log_files/prep_full_mpro_AP_test.out
 #BSUB -e log_files/prep_full_mpro_AP_test.stderr
-#BSUB -n 32
+#BSUB -n 16
 #BSUB -R rusage[mem=4]
-#BSUB -W 72:00
+#BSUB -W 12:00
 mkdir -p /data/chodera/asap-datasets/full_frag_prepped_mpro_20230118/
 source ~/.bashrc
 conda activate mers-docking
@@ -13,7 +13,7 @@ python /data/chodera/paynea/covid-moonshot-ml/asapdiscovery/docking/scripts/prep
 -x /data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/extra_files/Mpro_compound_tracker_csv.csv \
 -o /data/chodera/asap-datasets/full_frag_prepped_mpro_20230118/ \
 -l /data/chodera/asap-datasets/rcsb_spruce.loop_db \
--n 32 \
+-n 16 \
 -s /data/chodera/paynea/covid-moonshot-ml/metadata/mpro_sars2_seqres.yaml \
 --include_non_Pseries
 echo date
