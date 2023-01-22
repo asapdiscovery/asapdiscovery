26310909dacbf375606b26e62e1e65909f7501ca
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-01-22 16:33:58 -0500

    simplify protein prep script

--- cluster_scripts/prep_full_mpro_AP.sh
+++ cluster_scripts/prep_full_mpro_AP.sh
@@ -2,18 +2,18 @@
 #BSUB -J prep_full_mpro_AP
 #BSUB -oo log_files/prep_full_mpro_AP.out
 #BSUB -e log_files/prep_full_mpro_AP.stderr
-#BSUB -n 32
+#BSUB -n 16
 #BSUB -R rusage[mem=4]
-#BSUB -W 72:00
-mkdir -p /data/chodera/asap-datasets/full_frag_prepped_mpro_20230111/
+#BSUB -W 12:00
+mkdir -p /data/chodera/asap-datasets/full_frag_prepped_mpro_20230122/
 source ~/.bashrc
 conda activate mers-docking
 python /data/chodera/paynea/covid-moonshot-ml/asapdiscovery/docking/scripts/prep_proteins.py \
 -d /data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/aligned/ \
 -x /data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/extra_files/Mpro_compound_tracker_csv.csv \
--o /data/chodera/asap-datasets/full_frag_prepped_mpro_20230111/ \
--l /data/chocera/asap-datasets/rcsb_spruce.loop_db \
--n 32 \
+-o /data/chodera/asap-datasets/full_frag_prepped_mpro_20230122/ \
+-l /data/chodera/asap-datasets/rcsb_spruce.loop_db \
+-n 16 \
 -s /data/chodera/paynea/covid-moonshot-ml/metadata/mpro_sars2_seqres.yaml \
 --include_non_Pseries
 echo date
