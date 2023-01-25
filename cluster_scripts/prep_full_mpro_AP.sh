ca814004b0cb51b2bd4edab29ae1bb6701a02ad0
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-01-25 16:21:46 -0500

    change directory for cluster script

--- cluster_scripts/prep_full_mpro_AP.sh
+++ cluster_scripts/prep_full_mpro_AP.sh
@@ -5,13 +5,13 @@
 #BSUB -n 32
 #BSUB -R rusage[mem=4]
 #BSUB -W 12:00
-mkdir -p /data/chodera/asap-datasets/full_frag_prepped_mpro_20230122/
+mkdir -p /data/chodera/asap-datasets/full_frag_prepped_mpro_20230125/
 source ~/.bashrc
 conda activate mers-docking
 python /data/chodera/paynea/covid-moonshot-ml/asapdiscovery/docking/scripts/prep_proteins.py \
 -d /data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/aligned/ \
 -x /data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/metadata.csv \
--o /data/chodera/asap-datasets/full_frag_prepped_mpro_20230122/ \
+-o /data/chodera/asap-datasets/full_frag_prepped_mpro_20230125/ \
 -l /data/chodera/asap-datasets/rcsb_spruce.loop_db \
 -n 32 \
 -s /data/chodera/paynea/covid-moonshot-ml/metadata/mpro_sars2_seqres.yaml \
