869845461da966775d2553617b8a34de52918692
Kendall Lemons <116607074+kendallrlemons@users.noreply.github.com>
2023-02-01 21:09:54 -0500

    Update MProP0009_Sub.sh

--- asapdiscovery/simulation/scripts/MProP0009_Sub.sh
+++ asapdiscovery/simulation/scripts/MProP0009_Sub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #BSUB -J run_single_simulation
-#BSUB -oo log_files/prep_full_mpro_AP_test.out
-#BSUB -e log_files/prep_full_mpro_AP_test.stderr
+#BSUB -oo log_files/MProP0009_Sub.out
+#BSUB -e log_files/MProP0009_Sub.stderr
 #BSUB -n 1
 #BSUB -R rusage[mem=4]
 #BSUB -q gpuqueue
@@ -12,10 +12,8 @@ source ~/.bashrc
 conda activate docking
 
 python /data/chodera/lemonsk/covid-moonshot-ml/asapdiscovery/simulation/scripts/MproP0009.py \
-
 -i /data/chodera/asap-datasets/full_frag_prepped_mpro_12_2022/Mpro-P0009_0A_MAT-POS-f2460aef-1/prepped_receptor_0.pdb \
 -o /data/chodera/lemonsk/default_out \
-
 --include_non_Pseries
 echo date
 echo done
