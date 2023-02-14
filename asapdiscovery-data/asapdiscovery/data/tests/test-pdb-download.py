e111451895ea50415f8c6d805943b241ce944621
Iván Pulido <2949729+ijpulidos@users.noreply.github.com>
2023-02-14 17:46:17 -0500

    moving test script to asapdiscovery-data test dir

--- asapdiscovery-data/asapdiscovery/data/tests/test-pdb-download.py
+++ asapdiscovery-data/asapdiscovery/data/tests/test-pdb-download.py
@@ -2,9 +2,9 @@ from asapdiscovery.data.pdb import download_pdb_structure, load_pdbs_from_yaml
 from pathlib import Path
 
 
-pdb_yaml = Path("../../../metadata/mers-structures.yaml")
+pdb_yaml = Path("../../../../metadata/mers-structures.yaml")
 pdb_dict = load_pdbs_from_yaml(str(pdb_yaml))
-pdb_dir = Path("pdb_download")
+pdb_dir = Path("../../../../asapdiscovery/data/tests/pdb_download")
 pdb_dir.mkdir(exist_ok=True)
 
 download_pdb_structure("8DGY", pdb_dir, file_format="pdb")
