cd806b3c2729347467acc66d301f5e6bda433dd5
kaminow <benjamin.kaminow@choderalab.org>
2023-02-22 05:21:33 -0500

    Make random seed optional.

--- asapdiscovery-modeling/asapdiscovery/modeling/scripts/run_colabfold.sh
+++ asapdiscovery-modeling/asapdiscovery/modeling/scripts/run_colabfold.sh
@@ -3,25 +3,40 @@
 ################################################################################
 ## Script to run colabfold_batch using a random seed
 ## Input CSV file should have a {} in the id column that will be filled with
-##  the random seed to make sure different structure are generated
-## Usage is ./run_colabfold.sh ./query.csv ./template_dir/ ./out_dir/
+##  the random seed to make sure different structures are generated. If
+##  n_structures is not provided, the script will generate one structure. If the
+##  --no-rand flag is passed, the default random seed of 0 will be used.
+## Usage is ./run_colabfold.sh ./query.csv ./template_dir/ ./out_dir/ \
+##          [n_structures] [--no-rand]
 ################################################################################
 
 csv_fn=$1
 template_dir=$2
 out_dir=$3
 n_gen=$4
+no_rand=$5
+
+if [[ $n_gen == "--no-rand" || $no_rand == "--no-rand" ]]; then
+    no_rand=true
+else
+    no_rand=false
+fi
+
+[[ $n_gen == "" || $n_gen == "--no-rand" ]] && n_gen=1
 
 echo csv $csv_fn
 echo templates $template_dir
 echo outputs $out_dir
 echo samples $n_gen
-
-[[ $n_gen == "" ]] && n_gen=1
+echo no_rand $no_rand
 
 for i in $(seq 1 $n_gen); do
     ## Generate random seed for structure generation
-    rng_seed=$RANDOM
+    if [[ $no_rand ]]; then
+        rng_seed=0
+    else
+        rng_seed=$RANDOM
+    fi
     echo running with seed $rng_seed
 
     ## Make temp CSV file with seed in name
