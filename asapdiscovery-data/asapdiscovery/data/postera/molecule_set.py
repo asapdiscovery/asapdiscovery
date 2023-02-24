2f2e1285a6034e05805f4cfab16e15b7aeaa2714
David Dotson <dotsdl@gmail.com>
2023-02-23 22:33:19 -0700

    Completed API points, new CLI

--- asapdiscovery-data/asapdiscovery/data/postera/molecule_set.py
+++ asapdiscovery-data/asapdiscovery/data/postera/molecule_set.py
@@ -1,6 +1,6 @@
 from .postera_api import PostEraAPI
 
-from typing import Dict, Union, List
+from typing import Dict, Union, List, Optional
 from typing_extensions import TypedDict
 import json
 import pandas as pd
@@ -23,29 +23,26 @@ class MoleculeUpdate(TypedDict):
 class MoleculeList(List[Molecule]):
     """Data type to pass to PostEra API in molecule set create"""
 
+    @classmethod
     def from_pandas_df(
-        self,
+        cls,
         df: pd.DataFrame,
-        smiles_field: str = None,
-        first_entry: int = 0,
-        last_entry: int = 0,
+        smiles_field: str = 'smiles',
+        postera_id_field: str = 'id',
     ):
-
-        #data = json.loads(df.to_json(orient="records"))
-
-        self.extend(
+        return cls(
             [
                 {
-                    "smiles": datum[smiles_field],
+                    "smiles": row[smiles_field],
                     "customData": {
                         **{
                             key: value
-                            for key, value in datum.items()
-                            if key not in [smiles_field, "mol"]
+                            for key, value in row.items()
+                            if key not in [smiles_field, postera_id_field]
                         },
                     },
                 }
-                for datum in df[first_entry:last_entry]
+                for _, row in df.iterrows()
             ]
         )
 
@@ -53,112 +50,235 @@ class MoleculeList(List[Molecule]):
 class MoleculeUpdateList(List[MoleculeUpdate]):
     """Data type to pass to PostEra API in molecule set update_custom_data"""
 
+    @classmethod
     def from_pandas_df(
-        self,
+        cls,
         df: pd.DataFrame,
-        postera_id_field: str = None,
-        first_entry: int = 0,
-        last_entry: int = 0,
+        smiles_field: str = 'smiles',
+        postera_id_field: str = 'id',
     ):
-
-        #data = json.loads(df.to_json(orient="records"))
-
-        self.extend(
+        return cls(
             [
                 {
-                    "id": str(datum[postera_id_field]),
+                    "id": str(row[postera_id_field]),
                     "customData": {
                         **{
                             key: value
-                            for key, value in datum.items()
-                            if key not in [postera_id_field, "mol"]
+                            for key, value in row.items()
+                            if key not in [smiles_field, postera_id_field]
                         },
                     },
                 }
-                for datum in df[first_entry:last_entry]
+                for _, row in df.iterrows()
             ]
         )
 
 
-class MoleculeSet(PostEraAPI):
+class MoleculeSetAPI(PostEraAPI):
     """Connection and commands for PostEra Molecule Set API"""
 
-    def __init__(self, *args, **kwargs):
+    @property
+    def molecule_set_url(self):
+        return f"{self.api_url}/moleculesets"
 
-        super(MoleculeSet, self).__init__(*args, **kwargs)
+    def create(
+            self, 
+            molecule_set_name: str,
+            data: MoleculeList,
+            return_full: bool = False
+        ) -> str:
+        """Create a MoleculeSet from a list of Molecules.
 
-        self.molecule_set_url = f"{self.api_url}/moleculesets"
+        Parameters
+        ----------
+        set_name
+            The human-readable name for the set.
+        data
+            MoleculeList giving Molecules to add.
+        return_full
+            If `True`, return a dict containing summary data for the created
+            MoleculeSet; if `False`, return only its unique id.
 
-    def create(self, data: MoleculeList, set_name: str) -> str:
-
-        create_url = f"{self.molecule_set_url}/"
+        """
+        url = f"{self.molecule_set_url}/"
         response = self._session.post(
-            create_url,
+            url,
             json={
                 "molecules": data,
-                "name": set_name,
+                "name": molecule_set_name,
             },
         ).json()
 
-        return response["id"]
+        if return_full:
+            return response
+        else:
+            return response["id"]
 
-    def read_page(
+    def _read_page(
             self, 
-            molecule_set_id: str,
+            url: str,
             page: int
         ) -> (pd.DataFrame, str):
 
-        print(f"Getting page {page}")
-        read_url = f"{self.molecule_set_url}/{molecule_set_id}/get_all_molecules/"
-        response = self._session.get(read_url, params={"page": page}).json()
+        response = self._session.get(url, params={"page": page}).json()
 
         return response["results"], response["paginationInfo"]["hasNext"]
 
-    def read(
+    def _collate(
             self,
-            molecule_set_id: str
-        ) -> pd.DataFrame:
-
+            url
+            ):
         page = 0
         has_next = True
-        all_results = []
+        results = []
 
         while has_next:
             page += 1
-            result, has_next = self.read_page(molecule_set_id, page)
-            all_results.extend(result)
+            result, has_next = self._read_page(url, page)
+            results.extend(result)
 
-        response_data = [
-            {
-                "SMILES": result["smiles"],
-                "postera_molecule_id": result["id"],
-                **result["customData"],
-            }
-            for result in all_results
-        ]
+        return results
 
-        result_df = pd.DataFrame(response_data)
 
-        return result_df
+    def list(self, return_full: bool = False) -> Union[list[dict], dict]:
+        """List available MoleculeSets.
 
-    def update_custom_data(
-        self, 
-        molecule_set_id: str, 
-        data: MoleculeUpdateList, 
-        overwrite=False
-    ) -> List[float]:
-        """Updates the custom data associated with the Molecules in a MoleculeSet.
+        Parameters
+        ----------
+        return_full
+            If `True`, return a list of dicts containing summary data for each
+            MoleculeSet; if `False`, return a dict with the unique id for each
+            MoleculeSet as keys, human-readable name as values.
+
+        """
+        url = f"{self.molecule_set_url}/"
+
+        results = self._collate(url)
+
+        if return_full:
+            return results
+        else:
+            return {result['id']: result['name'] for result in results}
+
+
+    def get(self, molecule_set_id: str) -> dict:
+        """Get summary data for a given MoleculeSet.
 
         Parameters
         ----------
         molecule_set_id
             The unique id of the MoleculeSet
 
+        Returns
+        -------
+        Summary data as a dict.
+
+        """
+        url = f"{self.molecule_set_url}/{molecule_set_id}"
+        response = self._session.get(
+            url,
+        ).json()
+
+        return response
+
+    def get_molecules(
+            self,
+            molecule_set_id: str,
+            return_as = "dataframe"
+        ) -> Union[pd.DataFrame, list]:
+        """Pull the full contents of a MoleculeSet as a DataFrame.
+
+        Parameters
+        ----------
+        molecule_set_id
+            The unique id of the MoleculeSet
+        return_as : {'dataframe', 'list'}
+
         """
 
-        update_url = f"{self.molecule_set_url}/{molecule_set_id}/update_molecules/"
+        if not return_as in ('dataframe', 'list'):
+            raise ValueError("`return_as` must be either 'dataframe' or 'list'")
+
+        url = f"{self.molecule_set_url}/{molecule_set_id}/get_all_molecules/"
+
+        results = self._collate(url)
+
+        if return_as == 'list':
+            return results
+        elif return_as == 'dataframe':
+            response_data = [
+                {
+                    "smiles": result["smiles"],
+                    "id": result["id"],
+                    **result["customData"],
+                }
+                for result in results
+            ]
+            return pd.DataFrame(response_data)
+
+    def add_molecules(
+            self,
+            molecule_set_id: id,
+            data: MoleculeList,
+        ) -> int:
+        """Add additional molecules to the MoleculeSet.
+
+        Parameters
+        ----------
+        molecule_set_id
+            The unique id of the MoleculeSet.
+        data
+            MoleculeList giving Molecules to add.
+
+        Returns
+        -------
+        Number of molecules not added due to exceeding the max number of
+        molecules allowed in a MoleculeSet.
+
+        """
+        url = f"{self.molecule_set_url}/{molecule_set_id}/add_molecules/"
+        response = self._session.post(
+            url,
+            json={
+                "newMolecules": data,
+            },
+        )
+
+        return response.json()['nOverLimit']
+
+
+    def update_molecules(
+        self, 
+        molecule_set_id: str, 
+        data: MoleculeUpdateList,
+        overwrite=False
+    ) -> List[str]:
+        """Updates the custom data associated with the Molecules in a MoleculeSet.
+
+        Parameters
+        ----------
+        molecule_set_id
+            The unique id of the MoleculeSet.
+        data
+            MoleculeUpdateList giving MoleculeUpdates with update data for
+            existing Molecules.
+        overwrite
+            If `True`, then customData will be entirely replaced with new data
+            submitted; if `False`, then old and new customData will be combined,
+            overwriting keys existing in both.
+
+        Returns
+        -------
+        List of Molecule ids that were updated.
+
+        """
+
+        url = f"{self.molecule_set_url}/{molecule_set_id}/update_molecules/"
+
         response = self._session.patch(
-            update_url, json={"moleculesToUpdate": data, "overwrite": overwrite}
+            url, 
+            json={"moleculesToUpdate": data, 
+                  "overwrite": overwrite}
         ).json()
 
         return response["moleculesUpdated"]
