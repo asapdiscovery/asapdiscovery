45c9045ef0993ea645ec1b0dbace6c9235d253e9
kaminow <benjamin.kaminow@choderalab.org>
2023-02-27 11:26:10 -0500

    Add EarlyStopping class.

---
+++ asapdiscovery/ml/es.py
@@ -0,0 +1,63 @@
+"""
+Class for handling early stopping in training.
+"""
+
+
+class EarlyStopping(object):
+    """Class for handling early stopping in training."""
+
+    def __init__(self, patience):
+        """
+        Parameters
+        ----------
+        patience : int, optional
+            The maximum number of epochs to continue training with no improvement in the
+            val loss. If not given, no early stopping will be performed
+        """
+        super(EarlyStopping, self).__init__()
+        self.patience = patience
+
+        # Variables to track early stopping
+        self.counter = 0
+        self.best_loss = None
+        self.best_wts = None
+
+    def check(loss, wts_dict):
+        """
+        Check if training should be stopped. Return True to stop, False to keep going.
+
+        Parameters
+        ----------
+        loss : float
+            Model loss from the current epoch of training
+        wts_dict : dict
+            Weights dict from Pytorch for keeping track of the best model
+
+        Returns
+        -------
+        bool
+            Whether to stop training
+        """
+        # If this is the first epoch, just set internal variables and return
+        if self.best_loss is None:
+            self.best_loss = loss
+            self.best_wts = best_wts
+            return False
+
+        # Update best loss and best weights
+        if loss < self.best_loss:
+            self.best_loss = loss
+            self.best_wts = wts_dict
+
+            # Reset counter
+            self.counter = 0
+
+            # Keep training
+            return False
+
+        # Increment counter and check for stopping
+        self.counter += 1
+        if self.counter == self.patience:
+            return True
+
+        return False
