3cebf1f9203eaac42b8a88fe1664fd296242cd81
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-03-03 10:11:17 -0700

    see if this fixes our issue and also only run the test we are working on to speed up CI

--- asapdiscovery-data/asapdiscovery/data/tests/aws/conftest.py
+++ asapdiscovery-data/asapdiscovery/data/tests/aws/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 from asapdiscovery.data.aws.s3 import S3
 from boto3.session import Session
@@ -6,6 +8,11 @@ from moto.server import ThreadedMotoServer
 
 @pytest.fixture(scope="module")
 def s3():
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
     server = ThreadedMotoServer()
     server.start()
 
@@ -13,7 +20,7 @@ def s3():
         #    aws_access_key_id="test-access-key-id",
         #    aws_secret_access_key="test-secret-access-key",
         #    aws_session_token="test-session-token",
-        region_name="us-east-1",
+        #    region_name="us-east-1",
     )
 
     s3 = S3(session, "test-bucket", "test-prefix", endpoint_url="http://127.0.0.1:5000")
