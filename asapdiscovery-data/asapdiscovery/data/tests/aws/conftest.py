04c2d259404311de6cefe4a8cd4c079c252df39f
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-03-03 10:41:32 -0700

    black fix

--- asapdiscovery-data/asapdiscovery/data/tests/aws/conftest.py
+++ asapdiscovery-data/asapdiscovery/data/tests/aws/conftest.py
@@ -6,22 +6,22 @@ from boto3.session import Session
 from moto.server import ThreadedMotoServer
 
 
-@pytest.fixture(scope="module")
-def s3():
+@pytest.fixture(scope="function")
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+@pytest.fixture(scope="module")
+def s3(aws_credentials):
     server = ThreadedMotoServer()
     server.start()
 
-    session = Session(
-        #    aws_access_key_id="test-access-key-id",
-        #    aws_secret_access_key="test-secret-access-key",
-        #    aws_session_token="test-session-token",
-        #    region_name="us-east-1",
-    )
+    session = Session()
 
     s3 = S3(session, "test-bucket", "test-prefix", endpoint_url="http://127.0.0.1:5000")
     s3.initialize()
