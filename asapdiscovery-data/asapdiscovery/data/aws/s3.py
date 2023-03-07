2c061bb6d0bea97596a52138745a163e8d45f4b0
David Dotson <dotsdl@gmail.com>
2023-03-06 17:06:12 -0700

    Added AWS S3 push interface (#159)
    * Added AWS S3 push interface
    
    Added S3 object and `aws.s3` submodule.
    
    Also added CLI command tree, and reorganized other command trees (e.g.
    `postera`) into own submodule.
    
    Needs:
    - [ ] CLI tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Updated test env
    
    * data : Small change to top-level CLI
    
    * Added CLI test, reorganized fixture; switched to server mode for moto
    
    We need to use server mode in order for our CLI calls to hit the same
    session state as the fixture.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Lol...forgot to add new files
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Small changes; troubleshooting failed CI and hanging
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Trying to reduce number of times we thrash threads
    
    * output after the first error
    
    * try passing in region
    
    * see if this fixes our issue and also only run the test we are working on to speed up CI
    
    * revert file origin main
    
    * black fix
    
    * Fix scope mismatch
    
    * data : Removed unnecessary env setting in CLI test
    
    * some flake8 fixes
    
    * removed unused import
    
    * missed an unsused import
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update asapdiscovery-data/asapdiscovery/data/tests/aws/test_aws_cli.py
    
    Co-authored-by: Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
    
    * data : updates to aws CLI from @hmacdope review
    
    * data : more updates from @hmacdope review
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * data : flake8 fix
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Mike Henry <11765982+mikemhenry@users.noreply.github.com>
    Co-authored-by: Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>

---
+++ asapdiscovery-data/asapdiscovery/data/aws/s3.py
@@ -0,0 +1,106 @@
+"""Interface for object generation on S3.
+
+"""
+
+import os
+from os import PathLike
+from typing import Optional
+
+import boto3
+
+
+class S3Error(Exception):
+    ...
+
+
+class S3:
+    """Interface for AWS S3."""
+
+    def __init__(
+        self,
+        session: boto3.Session,
+        bucket: str,
+        prefix: Optional[str] = None,
+        endpoint_url=None,
+    ):
+        """Create an interface to AWS S3.
+
+        Parameters
+        ----------
+        session
+            A `boto3.Session` object, already parameterized with credentials,
+            region, etc.
+        bucket
+            The name of the S3 bucket to target.
+        prefix
+            The prefix to use for referencing objects in the bucket;
+            functions as a working sub-folder/directory.
+        endpoint_url
+            The S3 endpoint to use; used for testing, generally not needed.
+
+        """
+        self.session = session
+        self.resource = self.session.resource("s3", endpoint_url=endpoint_url)
+
+        self.bucket = bucket
+        self.prefix = prefix.strip("/") if prefix is not None else ""
+
+    def initialize(self):
+        """Initialize bucket.
+
+        Creates bucket if it does not exist.
+
+        """
+        bucket = self.resource.Bucket(self.bucket)
+        bucket.create()
+        bucket.wait_until_exists()
+
+    def reset(self):
+        """Delete all objects, including bucket itself.
+
+        Inverse operation of `initialize`.
+
+        """
+        bucket = self.resource.Bucket(self.bucket)
+
+        # delete all objects, then the bucket
+        bucket.objects.delete()
+        bucket.delete()
+        bucket.wait_until_not_exists()
+
+    def push_file(
+        self, path: PathLike, location: PathLike = None, content_type: str = None
+    ):
+        """Push a file at the local filesystem `path` to an object `location`
+        in this S3 Bucket.
+
+        `location` is relative to the `prefix` set for use of this bucket; e.g.
+        if ``location='foo/bar.html'`` and ``self.prefix == 'baz'``, then the object will
+        be located at ``baz/foo/bar.html`` in the bucket.
+
+        Parameters
+        ----------
+        path
+            Path to file on local filesystem to push.
+        location
+            Location in the S3 bucket to place object relative to ``self.prefix``;
+            should not contain a leading ``/``.
+        content_type
+            Media type of the file being pushed. This will impact how the file
+            is handled by a browser upon URL access, e.g. for ``html`` you want
+            rendered on access, use ``'text/html'``.
+        """
+        if content_type is None:
+            extra_args = {}
+        else:
+            extra_args = {"ContentType": content_type}
+
+        if location is None:
+            location = os.path.basename(path)
+
+        key = os.path.join(self.prefix, location)
+
+        self.resource.Bucket(self.bucket).upload_file(path, key, ExtraArgs=extra_args)
+
+    def pull_file(self):
+        ...
