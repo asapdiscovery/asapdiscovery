4684c2302b30880cfa8ff5819ecf249fb97c6a5b
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-03-07 17:06:56 -0500

    fix path generator in test-mers_download.py

--- asapdiscovery-simulation/asapdiscovery/simulation/tests/test-mers_download.py
+++ asapdiscovery-simulation/asapdiscovery/simulation/tests/test-mers_download.py
@@ -4,9 +4,12 @@
 from asapdiscovery.simulation.utils import test_forcefield_generation
 from pathlib import Path
 
-paths = Path(
-    "/lila/data/chodera/asap-datasets/mers_fauxalysis/20230307_prepped_mers_pdbs/"
-).glob("*0_pdb")
+paths = [
+    pdb_path
+    for pdb_path in Path(
+        "/lila/data/chodera/asap-datasets/mers_fauxalysis/20230307_prepped_mers_pdbs/"
+    ).glob("*0_pdb")
+]
 if __name__ == "__main__":
     for path in paths:
         test_forcefield_generation(str(path))
