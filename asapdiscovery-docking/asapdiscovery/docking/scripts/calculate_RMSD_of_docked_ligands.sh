817f31405ee3008f00bd869b9780c6c1f7deb1b8
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-03-11 14:43:08 -0500

    change calculate_RMSD_of_docked_ligands.sh parameters

--- asapdiscovery-docking/asapdiscovery/docking/scripts/calculate_RMSD_of_docked_ligands.sh
+++ asapdiscovery-docking/asapdiscovery/docking/scripts/calculate_RMSD_of_docked_ligands.sh
@@ -2,17 +2,17 @@
 #BSUB -J calculate_RMSD
 #BSUB -o log_files/calculate_RMSD_of_docked_ligands.out
 #BSUB -e log_files/calculate_RMSD_of_docked_ligands.stderr
-#BSUB -n 32
+#BSUB -n 1
 #BSUB -q cpuqueue
-#BSUB -R rusage[mem=4]
-#BSUB -W 24:00
+#BSUB -R rusage[mem=1]
+#BSUB -W 1:00
 source ~/.bashrc
-conda activate mers-docking
+conda activate ad-3.9
 python calculate_RMSD_of_docked_ligands.py \
 -sdf /data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221208/combined.sdf \
 -o /data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221208 \
--r '/data/chodera/asap-datasets/full_frag_prepped_mpro_12_2022/*/prepped_receptor_0.pdb' \
--n 32
+-r '/data/chodera/asap-datasets/full_frag_prepped_mpro_20230122/*/sdf' \
+-n 1
 
 echo Done
 date
