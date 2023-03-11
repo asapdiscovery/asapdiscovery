d561ea12e26a8abcedfcb03bee156880b837aeb5
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-03-11 15:49:00 -0500

    skip test_rmsd_calculation.py since no OELicense in CI

--- asapdiscovery-docking/asapdiscovery/docking/tests/test_rmsd_calculation.py
+++ asapdiscovery-docking/asapdiscovery/docking/tests/test_rmsd_calculation.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+from pytest import skip
 import numpy as np
 from asapdiscovery.data.openeye import load_openeye_sdf
 from asapdiscovery.docking.analysis import (
@@ -7,14 +7,16 @@ from asapdiscovery.docking.analysis import (
     write_all_rmsds_to_reference,
 )
 
-output_dir = Path("outputs")
-input_dir = Path("inputs")
 
-ref_mol = load_openeye_sdf(str(input_dir / "Mpro-P0008_0A_ERI-UCB-ce40166b-17.sdf"))
-query_mol = load_openeye_sdf(str(input_dir / "ERI-UCB-ce40166b-17_Mpro-P2201_0A.sdf"))
-
-
+@skip("No OELicense in CI yet")
 def test_rmsd_calculation():
+    input_dir = Path("inputs")
+
+    ref_mol = load_openeye_sdf(str(input_dir / "Mpro-P0008_0A_ERI-UCB-ce40166b-17.sdf"))
+    query_mol = load_openeye_sdf(
+        str(input_dir / "ERI-UCB-ce40166b-17_Mpro-P2201_0A.sdf")
+    )
+
     rmsd = calculate_rmsd_openeye(ref_mol, ref_mol)
     assert rmsd == 0.0
 
@@ -22,9 +24,18 @@ def test_rmsd_calculation():
     assert rmsd == 5.791467472680422
 
 
+@skip("No OELicense in CI yet")
 def test_writing_rmsd_calculation():
+    output_dir = Path("outputs")
+    input_dir = Path("inputs")
+
+    ref_mol = load_openeye_sdf(str(input_dir / "Mpro-P0008_0A_ERI-UCB-ce40166b-17.sdf"))
+    query_mol = load_openeye_sdf(
+        str(input_dir / "ERI-UCB-ce40166b-17_Mpro-P2201_0A.sdf")
+    )
+
     write_all_rmsds_to_reference(
         ref_mol, [query_mol], output_dir, "ERI-UCB-ce40166b-17"
     )
-    rmsds = np.load(output_dir / "ERI-UCB-ce40166b-17.npy")
+    rmsds = np.load(str(output_dir / "ERI-UCB-ce40166b-17.npy"))
     print(rmsds)
