38f0adee92e21a43d143c7e1e4cd98731cd7c241
hmacdope <hugomacdermott@gmail.com>
2023-03-14 16:06:06 +1100

    add testing of weights

---
+++ asapdiscovery-ml/asapdiscovery/ml/tests/tests.py
@@ -0,0 +1,17 @@
+import asapdiscovery.ml
+import pytest
+import shutil
+
+@pytest.fixture()
+def weights_yaml():
+    weights = "test_weights.yaml"
+    yield weights
+    shutil.rmtree('./_weights', ignore_errors=True)
+
+    
+
+@pytest.mark.parametrize("force_fetch", [True, False])
+def test_fetch_weights(weights_yaml, force_fetch):
+    w = asapdiscovery.ml.weights.fetch_weights_from_spec(weights_yaml, ["schnet", "schnet2"], local_path="./_weights/", force_fetch=force_fetch)
+
+
