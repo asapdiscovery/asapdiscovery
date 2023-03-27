a56f99dc83c79fce7b960610cdb5448ebe924c43
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-03-27 09:11:22 -0400

    update RMSD calculation script

--- asapdiscovery-docking/asapdiscovery/docking/scripts/calculate_RMSD_of_docked_ligands.sh
+++ asapdiscovery-docking/asapdiscovery/docking/scripts/calculate_RMSD_of_docked_ligands.sh
@@ -2,17 +2,17 @@
 #BSUB -J calculate_RMSD
 #BSUB -o log_files/calculate_RMSD_of_docked_ligands.out
 #BSUB -e log_files/calculate_RMSD_of_docked_ligands.stderr
-#BSUB -n 1
+#BSUB -n 16
 #BSUB -q cpuqueue
-#BSUB -R rusage[mem=1]
+#BSUB -R rusage[mem=4]
 #BSUB -W 1:00
 source ~/.bashrc
 conda activate ad-3.9
-python calculate_RMSD_of_docked_ligands.py \
+calculate-RMSD-of-docked-ligands \
 -sdf /data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221208/combined.sdf \
--o /data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221208 \
--r '/data/chodera/asap-datasets/full_frag_prepped_mpro_20230122/*/sdf' \
--n 1
+-o /data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221208_analysis \
+-r '/data/chodera/asap-datasets/full_frag_prepped_mpro_20230122/*/*.sdf' \
+-n 16
 
 echo Done
 date
