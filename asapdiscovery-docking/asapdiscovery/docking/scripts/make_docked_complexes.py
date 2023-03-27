836fed607ba4bc5915d7636bc9234d036a57abcd
kaminow <benjamin.kaminow@choderalab.org>
2023-03-27 13:32:48 -0400

    Make caching structures an option.

--- asapdiscovery-docking/asapdiscovery/docking/scripts/make_docked_complexes.py
+++ asapdiscovery-docking/asapdiscovery/docking/scripts/make_docked_complexes.py
@@ -5,11 +5,12 @@ import argparse
 import os
 
 import pandas
+from asapdiscovery.data.openeye import combine_protein_ligand
 from asapdiscovery.data.openeye import load_openeye_pdb  # noqa: E402
 from asapdiscovery.data.openeye import load_openeye_sdf  # noqa: 402
+from asapdiscovery.data.openeye import oechem
 from asapdiscovery.data.openeye import save_openeye_pdb  # noqa: 402
 from asapdiscovery.data.openeye import split_openeye_mol  # noqa: E402
-from asapdiscovery.data.openeye import combine_protein_ligand, oechem
 
 
 def get_args():
@@ -38,6 +39,13 @@ def get_args():
         help="Only keep protein atoms from the apo PDB file.",
     )
 
+    # Cache protein structures
+    parser.add_argument(
+        "--cache",
+        action="store_true",
+        help="Cache protein structures to avoid loading the same structure many times.",
+    )
+
     return parser.parse_args()
 
 
@@ -47,41 +55,55 @@ def main():
     # Load docking results
     df = pandas.read_csv(args.in_file, index_col=0)
 
-    # Dict to hold loaded Mpro structures to avoid reloading the same one a
-    #  bunch of times
+    # Dict to hold loaded Mpro structure info to avoid recalculating
     xtal_structs = {}
     use_cols = ["ligand_id", "du_structure", "docked_file"]
     for _, (compound_id, struct, docked_fn) in df[use_cols].iterrows():
-        try:
-            xtal = xtal_structs[struct][0].CreateCopy()
-            new_resid, new_atomid = xtal_structs[struct][1:]
-        except KeyError:
+        print("Working on", compound_id, struct, docked_fn, flush=True)
+        # Either haven't seen the structure yet (so need to load/calculate anyway),
+        #  or aren't caching protein structures, so need to reload structure
+        if (struct not in xtal_structs) or (not args.cache):
+            # Load protein structure
             xtal_fn = f"{args.xtal_dir}/{struct}/{struct}_apo.pdb"
             xtal = load_openeye_pdb(xtal_fn)
-
             # Get rid of non-protein atoms
             if args.prot_only:
                 xtal = split_openeye_mol(xtal, lig_chain=None)["pro"]
 
-            # Find max resid for numbering the ligand residue
-            new_resid = (
-                max([r.GetResidueNumber() for r in oechem.OEGetResidues(xtal)]) + 1
-            )
-
-            # Same with atom numbering
-            new_atomid = (
-                max(
-                    [
-                        oechem.OEAtomGetResidue(a).GetSerialNumber()
-                        for a in xtal.GetAtoms()
-                    ]
+            # If we have seen the struct before (and therefore aren't caching)
+            #  structures), we can just pull the calculated resid/atomid
+            if struct in xtal_structs:
+                new_resid, new_atomid = xtal_structs[struct]
+            # Otherwise need to calculate resid/atomid, and store relevant info
+            else:
+                # Find max resid for numbering the ligand residue
+                new_resid = (
+                    max([r.GetResidueNumber() for r in oechem.OEGetResidues(xtal)]) + 1
                 )
-                + 1
-            )
 
-            # Store data to avoid reloading/recalculating
-            xtal_structs[struct] = (xtal.CreateCopy(), new_resid, new_atomid)
+                # Same with atom numbering
+                new_atomid = (
+                    max(
+                        [
+                            oechem.OEAtomGetResidue(a).GetSerialNumber()
+                            for a in xtal.GetAtoms()
+                        ]
+                    )
+                    + 1
+                )
 
+                # Store protein structure if we're doing that, otherwise just resid
+                #  and atomid
+                if args.cache:
+                    xtal_structs[struct] = (xtal.CreateCopy(), new_resid, new_atomid)
+                else:
+                    xtal_structs[struct] = (new_resid, new_atomid)
+        else:
+            # Already seen the protein and are caching structure
+            xtal = xtal_structs[struct][0].CreateCopy()
+            new_resid, new_atomid = xtal_structs[struct][1:]
+
+        # Load ligand
         try:
             mol = load_openeye_sdf(docked_fn)
         except TypeError:
@@ -94,6 +116,7 @@ def main():
             )
             continue
 
+        # Combine protein and ligand
         xtal = combine_protein_ligand(
             xtal, mol, resid=new_resid, start_atom_id=new_atomid
         )
