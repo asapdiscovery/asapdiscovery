837663208d2cab0aaf08fb96e5659aa558f8d1ec
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-03-28 14:20:37 -0400

    change cluster params to use debug flag for retro docking

--- asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective.sh
+++ asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-#BSUB -J test_run_oe_docking
-#BSUB -o log_files/run_fragalysis_retrospective.out
-#BSUB -e log_files/run_fragalysis_retrospective.stderr
-#BSUB -n 32
+#BSUB -J run_oe_docking
+#BSUB -o run_fragalysis_retrospective.out
+#BSUB -e run_fragalysis_retrospective.stderr
+#BSUB -n 2
 #BSUB -q cpuqueue
 #BSUB -R rusage[mem=4]
-#BSUB -W 48:00
+#BSUB -W 00:10
 source ~/.bashrc
 conda activate mers-docking
 run-docking-oe \
 -l /lila/data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/2022_12_02_fragalysis_correct_bond_orders_220_P_structures.sdf \
 -r '/lila/data/chodera/asap-datasets/full_frag_prepped_mpro_12_2022/*/prepped_receptor_0.oedu' \
--o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20221208/ \
--n 32
+-o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230328/ \
+-n 2 \
+--omega \
+--relax clash \
+--debug_num 2
 echo Done
 date
