508f3094411124308ea9f3ef3a2875d0df1a3a05
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-03-28 15:24:14 -0400

    update run_fragalysis_retrospective.sh cluster params

--- asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective.sh
+++ asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #BSUB -J run_oe_docking
-#BSUB -o run_fragalysis_retrospective.out
-#BSUB -e run_fragalysis_retrospective.stderr
-#BSUB -n 2
+#BSUB -oo run_fragalysis_retrospective.out
+#BSUB -eo run_fragalysis_retrospective.stderr
+#BSUB -n 32
 #BSUB -q cpuqueue
-#BSUB -R rusage[mem=4]
-#BSUB -W 00:10
+#BSUB -R rusage[mem=16]
+#BSUB -W 01:00
 source ~/.bashrc
-conda activate mers-docking
+conda activate ad-3.9
 run-docking-oe \
 -l /lila/data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/2022_12_02_fragalysis_correct_bond_orders_220_P_structures.sdf \
--r '/lila/data/chodera/asap-datasets/full_frag_prepped_mpro_12_2022/*/prepped_receptor_0.oedu' \
+-r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/*/*_prepped_receptor_0.oedu' \
 -o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230328/ \
--n 2 \
+-n 32 \
 --omega \
 --relax clash \
---debug_num 2
+--debug_num 32
 echo Done
 date
