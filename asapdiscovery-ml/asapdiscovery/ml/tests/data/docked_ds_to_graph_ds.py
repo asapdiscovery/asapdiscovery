95bb65325c2cff2e33a26e673b0be5e2ec93bab1
hmacdope <hugomacdermott@gmail.com>
2023-03-29 16:49:31 +1100

    test prediction and smiles ordering equivariance

--- asapdiscovery-ml/asapdiscovery/ml/tests/data/docked_ds_to_graph_ds.py
+++ asapdiscovery-ml/asapdiscovery/ml/tests/data/docked_ds_to_graph_ds.py
@@ -1,11 +1,16 @@
-"""Subsample a dataset to a DockedDataset to the first N entries given size."""
+"""Convert a DockedDataset to a GraphDataset with just a few entries for testing purposes."""
 
 import pickle
 import numpy as np
 from asapdiscovery.ml.dataset import DockedDataset, GraphDataset
 from asapdiscovery.ml.utils import split_dataset
-from asapdiscovery.data.schema import ExperimentalCompoundDataUpdate, ExperimentalCompoundData
+from asapdiscovery.data.schema import (
+    ExperimentalCompoundDataUpdate,
+    ExperimentalCompoundData,
+)
 from dgllife.utils import CanonicalAtomFeaturizer
+
+from rdkit import Chem
 
 
 def load_data(file):
@@ -13,33 +18,53 @@ def load_data(file):
         data = pickle.load(f)
     return data
 
+
 data = load_data("./achiral_enantiopure_best_02_13_23_dd.pkl")
 
 print(data)
 
+# we will only take a single value
 compound, pose = data[0]
-print(pose)
-print(pose.keys())
 
-for k, v in pose.items():
-    print(k, v, type(v))
+# randomly shuffle the smiles string
+mol = Chem.MolFromSmiles(pose["smiles"])
+newsmiles = Chem.MolToSmiles(mol, doRandom=True)
 
-print( compound[0], type(compound[0]))
-print( compound[1], type(compound[1]))
 
-data = {'compound_id':compound[1], 'smiles':pose['smiles'], 'experimental_data':{'pIC50':pose['pIC50'], 'pIC50_range':pose['pIC50_range'], 'pIC50_stderr':pose['pIC50_stderr']  }}
+data = {
+    "compound_id": compound[1],
+    "smiles": pose["smiles"],
+    "experimental_data": {
+        "pIC50": pose["pIC50"],
+        "pIC50_range": pose["pIC50_range"],
+        "pIC50_stderr": pose["pIC50_stderr"],
+    },
+}
+data_reorder = {
+    "compound_id": compound[1],
+    "smiles": newsmiles,
+    "experimental_data": {
+        "pIC50": pose["pIC50"],
+        "pIC50_range": pose["pIC50_range"],
+        "pIC50_stderr": pose["pIC50_stderr"],
+    },
+}
+
 
 ecd = ExperimentalCompoundData(**data)
+ecd_reorder = ExperimentalCompoundData(**data_reorder)
 
-gds = GraphDataset([ecd], cache_file="./cache.bin", node_featurizer=CanonicalAtomFeaturizer())
+gds = GraphDataset(
+    [ecd, ecd_reorder],
+    cache_file="./cache.bin",
+    node_featurizer=CanonicalAtomFeaturizer(),
+)
 
 print(ecd)
+print(ecd_reorder)
 print(gds)
-print(gds[0])
-print(gds[0][1]["g"])
-print(type(gds[0][1]["g"]))
 
 
-# dump val to a pickle file 
+# dump val to a pickle file
 with open("graph_ds.pkl", "wb") as f:
     pickle.dump(gds, f)
--- asapdiscovery-ml/asapdiscovery/ml/tests/test_inference.py
+++ asapdiscovery-ml/asapdiscovery/ml/tests/test_inference.py
@@ -6,6 +6,7 @@ import asapdiscovery.ml
 import pytest
 import pickle
 import torch
+from numpy.testing import assert_allclose
 
 
 def load_data(file):
@@ -26,10 +27,13 @@ def weights_yaml():
 @pytest.fixture()
 def test_data():
     # ugly hack to make the directory relative
+    # contains two data points in a GraphDataset, both the same with the smiles order changed in the second one
     data = load_data(os.path.join(os.path.dirname(__file__), "data/graph_ds.pkl"))
     # has structure ((design_unit, compound),  {smiles: smiles, g: graph, **kwargs})
     # we want the graph
-    return data[0][1]["g"]
+    g1 = data[0][1]["g"]
+    g2 = data[1][1]["g"]
+    return g1, g2
 
 
 def test_gatinference_construct(weights_yaml):
@@ -51,3 +55,25 @@ def test_gatinference_predict(weights_yaml, test_data):
     assert inference_cls is not None
     output = inference_cls.predict(test_data)
     assert output is not None
+
+
+def test_gatinference_predict(weights_yaml, test_data):
+    inference_cls = asapdiscovery.ml.inference.GATInference(
+        "gatmodel_test", weights_yaml
+    )
+    g1, _ = test_data
+    assert inference_cls is not None
+    output = inference_cls.predict(g1)
+    assert output is not None
+
+
+def test_gatinference_predict_smiles_equivariant(weights_yaml, test_data):
+    inference_cls = asapdiscovery.ml.inference.GATInference(
+        "gatmodel_test", weights_yaml
+    )
+    g1, g2 = test_data
+    # same data different smiles order
+    assert inference_cls is not None
+    output1 = inference_cls.predict(g1)
+    output2 = inference_cls.predict(g2)
+    assert_allclose(output1, output2)
