157a9cc1d8c5752aacc97b2dac4f312b8be3b25d
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-03-30 17:07:43 -0400

    change inputs for test_retro_docking.sh

--- asapdiscovery-docking/asapdiscovery/docking/tests/test_retro_docking.sh
+++ asapdiscovery-docking/asapdiscovery/docking/tests/test_retro_docking.sh
@@ -1,8 +1,8 @@
 # I know we'd rather use pytest, but this was much faster to write
 run-docking-oe \
--l './inputs/2022_12_02_fragalysis_correct_bond_orders_220_P_structures.sdf' \
+-l './inputs/Mpro_combined_labeled.sdf' \
 -r './inputs/Mpro-P0008_0A_ERI-UCB-ce40166b-17/*.oedu' \
--o . \
+-o '.outputs/.' \
 -n 2 \
 --omega \
 --relax clash \
