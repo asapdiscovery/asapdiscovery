9a15f07dd79630c83995c7ab802cbb6a6ed10c5f
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-04-06 09:56:27 -0400

    increase cluster script time

--- asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective.sh
+++ asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective.sh
@@ -2,19 +2,18 @@
 #BSUB -J run_oe_docking
 #BSUB -oo run_fragalysis_retrospective.out
 #BSUB -eo run_fragalysis_retrospective.stderr
-#BSUB -n 32
+#BSUB -n 72
 #BSUB -q cpuqueue
-#BSUB -R rusage[mem=16]
-#BSUB -W 01:00
+#BSUB -R rusage[mem=2]
+#BSUB -W 168:00
 source ~/.bashrc
 conda activate ad-3.9
 run-docking-oe \
--l /lila/data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/2022_12_02_fragalysis_correct_bond_orders_220_P_structures.sdf \
+-l /lila/data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/Mpro_combined_labeled.sdf \
 -r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/*/*_prepped_receptor_0.oedu' \
--o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230328/ \
--n 32 \
+-o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230330/ \
+-n 72 \
 --omega \
---relax clash \
---debug_num 32
+--relax clash
 echo Done
 date
