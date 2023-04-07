bdabd6af3b317d9e6c67a5b89b7f6a98d3281f88
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-04-07 12:19:43 -0400

    add file to test different logging configurations

---
+++ asapdiscovery-data/asapdiscovery/data/tests/run_logging.py
@@ -0,0 +1,30 @@
+from asapdiscovery.data.logging import FileLogger
+import logging
+from pathlib import Path
+
+
+def run_func(name):
+    logger = logging.getLogger(f"internal_logger.{name}")
+    logger.info("Default test")
+
+
+def internal_func(name: str):
+    file_logger = FileLogger(f"internal_logger.{name}", ".")
+    logger = file_logger.getLogger()
+    logger.info("Running run_func")
+    run_func(name)
+    with open(file_logger.logfile, "r") as f:
+        assert "Default test" in f.read()
+
+
+def main():
+    # logging.basicConfig(filename="different_test.txt", level=logging.DEBUG)
+    file_logger = FileLogger("top_logger", ".")
+    logger = file_logger.getLogger()
+    logger.info("Top level test")
+    internal_func("test1")
+    internal_func("test2")
+
+
+if __name__ == "__main__":
+    main()
