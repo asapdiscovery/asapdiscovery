d1530338fc60b18e542b1a66dda6e1ae7a177ce3
hmacdope <hugomacdermott@gmail.com>
2023-04-11 08:59:01 +1000

    remove args

--- asapdiscovery-docking/asapdiscovery/docking/tests/test_docking.py
+++ asapdiscovery-docking/asapdiscovery/docking/tests/test_docking.py
@@ -3,17 +3,15 @@ import os
 import subprocess
 
 
-
 def finalizer_function():
-    yield 
-    # clean up 
+    yield
+    # clean up
     shutil.rmtree("./outputs")
 
+
 @pytest.mark.timeout(200)
-@pytest.mark.script_launch_mode('subprocess')
+@pytest.mark.script_launch_mode("subprocess")
 def test_docking(script_runner):
     os.makedirs("./outputs", exist_ok=True)
-    ret = script_runner.run("run-docking-oe", "-l", "'./inputs/Mpro_combined_labeled.sdf", "-r", "'./inputs/Mpro-P0008_0A_ERI-UCB-ce40166b-17/*.oedu'", "-o", "'./outputs/retro_docking_test'", "-n", "2", "--omega")
+    ret = script_runner.run("run-docking-oe")
     assert ret.success
-
-
