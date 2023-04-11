5e8850d59c392a7fa85f5345f80ea564028d0e0a
hmacdope <hugomacdermott@gmail.com>
2023-04-11 13:01:55 +1000

    switch to pooch

--- asapdiscovery-ml/asapdiscovery/ml/weights/weights.py
+++ asapdiscovery-ml/asapdiscovery/ml/weights/weights.py
@@ -12,6 +12,9 @@ from asapdiscovery.data.hash_utils import compare_file_sha256_hashes
 ModelSpec = namedtuple("ModelSpec", ["name", "type", "weights", "config"])
 
 
+import pooch
+
+
 def fetch_model_from_spec(
     yamlfile: str,
     models: Union[list[str], str],
@@ -49,37 +52,49 @@ def fetch_model_from_spec(
     if isinstance(models, str):
         models = [models]
 
+    if not local_dir:
+        raise ValueError("local_dir must be provided and must not be falsy.")
+
+    if not Path(local_dir).exists():
+        Path(local_dir).mkdir(parents=True, exist_ok=True)
+
     specs = {}
-    # cannot specify the same weights file for multiple models
-    weights_set = set()
     for model in models:
         model_spec = spec[model]
         model_type = model_spec["type"]
+        base_url = model_spec["base_url"]
 
         weights = model_spec["weights"]
         weights_resource = weights["resource"]
         weights_hash = weights["sha256hash"]
+
+        registry = {}
+        registry[weights_resource] = f"sha256:{weights_hash}"
 
         # fetch config if provided
         if "config" in model_spec:
             config = model_spec["config"]
             config_resource = config["resource"]
             config_hash = config["sha256hash"]
-            config_file = fetch_file(config_resource, local_dir, force_fetch)
-            compare_file_sha256_hashes(config_file, config_hash)
-
+            registry[config_resource] = f"sha256:{config_hash}"
         else:
-            config_file = None
-        if weights_resource in weights_set:
-            raise ValueError(
-                f"Duplicate file {weights} in spec file. Please specify a unique filename for each model."
-            )
+            config_resource = None
+
+        # make pooch registry
+        subdir = model
+        registry = pooch.create(
+            path=Path(local_dir).joinpath(Path(subdir)),
+            base_url=base_url,
+            registry=registry,
+        )
 
         # fetch weights
-        weights_file = fetch_file(weights_resource, local_dir, force_fetch)
-        compare_file_sha256_hashes(weights_file, weights_hash)
-
-        weights_set.add(weights_resource)
+        weights_file = Path(registry.fetch(weights_resource))
+        # fetch config
+        if config_resource:
+            config_file = Path(registry.fetch(config_resource))
+        else:
+            config_file = None
         # make model spec
         specs[model] = ModelSpec(model, model_type, weights_file, config_file)
 
