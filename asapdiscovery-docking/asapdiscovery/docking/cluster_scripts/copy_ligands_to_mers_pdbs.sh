e13020da04cb62eabaa1db13fd329fac5fa3fcda
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-04-11 16:45:57 -0400

    fix script command typo in copy_ligands_to_mers_pdbs.sh

--- asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/copy_ligands_to_mers_pdbs.sh
+++ asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/copy_ligands_to_mers_pdbs.sh
@@ -8,7 +8,7 @@
 #BSUB -W 168:00
 source ~/.bashrc
 conda activate ad-3.9
-copy_ligands_to_new_structures.py \
+copy-ligands-to-new-structures \
 -l "/lila/data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/Mpro_combined_labeled.sdf" \
 -p '/lila/data/chodera/asap-datasets/current/mers_01_prepped_pdbs_v2/*/*_prepped_receptor_0.pdb' \
 -o "/lila/data/chodera/asap-datasets/mers_fauxalysis/20230411_prepped_for_fauxalysis" \
