c81696f43f4546f116ccae1ea68bb27a723a8ff8
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-04-11 17:27:09 -0400

    test not loading values in check_results to see if the issue is happening there

--- asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective.sh
+++ asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective.sh
@@ -4,14 +4,14 @@
 #BSUB -eo run_fragalysis_retrospective.stderr
 #BSUB -n 32
 #BSUB -q cpuqueue
-#BSUB -R rusage[mem=4]
+#BSUB -R rusage[mem=8]
 #BSUB -W 168:00
 source ~/.bashrc
 conda activate ad-3.9
 run-docking-oe \
 -l /lila/data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/Mpro_combined_labeled.sdf \
 -r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/*/*_prepped_receptor_0.oedu' \
--o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230330/ \
+-o /lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230411 \
 -n 32 \
 --omega \
 --relax clash
