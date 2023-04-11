f9e0c3eed8f311fa85ae00984a94dc89d5c581a4
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-04-11 17:10:49 -0400

    fix typo in protein glob string

--- asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/copy_ligands_to_mers_pdbs.sh
+++ asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/copy_ligands_to_mers_pdbs.sh
@@ -10,7 +10,7 @@ source ~/.bashrc
 conda activate ad-3.9
 copy-ligands-to-new-structures \
 -l "/lila/data/chodera/asap-datasets/mpro_fragalysis_2022_10_12/Mpro_combined_labeled.sdf" \
--p '/lila/data/chodera/asap-datasets/current/mers_01_prepped_pdbs_v2/*_prepped_receptor_0.pdb' \
+-p '/lila/data/chodera/asap-datasets/current/mers_01_prepped_pdbs_v2/*prepped_receptor_0.pdb' \
 -o "/lila/data/chodera/asap-datasets/mers_fauxalysis/20230411_prepped_for_fauxalysis" \
 -n 32 \
 --debug_num 32 \
