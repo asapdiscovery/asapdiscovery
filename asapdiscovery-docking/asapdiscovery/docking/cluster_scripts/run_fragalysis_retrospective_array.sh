419b22ed995c3f6b428be126bfce52a8aa5f8fab
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-04-12 13:28:25 -0400

    implement using different lognames for job array

--- asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective_array.sh
+++ asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective_array.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 ## Example usage:
-## bsub -J "run_fragalysis_retrospective[1-58]" < run_fragalysis_retrospective_array.sh
+## bsub -J "run_fragalysis_retrospective[1-576]" < run_fragalysis_retrospective_array.sh
 
 #BSUB -J run_oe_docking
 #BSUB -oo run_fragalysis_retrospective_%I.out
 #BSUB -eo run_fragalysis_retrospective_%I.stderr
 #BSUB -n 32
 #BSUB -q cpuqueue
-#BSUB -R rusage[mem=8]
+#BSUB -R rusage[mem=3]
 #BSUB -W 1:00
 source ~/.bashrc
 conda activate ad-3.9
@@ -17,7 +17,7 @@ run-docking-oe \
 -o '/lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230411' \
 -n 32 \
 --omega \
---debug_num 1 \
---relax clash
+--relax clash \
+-log "run_docking_oe."$LSB_JOBINDEX
 echo Done
 date
