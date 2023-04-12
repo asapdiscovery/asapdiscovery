bb8c1cf23d804470d88b530961aaeae2e258354b
hmacdope <hugomacdermott@gmail.com>
2023-04-12 15:38:33 +1000

    improve prep and cleanup again

--- asapdiscovery-docking/asapdiscovery/docking/tests/test_docking.py
+++ asapdiscovery-docking/asapdiscovery/docking/tests/test_docking.py
@@ -4,8 +4,11 @@ import subprocess
 
 import pytest
 
+
 @pytest.fixture()
-def output_dir_cleanup():
+def make_output_dir_and_cleanup():
+    # create output dir
+    os.makedirs("./outputs", exist_ok=True)
     yield
     # clean up
     shutil.rmtree("./outputs")
@@ -13,7 +16,15 @@ def output_dir_cleanup():
 
 @pytest.mark.timeout(200)
 @pytest.mark.script_launch_mode("subprocess")
-def test_docking(script_runner, output_dir_cleanup):
-    os.makedirs("./outputs", exist_ok=True)
-    ret = script_runner.run("run-docking-oe", "-l", "input/Mpro-P0008_0A_ERI-UCB-ce40166b-17.sdf", "-r", "'sars_01_prepped_v2/*/*prepped_receptor_0.oedu'", "-o", "./outputs")
+def test_docking(script_runner, make_output_dir_and_cleanup):
+    ret = script_runner.run(
+        "run-docking-oe",
+        "-l",
+        "input/Mpro-P0008_0A_ERI-UCB-ce40166b-17.sdf",
+        "-r",
+        "'sars_01_prepped_v2/*/*prepped_receptor_0.oedu'",
+        "-o",
+        "./outputs",
+    )
     assert ret.success
+    # can add other checks here
