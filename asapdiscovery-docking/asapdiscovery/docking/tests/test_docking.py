5396abcfa9e855122649f46abe40693601605176
hmacdope <hugomacdermott@gmail.com>
2023-04-12 15:36:28 +1000

    add proper cleanup

--- asapdiscovery-docking/asapdiscovery/docking/tests/test_docking.py
+++ asapdiscovery-docking/asapdiscovery/docking/tests/test_docking.py
@@ -4,8 +4,8 @@ import subprocess
 
 import pytest
 
-
-def finalizer_function():
+@pytest.fixture()
+def output_dir_cleanup():
     yield
     # clean up
     shutil.rmtree("./outputs")
@@ -13,7 +13,7 @@ def finalizer_function():
 
 @pytest.mark.timeout(200)
 @pytest.mark.script_launch_mode("subprocess")
-def test_docking(script_runner):
+def test_docking(script_runner, output_dir_cleanup):
     os.makedirs("./outputs", exist_ok=True)
-    ret = script_runner.run("run-docking-oe")
+    ret = script_runner.run("run-docking-oe", "-l", "input/Mpro-P0008_0A_ERI-UCB-ce40166b-17.sdf", "-r", "'sars_01_prepped_v2/*/*prepped_receptor_0.oedu'", "-o", "./outputs")
     assert ret.success
