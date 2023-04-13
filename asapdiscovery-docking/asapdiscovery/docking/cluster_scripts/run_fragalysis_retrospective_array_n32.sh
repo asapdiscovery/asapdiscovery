d75f63bc01ad4a10b02d4a67bf676e18b10d3066
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-04-13 16:07:44 -0400

    add some cluster test scripts

---
+++ asapdiscovery-docking/asapdiscovery/docking/cluster_scripts/run_fragalysis_retrospective_array_n32.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+## Example usage:
+## bsub -J "run_fragalysis_retrospective[1-576]" < run_fragalysis_retrospective_array.sh
+
+#BSUB -J run_oe_docking
+#BSUB -oo run_fragalysis_retrospective_%I.out
+#BSUB -eo run_fragalysis_retrospective_%I.stderr
+#BSUB -n 32
+#BSUB -q cpuqueue
+#BSUB -R rusage[mem=3]
+#BSUB -W 2:00
+source ~/.bashrc
+conda activate ad-3.9
+run-docking-oe \
+-l "/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/sdf_lsf_array/"$LSB_JOBINDEX".sdf" \
+-r '/lila/data/chodera/asap-datasets/current/sars_01_prepped_v3/*/*_prepped_receptor_0.oedu' \
+-o '/lila/data/chodera/asap-datasets/retro_docking/sars_fragalysis_retrospective/20230411_speed_test_n32' \
+-n 32 \
+--omega \
+--relax clash \
+-log "run_docking_oe."$LSB_JOBINDEX
+echo Done
+date
