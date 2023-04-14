e5a659769a02fe4aaf14aa32962d459fb5e3db52
hmacdope <hugomacdermott@gmail.com>
2023-04-14 16:20:55 +1000

    test trying to pull a bad file fails

--- asapdiscovery-data/asapdiscovery/data/tests/test_testing_resources.py
+++ asapdiscovery-data/asapdiscovery/data/tests/test_testing_resources.py
@@ -7,3 +7,9 @@ def test_get_file(file):
     path = fetch_test_file(file)
     assert path.exists()
     assert path.is_file()
+
+
+@pytest.mark.parametrize("file", ["file5.txt", "file6.txt", "subdir/file7.txt"])
+def test_get_fake_file(file):
+    with pytest.raises(ValueError, match="Could not fetch test file"):
+        path = fetch_test_file(file)
