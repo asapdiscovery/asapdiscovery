521fad0781759f8aa4a0c7035f0176a953a32ea7
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-04-25 16:22:12 -0400

    add more extensive options for splitting out the csv add options to explicitly include ligand / protein names

--- asapdiscovery-docking/asapdiscovery/docking/scripts/generate_docking_input_csv.py
+++ asapdiscovery-docking/asapdiscovery/docking/scripts/generate_docking_input_csv.py
@@ -29,14 +29,52 @@ def get_args():
         help="Path to output csv file.",
     )
     parser.add_argument(
+        "--protein_name",
+        type=str,
+        default=None,
+        help="Name of protein.",
+    )
+    parser.add_argument(
+        "--ligand_name",
+        type=str,
+        default=None,
+        help="Name of ligand.",
+    )
+    parser.add_argument(
         "--protein_regex",
         type=str,
+        default=None,
         help="Regex to extract protein name from file name.",
     )
     parser.add_argument(
         "--ligand_regex",
         type=str,
+        default=None,
         help="Regex to extract ligand name from file name.",
+    )
+    parser.add_argument(
+        "--split_by_ligand",
+        action="store_true",
+        default=False,
+        help="If true, split out csv by ligand.",
+    )
+    parser.add_argument(
+        "--split_by_protein",
+        action="store_true",
+        default=False,
+        help="If true, split out csv by protein.",
+    )
+    parser.add_argument(
+        "--split_by_n_rows",
+        type=int,
+        default=None,
+        help="Split out csv by number of rows.",
+    )
+    parser.add_argument(
+        "--split_by_n_files",
+        type=int,
+        default=None,
+        help="Split out csv into that many files.",
     )
     return parser.parse_args()
 
@@ -46,14 +84,54 @@ def main():
     du_fns = list(glob(args.glob_str))
     rows = []
     for du_fn in du_fns:
-        protein = re.search(args.protein_regex, du_fn).group(1)
-        ligand = re.search(args.ligand_regex, du_fn).group(1)
+        if args.protein_regex:
+            protein = re.search(args.protein_regex, du_fn).group(1)
+        elif args.protein_name:
+            protein = args.protein_name
+        else:
+            raise ValueError("Must provide either protein_regex or protein_name.")
+
+        if args.ligand_regex:
+            ligand = re.search(args.ligand_regex, du_fn).group(1)
+        elif args.ligand_name:
+            ligand = args.ligand_name
+        else:
+            raise ValueError("Must provide either ligand_regex or ligand_name.")
         complex_name = f"{protein}_{ligand}"
         rows.append([protein, ligand, complex_name, du_fn])
-    df = pd.DataFrame(rows, columns=["protein", "ligand", "complex", "du_fn"])
+
     if not args.output_csv.parent.exists():
         args.output_csv.parent.mkdir(parents=True)
+
+    df = pd.DataFrame(rows, columns=["protein", "ligand", "complex", "du_fn"])
     df.to_csv(args.output_csv, index=False)
+
+    if args.split_by_ligand:
+        for ligand, ligand_df in df.groupby("ligand"):
+            ligand_df.to_csv(
+                args.output_csv.parent / f"{ligand}_docking_input.csv", index=False
+            )
+    elif args.split_by_protein:
+        for protein, protein_df in df.groupby("protein"):
+            protein_df.to_csv(
+                args.output_csv.parent / f"{protein}_docking_input.csv", index=False
+            )
+    elif args.split_by_n_rows:
+        import math, numpy as np
+
+        for i, chunk in enumerate(
+            np.array_split(df, math.ceil(len(df) / args.split_by_n_rows))
+        ):
+            chunk.to_csv(
+                args.output_csv.parent / f"{i+1}_docking_input.csv", index=False
+            )
+    elif args.split_by_n_files:
+        import math, numpy as np
+
+        for i, chunk in enumerate(np.array_split(df, args.split_by_n_files)):
+            chunk.to_csv(
+                args.output_csv.parent / f"{i+1}_docking_input.csv", index=False
+            )
 
 
 if __name__ == "__main__":
--- asapdiscovery-docking/asapdiscovery/docking/tests/test_generate_docking_input_csv.sh
+++ asapdiscovery-docking/asapdiscovery/docking/tests/test_generate_docking_input_csv.sh
@@ -1,5 +1,9 @@
-python ../scripts/generate_self_docking_input_csv.py \
--g "/Users/alexpayne/Scientific_Projects/covid-moonshot-ml/asapdiscovery-docking/asapdiscovery/docking/tests/inputs/multireceptor_docking_test/*.oedu" \
--o ./outputs/test_generate_self_docking_input_csv/test_generate_self_docking_input.csv \
+rm -rf ./outputs/test_generate_docking_input_csv
+python ../scripts/generate_docking_input_csv.py \
+-g "/Users/alexpayne/Scientific_Projects/covid-moonshot-ml/asapdiscovery-docking/asapdiscovery/docking/tests/inputs/prepped_mers_receptors/*.oedu" \
+-o ./outputs/test_generate_docking_input_csv/test_generate_docking_input.csv \
 --protein_regex "rcsb_([A-Za-z0-9]{4})" \
---ligand_regex "\/([\w-]*)_rcsb" \
+--ligand_name "my_ligand" \
+--split_by_n_rows 2
+#--split_by_protein \
+#--ligand_regex "\/([\w-]*)_rcsb" \
