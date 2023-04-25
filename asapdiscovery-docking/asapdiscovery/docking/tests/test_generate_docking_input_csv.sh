3ac6a95da3d841ea56929dfcb1dd17d9e76fe956
Alexander Matthew Payne <alex.payne@choderalab.org>
2023-04-25 16:28:58 -0400

    put self docking input in its own directory

--- asapdiscovery-docking/asapdiscovery/docking/tests/test_generate_docking_input_csv.sh
+++ asapdiscovery-docking/asapdiscovery/docking/tests/test_generate_docking_input_csv.sh
@@ -8,7 +8,7 @@ rm -rf ./outputs/test_generate_docking_input_csv
 
 python ../scripts/generate_docking_input_csv.py \
 -g "/lila/data/chodera/asap-datasets/mers_fauxalysis/20230411_prepped_for_fauxalysis/*/*.oedu" \
--o '/lila/data/chodera/asap-datasets/mers_fauxalysis/20230411_prepped_for_fauxalysis/docking_input.csv' \
+-o '/lila/data/chodera/asap-datasets/mers_fauxalysis/20230411_prepped_for_fauxalysis/docking_input_csvs/docking_input.csv' \
 --protein_regex "rcsb_([A-Za-z0-9]{4})" \
 --ligand_regex "\/([\w-]*)_rcsb" \
 --split_by_ligand
