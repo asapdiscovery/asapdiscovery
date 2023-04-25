8249ad9910cc7938653c30d0da5369642ab0bfc7
kaminow <51923685+kaminow@users.noreply.github.com>
2023-04-25 07:13:30 -0400

    Migrate GAT model (#231)
    * Switch build_model to use mtenn GAT model.
    
    * Remove unused models.
    
    * Get rid of model_call.
    
    * Require new version of mtenn.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update inference to match new GAT model.
    
    * Update load_weights to be backwards compatible for old GAT model weights files.
    
    * More helpful errors.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: kaminow <benjamin.kaminow@choderalab.org>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>

--- asapdiscovery-ml/asapdiscovery/ml/__init__.py
+++ asapdiscovery-ml/asapdiscovery/ml/__init__.py
@@ -6,6 +6,5 @@ __version__ = version("asapdiscovery-ml")
 from .es import EarlyStopping  # noqa: F401
 from .inference import *  # noqa: F401,F403
 from .loss import *  # noqa: F401,F403
-from .models import *  # noqa: F401,F403
 from .pretrained_models import *  # noqa: F401,F403
 from .weights import *  # noqa: F401,F403
