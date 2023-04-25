9cdca8b47ead5bbb769c9d0572bf2d341dba8367
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-04-25 16:26:42 -0400

    add example for generating self-docking input

--- asapdiscovery-docking/asapdiscovery/docking/tests/test_generate_docking_input_csv.sh
+++ asapdiscovery-docking/asapdiscovery/docking/tests/test_generate_docking_input_csv.sh
@@ -1,9 +1,14 @@
 rm -rf ./outputs/test_generate_docking_input_csv
+#python ../scripts/generate_docking_input_csv.py \
+#-g "/Users/alexpayne/Scientific_Projects/covid-moonshot-ml/asapdiscovery-docking/asapdiscovery/docking/tests/inputs/prepped_mers_receptors/*.oedu" \
+#-o ./outputs/test_generate_docking_input_csv/test_generate_docking_input.csv \
+#--protein_regex "rcsb_([A-Za-z0-9]{4})" \
+#--ligand_name "my_ligand" \
+#--split_by_n_rows 2
+
 python ../scripts/generate_docking_input_csv.py \
--g "/Users/alexpayne/Scientific_Projects/covid-moonshot-ml/asapdiscovery-docking/asapdiscovery/docking/tests/inputs/prepped_mers_receptors/*.oedu" \
--o ./outputs/test_generate_docking_input_csv/test_generate_docking_input.csv \
+-g "/lila/data/chodera/asap-datasets/mers_fauxalysis/20230411_prepped_for_fauxalysis/*/*.oedu" \
+-o '/lila/data/chodera/asap-datasets/mers_fauxalysis/20230411_prepped_for_fauxalysis/docking_input.csv' \
 --protein_regex "rcsb_([A-Za-z0-9]{4})" \
---ligand_name "my_ligand" \
---split_by_n_rows 2
-#--split_by_protein \
-#--ligand_regex "\/([\w-]*)_rcsb" \
+--ligand_regex "\/([\w-]*)_rcsb" \
+--split_by_ligand
