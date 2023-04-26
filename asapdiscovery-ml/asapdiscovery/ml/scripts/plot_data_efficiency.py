7395f626df5b1e630493d771b2b5a08d10da0dc5
kaminow <benjamin.kaminow@choderalab.org>
2023-04-26 10:53:29 -0400

    Add functions for loading all losses and for plotting.

--- asapdiscovery-ml/asapdiscovery/ml/scripts/plot_data_efficiency.py
+++ asapdiscovery-ml/asapdiscovery/ml/scripts/plot_data_efficiency.py
@@ -25,12 +25,8 @@ def load_losses(loss_dir, conv_function=None):
 
     Returns
     -------
-    float
-        Train loss
-    float
-        Val loss
-    float
-        Test loss
+    List[float, float, float]
+        Train, val, test losses
     """
     p = Path(loss_dir)
     loss_arrays = {}
@@ -57,11 +53,70 @@ def load_losses(loss_dir, conv_function=None):
             sp: loss_arr.mean(axis=1) for sp, loss_arr in loss_arrays.items()
         }
 
-    return (
+    return [
         loss_arrays["train"][best_idx],
         loss_arrays["val"][best_idx],
         loss_arrays["test"][best_idx],
+    ]
+
+
+def load_all_losses(loss_dirs, labels, train_fracs, conv_function=None):
+    """
+    Load all train, val, and test losses, and build DataFrame.
+
+    Parameters
+    ----------
+    loss_dirs : List[str]
+        List of directories containing loss pickle files
+    labels : List[str]
+        List of labels for each dir (one to one)
+    train_fracs : List[float]
+        List of train fractions for each dir (one to one)
+    conv_function : callable, optional
+        If present, will use to convert mean absolute loss values
+
+    Returns
+    -------
+    pandas.DataFrame
+        DataFrame ready with losses, labels, and train fracs
+    """
+    # Parametrize load_losses function
+    load_losses_param = partial(load_losses, conv_function=conv_function)
+
+    df_rows = [
+        load_losses(d) + [l, train_frac]
+        for d, l, train_frac in zip(loss_dirs, labels, train_fracs)
+    ]
+    return pandas.DataFrame(
+        df_rows, columns=["train", "val", "test", "label", "train_frac"]
     )
+
+
+def plot_data_efficiency(plot_df, out_fn):
+    """
+    Plot loss as a function of data efficiency. Data points will be grouped by label,
+    with each split having a different line style.
+
+    Parameters
+    ----------
+    plot_df : pandas.DataFrame
+        DF ready for plotting. Should have the following columns:
+         * label
+         * train_frac
+         * split
+         * loss
+    out_fn : str
+        File to store plot
+    """
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    sns.lineplot(plot_df, x="train_frac", y="loss", hue="label", style="split", ax=ax)
+
+    # Set axes
+    ax.set_ylabel("Loss")
+    ax.set_xlabel("Fraction of Data in Training Split")
+
+    fig.savefig(out_fn, dpi=200, bbox_inches="tight")
 
 
 ################################################################################
@@ -87,6 +142,7 @@ def get_args():
         "-t",
         "--train_fracs",
         required=True,
+        type=float,
         nargs="+",
         help="List of training fractions (one for each loss dir).",
     )
@@ -132,7 +188,30 @@ def main():
     args = get_args()
 
     if len(args.loss_dirs) != len(args.labels):
-        raise ValueError("Incorrect number of labels")
+        raise ValueError("Incorrect number of labels.")
+    if len(args.loss_dirs) != len(args.train_fracs):
+        raise ValueError("Incorrect number of train fracs.")
+
+    if args.conv:
+        # Parametrized function
+        convert_pic50_param = partial(
+            convert_pic50, T=args.temp, cp_values=args.cheng_prusoff
+        )
+    else:
+        convert_pic50_param = None
+
+    # Build DF from losses
+    loss_df = load_all_losses(
+        args.loss_dirs, args.labels, args.train_fracs, convert_pic50_param
+    )
+
+    # Prepare for plotting
+    plot_df = loss_df.melt(
+        id_vars=["label", "train_frac"], var_name="split", value_name="loss"
+    )
+
+    # Plot
+    plot_data_efficiency(plot_df, args.out_fn)
 
 
 if __name__ == "__main__":
