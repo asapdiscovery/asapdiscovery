0df4dd6831b443ddf0b1ec6c37eb24cf8fc7085d
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-04-27 17:39:15 -0400

    move imports, add logging, and add default options in case sdf doesn't have data tags

--- asapdiscovery-data/asapdiscovery/data/scripts/sdf_to_smiles_list.py
+++ asapdiscovery-data/asapdiscovery/data/scripts/sdf_to_smiles_list.py
@@ -3,6 +3,9 @@ Load in a multi-ligand SDF file and write out a txt file with the format `SMILES
 Ligand name expected to be in the Compound_ID field of the sdf file.
 """
 from argparse import ArgumentParser
+from asapdiscovery.data.openeye import load_openeye_sdfs, oechem
+from asapdiscovery.data.logging import FileLogger
+from pathlib import Path
 
 
 def get_args():
@@ -11,21 +14,44 @@ def get_args():
         "-s",
         "--sdf_fn",
         required=True,
+        type=str,
         help="SDF file containing molecules of interest.",
     )
-    parser.add_argument("-o", "--output_fn", required=True, help="Output file.")
+    parser.add_argument(
+        "-o", "--output_fn", required=True, type=Path, help="Output file."
+    )
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    from asapdiscovery.data.openeye import load_openeye_sdfs, oechem
 
     mols = load_openeye_sdfs(args.sdf_fn)
-    output_lines = [
-        f"{oechem.OEGetSDData(mol, 'SMILES')} {oechem.OEGetSDData(mol, 'Compound_ID').rstrip()}\n"
-        for mol in mols
-    ]
+
+    logger = FileLogger(
+        logname="sdf_to_smiles_list", path=args.output_fn.stem
+    ).getLogger()
+
+    output_lines = []
+    for i, mol in enumerate(mols):
+
+        # Get the compound ID from the SD data if it exists, otherwise use the index
+        compound_id = oechem.OEGetSDData(mol, "Compound_ID").rstrip()
+        if not compound_id:
+            compound_id = f"compound_{i}"
+            logger.warning(
+                f"No Compound_ID found for '{compound_id}', using index instead."
+            )
+
+        # Get the SMILES string from the SD data if it exists, otherwise generate it
+        smiles = oechem.OEGetSDData(mol, "SMILES")
+        if not smiles:
+            logger.warning(
+                f"No SMILES string found for {compound_id}, generating one from molecule instead."
+            )
+            smiles = oechem.OEMolToSmiles(mol)
+
+        output_lines.append(f"{smiles} {compound_id}\n")
 
     with open(args.output_fn, "w") as f:
         for line in list(set(output_lines)):
