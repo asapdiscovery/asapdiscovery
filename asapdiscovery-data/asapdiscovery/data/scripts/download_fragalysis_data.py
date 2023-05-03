63c46ccc3831a25162f9d9cbaf13983e226afec7
kaminow <benjamin.kaminow@choderalab.org>
2023-05-03 13:33:01 -0400

    Add Mac1 option for download fragalysis script.

--- asapdiscovery-data/asapdiscovery/data/scripts/download_fragalysis_data.py
+++ asapdiscovery-data/asapdiscovery/data/scripts/download_fragalysis_data.py
@@ -1,11 +1,21 @@
 import argparse
 
-from asapdiscovery.data.fragalysis import download  # noqa: E402
+from asapdiscovery.data.fragalysis import (
+    download,
+    MAC1_API_CALL,
+    MPRO_API_CALL,
+)  # noqa: E402
 
 
 def get_args():
     parser = argparse.ArgumentParser(description="")
 
+    parser.add_argument(
+        "-t",
+        "--target",
+        required=True,
+        help="Which target to download. Options are [mpro, mac1].",
+    )
     parser.add_argument("-o", required=True, help="Output file name.")
     parser.add_argument(
         "-x", action="store_true", help="Extract file after downloading it."
@@ -17,7 +27,14 @@ def get_args():
 def main():
     args = get_args()
 
-    download(args.o, args.x)
+    if args.target.lower() == "mpro":
+        api_call = MPRO_API_CALL
+    elif args.target.lower() == "mac1":
+        api_call = MAC1_API_CALL
+    else:
+        raise ValueError(f"Unknown target: {args.target}")
+
+    download(args.o, api_call, args.x)
 
 
 if __name__ == "__main__":
