6b55a65a5402f7f8e677f0a41459e3aecfc80be1
Hugo MacDermott <hugomacdermott@gmail.com>
2023-05-09 12:58:28 -0400

    improve schema + validation

--- asapdiscovery-data/asapdiscovery/data/validation.py
+++ asapdiscovery-data/asapdiscovery/data/validation.py
@@ -1,5 +1,14 @@
 
 from asapdiscovery.data.openeye import oechem
+import warnings
+
+def read_file_as_str(filename):
+    with open(filename, "r") as f:
+        return f.read()
+
+def write_file_from_string(filename):
+    with open(filename, "w") as f:
+        f.write(string)
 
 def is_valid_smiles(smiles):
     # Create an OEMol object
@@ -14,3 +23,30 @@ def is_valid_smiles(smiles):
         return False
 
     return True
+
+def sdf_length():
+    if not Path(sdf_fn).exists():
+        raise FileNotFoundError(f"{sdf_fn} does not exist!")
+    ifs = oechem.oemolistream()
+    ifs.SetFlavor(
+        oechem.OEFormat_SDF,
+        oechem.OEIFlavor_SDF_Default,
+    )
+    if ifs.open(sdf_fn):
+        nmols = len(ifs.GetOEGraphMols())
+        return nmols
+    else:
+        oechem.OEThrow.Fatal(f"Unable to open {sdf_fn}")
+    
+def is_single_molecule_sdf(sdf_fn):
+    sdf_length = sdf_length(sdf_fn)
+    if sdf_length > 1:
+        return False
+    elif sdf_length == 0:
+        raise ValueError("SDF file must contain at least one molecule")
+    else:
+        return True
+
+def is_multiligand_sdf(sdf_fn):
+    return not is_single_molecule_sdf(sdf_fn)
+
