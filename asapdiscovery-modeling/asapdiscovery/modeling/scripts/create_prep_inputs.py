42821cc74c84369fa9167bfec5fb67db2f0b303b
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-06-03 18:47:12 -0400

    add option to create prep inputs from a CrystalCompoundDataset

--- asapdiscovery-modeling/asapdiscovery/modeling/scripts/create_prep_inputs.py
+++ asapdiscovery-modeling/asapdiscovery/modeling/scripts/create_prep_inputs.py
@@ -1,7 +1,11 @@
 import argparse
 from pathlib import Path
-from asapdiscovery.data.schema import CrystalCompoundData
+from asapdiscovery.data.utils import check_filelist_has_elements
+from asapdiscovery.data.schema import CrystalCompoundData, CrystalCompoundDataset
 from asapdiscovery.modeling.schema import PreppedTarget, PreppedTargets, MoleculeFilter
+
+# TODO: Add a function to check if the input file is a CrystalCompoundDataset
+# TODO: Probably want to move to requiring that the input is a CrystalCompoundDataset
 
 
 def get_args():
@@ -12,8 +16,15 @@ def get_args():
         "-d",
         "--structure_dir",
         type=Path,
-        required=True,
+        required=False,
         help="Path to downloaded input files",
+    )
+    parser.add_argument(
+        "-i",
+        "--input_file",
+        type=Path,
+        required=False,
+        help="Path to serialized CrystalCompoundDataset.",
     )
     parser.add_argument(
         "-o", "--output_dir", type=Path, required=True, help="Path to output_dir."
@@ -35,22 +46,55 @@ def get_args():
 
 def main():
     args = get_args()
+
+    if not "ligand" in args.components_to_keep and not args.active_site:
+        raise ValueError(
+            f"components_to_keep: {args.components_to_keep} do not include 'ligand' and no active_site provided.\n"
+            "Must provide OpenEye formatted active_site if not keeping ligand."
+        )
+
     args.output_dir.mkdir(exist_ok=True, parents=True)
-    protein_files = args.structure_dir.glob("*")
+    if args.input_file:
+        if args.input_file.suffix == ".csv":
+            xtals = CrystalCompoundDataset.from_csv(args.input_file)
+        elif args.input_file.suffix == ".pkl":
+            xtals = CrystalCompoundDataset.from_pkl(args.input_file)
+        else:
+            raise NotImplementedError
+
+    elif args.structure_dir:
+        protein_files = list(args.structure_dir.glob("*"))
+        check_filelist_has_elements(protein_files)
+        xtals = [
+            CrystalCompoundData(dataset=protein_file.stem, str_fn=str(protein_file))
+            for protein_file in protein_files
+            if protein_file.suffix == ".pdb" or protein_file.suffix == ".cif"
+        ]
+    else:
+        raise ValueError("Must provide either structure_dir or input_file.")
+
     targets = []
-    for protein_file in protein_files:
-        if protein_file.suffix == ".pdb" or protein_file.suffix == ".cif":
-            targets.append(
-                PreppedTarget(
-                    source=CrystalCompoundData(str_fn=str(protein_file)),
-                    output_name=protein_file.stem,
-                    active_site_chain=args.active_site_chain,
-                    active_site=args.active_site,
-                    molecule_filter=MoleculeFilter(
-                        components_to_keep=args.components_to_keep,
-                        ligand_chain=args.ligand_chain,
-                        protein_chains=args.protein_chains,
-                    ),
-                )
+    for xtal in xtals:
+
+        # TODO: Make this a bit more clever
+        output_tags = []
+        if xtal.dataset:
+            output_tags.append(xtal.dataset)
+        if xtal.compound_id:
+            output_tags.append(xtal.compound_id)
+        output_name = "_".join(output_tags)
+
+        targets.append(
+            PreppedTarget(
+                source=xtal,
+                output_name=output_name,
+                active_site_chain=args.active_site_chain,
+                active_site=args.active_site,
+                molecule_filter=MoleculeFilter(
+                    components_to_keep=args.components_to_keep,
+                    ligand_chain=args.ligand_chain,
+                    protein_chains=args.protein_chains,
+                ),
             )
+        )
     PreppedTargets.from_list(targets).to_pkl(args.output_dir / "to_prep.pkl")
