cf847e9550a2ff7eb2be69fcea5fc62352b04486
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-06-12 18:43:02 -0400

    change make du input to be pulled from aws

--- asapdiscovery-modeling/asapdiscovery/modeling/tests/test_protein_prep.py
+++ asapdiscovery-modeling/asapdiscovery/modeling/tests/test_protein_prep.py
@@ -60,6 +60,13 @@ def prep_dict(mers_target, sars_target):
         "sars": (sars_target, fetch_test_file("mpro_sars2_seqres.yaml")),
     }
 
+@pytest.fixture
+def spruced_protein_dict():
+    return {
+        "mers": fetch_test_file("mers_spruced.pdb"),
+        "sars": fetch_test_file("sars_spruced.pdb"),
+    }
+
 
 class TestProteinPrep:
     @pytest.mark.parametrize("target_name", ["mers", "sars"])
@@ -99,10 +106,9 @@ class TestProteinPrep:
         save_openeye_pdb(spruced, output_dir / f"{target_name}_spruced.pdb")
 
     @pytest.mark.parametrize("target_name", ["mers", "sars"])
-    def test_make_design_unit(self, target_name, prep_dict, output_dir):
+    def test_make_design_unit(self, target_name, prep_dict, output_dir, spruced_protein_dict):
         target, seqres_yaml = prep_dict[target_name]
-
-        mol = load_openeye_pdb(str(output_dir / f"{target_name}_spruced.pdb"))
+        mol = load_openeye_pdb(str(spruced_protein_dict[target_name]))
 
         # convert seqres to string
         with open(seqres_yaml) as f:
