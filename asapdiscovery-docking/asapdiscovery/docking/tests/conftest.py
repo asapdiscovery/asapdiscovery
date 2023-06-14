e4882bc6ae827ef85ffc03668e186bada92b73a3
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-06-13 21:23:41 -0400

    try to fix conftest error

--- asapdiscovery-docking/asapdiscovery/docking/tests/conftest.py
+++ asapdiscovery-docking/asapdiscovery/docking/tests/conftest.py
@@ -2,15 +2,6 @@ from pathlib import Path
 import pytest
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--local_path",
-        type=str,
-        default=None,
-        help="If provided, use this path to output files for tests",
-    )
-
-
 @pytest.fixture(scope="session")
 def local_path(request):
     return request.config.getoption("--local_path")
