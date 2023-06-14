3a032a329bdc38c11236576b6526c70403b55372
Alexander Matthew Payne <31800806+apayne97@users.noreply.github.com>
2023-06-14 08:00:34 -0400

    make different options for run_docking_oe posit methods possible (#314)
    * make different options for run_docking_oe posit methods possible
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * make posit methods their own test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * try to fix conftest error
    
    * Change docstring
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>

---
+++ asapdiscovery-docking/asapdiscovery/docking/tests/conftest.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def local_path(request):
+    return request.config.getoption("--local_path")
+
+
+# This needs to have a scope of session so that a new tmp file is not created for each test
+@pytest.fixture(scope="session")
+def output_dir(tmp_path_factory, local_path):
+    if not type(local_path) == str:
+        return tmp_path_factory.mktemp("test_prep")
+    else:
+        local_path = Path(local_path)
+        local_path.mkdir(exist_ok=True)
+        assert local_path.exists()
+        return local_path
