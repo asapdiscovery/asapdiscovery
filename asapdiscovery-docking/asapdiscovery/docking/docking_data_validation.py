8e0edd6414014dd827f29c1ff0f802ca055c6d35
hmacdope <hugomacdermott@gmail.com>
2023-06-23 15:07:33 +1000

    fix import of manifoldFilter

--- asapdiscovery-docking/asapdiscovery/docking/docking_data_validation.py
+++ asapdiscovery-docking/asapdiscovery/docking/docking_data_validation.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import List  # noqa: F401
 
-from asapdiscovery.data.postera.manifold_data_validation import ManifoldAllowedColumns
+from asapdiscovery.data.postera.manifold_data_validation import ManifoldAllowedColumns, ManifoldFilter
 
 
 class DockingResultCols(Enum):
