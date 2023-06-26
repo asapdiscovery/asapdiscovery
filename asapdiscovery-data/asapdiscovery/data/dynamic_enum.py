26122aca94b7927b0e984a75ee1ac36a2f376ad1
hmacdope <hugomacdermott@gmail.com>
2023-06-26 10:35:20 +1000

    fix up dynamic enum typing

--- asapdiscovery-data/asapdiscovery/data/dynamic_enum.py
+++ asapdiscovery-data/asapdiscovery/data/dynamic_enum.py
@@ -11,11 +11,14 @@ class DynamicEnum(Enum):
 def make_dynamic_enum(yaml_path: str | Path, enum_name: str) -> DynamicEnum:
     """Make a dynamic enum from a yaml file.
 
-    Args:
-        yaml_path (str | Path): Path to yaml file
+    Parameters:
+    ----------
+    yaml_path Union[str, Path]:
+        Path to yaml file
 
-    Returns:
-        Enum: Enum object
+    Returns
+    -------
+    DynamicEnum
     """
     with open(yaml_path) as f:
         data = yaml.safe_load(f)
--- asapdiscovery-data/asapdiscovery/data/schema_v2/target.py
+++ asapdiscovery-data/asapdiscovery/data/schema_v2/target.py
@@ -3,7 +3,13 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Any, Dict, Optional, Tuple, Union  # noqa: F401
 from enum import Enum
-from asapdiscovery.data.openeye import oechem
+from asapdiscovery.data.openeye import (
+    oechem,
+    oemol_to_pdb_string,
+    pdb_string_to_oemol,
+    oedu_to_pdb_string,
+    pdb_string_to_oedu,
+)
 from .dynamic_properties import TargetType
 from pydantic import Field
 
@@ -26,8 +32,9 @@ class TargetIdentifiers(DataModelAbstractBase):
     Identifiers for a Ligand
     """
 
-    target_type: Optional[DynamicEnum] = Field(
-        None, description="Dynamic Enum describing the target type e.g sars2 or mers"
+    target_type: Optional[TargetType] = Field(
+        None,
+        description="Dynamic Enum describing the target type e.g sars2, mers or mac1",
     )
 
     fragalysis_id: Optional[str] = Field(
@@ -64,7 +71,7 @@ class Target(DataModelAbstractBase):
 
     @classmethod
     def from_pdb(
-        cls, pdb_file: Union[str, Path], target_name: str = None, **kwargs
+        cls, pdb_file: Union[str, Path], target_name: str | None = None, **kwargs
     ) -> Target:
         # directly read in data
         pdb_str = read_file_directly(pdb_file)
@@ -75,7 +82,9 @@ class Target(DataModelAbstractBase):
         write_file_directly(filename, self.data)
 
     @classmethod
-    def from_oemol(cls, mol: oechem.OEMol, target_name: str = None, **kwargs) -> Target:
+    def from_oemol(
+        cls, mol: oechem.OEMol, target_name: str | None = None, **kwargs
+    ) -> Target:
         pdb_str = oemol_to_pdb_string(pdb_file)
         return cls(data=pdb_str, target_name=target_name, **kwargs)
 
@@ -83,7 +92,9 @@ class Target(DataModelAbstractBase):
         return pdb_string_to_oemol(self.data)
 
     @classmethod
-    def from_oedu(cls, du_file: Union[str, Path], compound_name: str = None) -> Target:
+    def from_oedu(
+        cls, du_file: Union[str, Path], target_name: str | None = None, **kwargs
+    ) -> Target:
         pdb_str = oedu_to_pdb_string(du_file)
         return cls(data=pdb_str, target_name=target_name, **kwargs)
 
