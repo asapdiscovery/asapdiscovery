6a2cf6dc0be1cd064f73002442fbb57a14bff99e
hmacdope <hugomacdermott@gmail.com>
2023-07-05 10:48:30 +1000

    make traj_to_viz.py

---
+++ asapdiscovery-dataviz/asapdiscovery/dataviz/scripts/traj_to_viz.py
@@ -0,0 +1,119 @@
+import argparse
+import logging
+import mdtraj as md
+
+from Pathlib import Path
+
+from asapdiscovery.dataviz.gif_viz import GIFVisualizer
+from asapdiscovery.dataviz.viz_targets import VizTargets
+from asapdiscovery.data.logging import FileLogger
+
+
+parser = argparse.ArgumentParser(description="Turn a trajectory into a GIF")
+
+parser.add_argument(
+    "--viz-target",
+    type=str,
+    required=True,
+    choices=VizTargets.get_allowed_targets(),
+    help="Target to write visualizations for, one of (sars2_mpro, mers_mpro, 7ene_mpro, 272_mpro, sars2_mac1)",
+)
+
+parser.add_argument(
+    "--traj",
+    type=str,
+    required=True,
+    help="Path to the trajectory to visualize",
+)
+
+parser.add_argument(
+    "--top",
+    type=str,
+    required=True,
+    help="Path to the topology file",
+)
+
+parser.add_argument(
+    "--out",
+    type=str,
+    required=True,
+    help="Path to the output gif file",
+)
+
+parser.add_argument(
+    "--start",
+    type=int,
+    default=0,
+    help="Frame to start visualization from",
+)
+
+parser.add_argument(
+    "--frames_per_ns",
+    type=int,
+    default=200,
+    help="Number of frames per ns, default matches the default output frequency for VanillaMDSimulator",
+)
+
+parser.add_argument(
+    "--smooth",
+    type=int,
+    default=5,
+    help="Number of frames to smooth over",
+)
+
+
+def main():
+
+    args = parser.parse_args()
+
+    # setup logging
+    logger_cls = FileLogger("traj_to_viz", path="./", stdout=True, level=logging.DEBUG)
+    logger = logger_cls.getLogger()
+    logger.info("Running GIF visualization")
+
+    # check all the required files exist
+    traj = Path(args.traj)
+    if not traj.exists():
+        raise FileNotFoundError(f"Trajectory file {traj} does not exist")
+    top = Path(args.top)
+    if not top.exists():
+        raise FileNotFoundError(f"Topology file {top} does not exist")
+    out = Path(args.out)
+
+    logger.info(f"Trajectory file: {traj}")
+    logger.info(f"Topology file: {top}")
+    logger.info(f"Output file: {out}")
+
+    logger.info("Loading trajectory")
+    traj = md.load(str(traj), top=str(top))
+    n_snapshots = traj.n_frames
+    logger.info(f"Loaded {n_snapshots} snapshots")
+
+    if not args.start:
+        # we want the last 100 snapshots
+        if n_snapshots < 100:
+            start = 1
+        else:
+            start = n_snapshots - 100
+    else:
+        start = args.start
+
+    logger.info(f"Starting from snapshot {start}")
+
+    gif_visualiser = GIFVisualizer(
+        [traj],
+        [args.top],
+        [out],
+        args.viz_target,
+        frames_per_ns=args.frames_per_ns,
+        smooth=args.smooth,
+        start=start,
+        logger=logger,
+    )
+    gif_visualiser.write_traj_visualizations()
+
+    logger.info("Done")
+
+
+if __name__ == "__main__":
+    main()
