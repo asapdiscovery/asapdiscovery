921a79005ef49160ec5745f8ebce33a17754086a
hmacdope <hugomacdermott@gmail.com>
2023-07-17 09:27:39 +1000

    add some docs

--- asapdiscovery-data/asapdiscovery/data/postera/manifold_artefacts.py
+++ asapdiscovery-data/asapdiscovery/data/postera/manifold_artefacts.py
@@ -13,7 +13,7 @@ from .manifold_data_validation import (
     rename_output_columns_for_manifold,
     map_output_col_to_manifold_tag,
 )
-from .molecule_set import MoleculeUpdateList, MoleculeSetAPI
+from .molecule_set import MoleculeSetAPI
 from ..aws.cloudfront import CloudFront
 from ..aws.s3 import S3
 
@@ -30,6 +30,12 @@ ARTIFACT_TYPE_TO_S3_CONTENT_TYPE = {
 
 
 class ManifoldArtifactUploader:
+    """
+    This class is used to upload artifacts to the Postera Manifold and AWS simultaneously, linking the two together
+    via CloudFront and the Manifold MoleculeSetAPI.
+
+    """
+
     def __init__(
         self,
         molecule_dataframe: pd.DataFrame,
@@ -41,8 +47,32 @@ class ManifoldArtifactUploader:
         target: str,
         artifact_column: str,
         bucket_name: str,
-        manifold_id_column: Optional[str] = DockingResultCols.LIGAND_ID.value,
+        manifold_id_column: str = DockingResultCols.LIGAND_ID.value,
     ):
+        """
+        Parameters
+        ----------
+        molecule_dataframe : pd.DataFrame
+            The dataframe containing the molecules to upload. Must contain a column with the name of the artifact
+        molecule_set_id : UUID
+            The UUID of the molecule set to upload to
+        artifact_type : ArtifactType
+            The type of artifact to upload
+        moleculeset_api : MoleculeSetAPI
+            The MoleculeSetAPI object to use to upload to Manifold
+        cloud_front : CloudFront
+            The CloudFront object to use to generate signed urls
+        s3 : S3
+            The S3 object to use to upload to S3
+        target : str
+            The target to upload to
+        artifact_column : str
+            The name of the column containing the artifact
+        bucket_name : str
+            The name of the bucket to upload to
+        manifold_id_column : str
+            The name of the column containing the manifold id
+        """
         self.molecule_dataframe = molecule_dataframe
         self.molecule_set_id = molecule_set_id
         self.artifact_type = artifact_type
@@ -62,11 +92,30 @@ class ManifoldArtifactUploader:
     def generate_cloudfront_url(
         self, bucket_path, expires_delta: timedelta = timedelta(days=365 * 5)
     ) -> str:
+        """
+        Generate a signed url for a given bucket path
+
+        Parameters
+        ----------
+        bucket_path : str
+            The path to the file in the bucket
+        expires_delta : timedelta
+            The timedelta for the signed url to be valid for
+
+        Returns
+        -------
+        str
+            The signed url for the file on S3
+        """
         # make a signed url with default timedelta of 5 years
         expiry = datetime.utcnow() + expires_delta
         return self.cloud_front.generate_signed_url(bucket_path, expiry)
 
     def upload_artifacts(self) -> None:
+        """
+        Upload the artifacts to Postera Manifold and S3
+        """
+
         # rename columns to match manifold
         output_tag_name = map_output_col_to_manifold_tag(ArtifactType, {}, self.target)[
             self.artifact_type.value
