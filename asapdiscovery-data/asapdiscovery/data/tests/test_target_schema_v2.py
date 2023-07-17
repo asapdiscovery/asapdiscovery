c627b938f69d85725f6cd89239b29cc44f081687
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-07-18 09:27:34 +1000

    Add Schema for Targets (#339)
    * trim out Target stuff for focused PR
    
    * add string overload for openeye loading in tests, remove later with #336
    
    * finish module reorganisation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix for hooks
    
    * add in INCHI and INCHIKey properties
    
    * remove smiles and inchi filed from LigandIdentifiers
    
    * add dynamic schema
    
    * reorganise modules
    
    * fix tests for module reorg
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix for hooks
    
    * add dynamic propeties
    
    * add in target type enums
    
    * fix up dynamic enum typing
    
    * add in target openeye methods
    
    * move INCHI stuff to openeye module
    
    * change slightly
    
    * add initial SD tag cycling
    
    * add sd tag printing
    
    * finally fix SD reading and writing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add sd tag printing
    
    * add dict get and set
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove bare except
    
    * temporarily remove
    
    * add stuff back in
    
    * remove import
    
    * add target_schema testing
    
    * add in data equal test
    
    * add new test file
    
    * update
    
    * change test file to be pre-processed
    
    * add roundtrip loader for PDB
    
    * add notes for testing
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove design unit filtering
    
    * add note on designunits
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix for hooks
    
    * move back to Union
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove future annotation
    
    * Revert "[pre-commit.ci] auto fixes from pre-commit.com hooks"
    
    This reverts commit 6cd72892aee3fe2a80038f3fa3c894e042d165c5.
    
    * add old type hints back in
    
    * add changed fields for ligands
    
    * add in tighter field checks and compchem UUID filed
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix for hooks
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix for hooks
    
    * redo typing and remove print from dynamicYAML
    
    * adding bettwer target decls
    
    * add in correct tests
    
    * add in equality testing that actually works
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove unnessecary rountrip method
    
    * add test for not enough IDs
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update asapdiscovery-data/asapdiscovery/data/schema_v2/target.py
    
    Co-authored-by: kaminow <51923685+kaminow@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: kaminow <51923685+kaminow@users.noreply.github.com>

---
+++ asapdiscovery-data/asapdiscovery/data/tests/test_target_schema_v2.py
@@ -0,0 +1,117 @@
+import pytest
+from asapdiscovery.data.schema_v2.dynamic_properties import TargetType
+from asapdiscovery.data.schema_v2.target import Target, TargetIdentifiers
+from asapdiscovery.data.testing.test_resources import fetch_test_file
+
+
+@pytest.fixture(scope="session")
+def moonshot_pdb():
+    pdb = fetch_test_file("Mpro-P2660_0A_bound.pdb")  # has a whole bunch of cruft in it
+    return pdb
+
+
+@pytest.fixture(scope="session")
+def moonshot_pdb_processed():
+    pdb = fetch_test_file(
+        "Mpro-P2660_0A_bound_oe_processed.pdb"
+    )  # already been processed with openeye
+    return pdb
+
+
+@pytest.fixture(scope="session")
+def sars2_spruced_pdb():
+    pdb = fetch_test_file("sars_spruced.pdb")
+    return pdb
+
+
+@pytest.mark.parametrize("ttype", ["sars2_mpro", "mers_mpro", "sars2_mac1"])
+def test_targettype_init(ttype):
+    tt = TargetType(ttype)
+    assert tt.value == ttype
+
+
+def test_targettype_init_bad_name():
+    with pytest.raises(ValueError):
+        _ = TargetType("bad_name")
+
+
+def test_target_from_pdb_at_least_one_id(moonshot_pdb):
+    with pytest.raises(ValueError):
+        # neither id is set
+        Target.from_pdb(moonshot_pdb)
+
+
+def test_target_from_pdb_at_least_one_target_id(moonshot_pdb):
+    with pytest.raises(ValueError):
+        # neither id is set
+        Target.from_pdb(moonshot_pdb, ids=TargetIdentifiers())
+
+
+@pytest.mark.parametrize("ttype", ["sars2_mpro", "mers_mpro", "sars2_mac1"])
+def test_target_identifiers(ttype):
+    ids = TargetIdentifiers(target_type=ttype, fragalysis_id="blah", pdb_code="blah")
+    assert ids.target_type == TargetType(ttype)
+    assert ids.fragalysis_id == "blah"
+    assert ids.pdb_code == "blah"
+
+
+@pytest.mark.parametrize("pdb_code", ["ABCD", None])
+@pytest.mark.parametrize("fragalysis_id", ["Mpro-P2660", None])
+@pytest.mark.parametrize("ttype", ["sars2_mpro", "mers_mpro", "sars2_mac1"])
+@pytest.mark.parametrize("target_name", ["test_name"])
+def test_target_dict_roundtrip(
+    moonshot_pdb, target_name, ttype, fragalysis_id, pdb_code
+):
+    t1 = Target.from_pdb(
+        moonshot_pdb,
+        target_name,
+        ids=TargetIdentifiers(
+            target_type=ttype, fragalysis_id=fragalysis_id, pdb_code=pdb_code
+        ),
+    )
+    t2 = Target.from_dict(t1.dict())
+    assert t1 == t2
+
+
+@pytest.mark.parametrize("pdb_code", ["ABCD", None])
+@pytest.mark.parametrize("fragalysis_id", ["Mpro-P2660", None])
+@pytest.mark.parametrize("ttype", ["sars2_mpro", "mers_mpro", "sars2_mac1"])
+@pytest.mark.parametrize("target_name", ["test_name"])
+def test_target_json_roundtrip(
+    moonshot_pdb, target_name, ttype, fragalysis_id, pdb_code
+):
+    t1 = Target.from_pdb(
+        moonshot_pdb,
+        target_name,
+        ids=TargetIdentifiers(
+            target_type=ttype, fragalysis_id=fragalysis_id, pdb_code=pdb_code
+        ),
+    )
+    t2 = Target.from_json(t1.json())
+    assert t1 == t2
+
+
+def test_target_data_equal(moonshot_pdb):
+    t1 = Target.from_pdb(moonshot_pdb, "TargetTestName")
+    t2 = Target.from_pdb(moonshot_pdb, "TargetTestName")
+    # does the same thing as the __eq__ method
+    assert t1.data_equal(t2)
+    assert t1 == t2
+
+
+def test_oemol_roundtrip(
+    moonshot_pdb_processed,
+):  # test that pre-processed pdb files can be read in and out consistently
+    t1 = Target.from_pdb(moonshot_pdb_processed, "TargetTestName")
+    mol = t1.to_oemol()
+    t2 = Target.from_oemol(mol, "TargetTestName")
+    assert t1 == t2
+
+
+def test_oemol_roundtrip_sars2(
+    sars2_spruced_pdb,
+):  # test that a pdb file can be read in and out consistently via roundtrip through openeye
+    t1 = Target.from_pdb(sars2_spruced_pdb, "TargetTestName")
+    mol = t1.to_oemol()
+    t2 = Target.from_oemol(mol, "TargetTestName")
+    assert t1 == t2
