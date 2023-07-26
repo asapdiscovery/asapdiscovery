b98fafabaf0f586da1539b67fd79e7434dcf59b8
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-07-26 18:17:17 +1000

    Add s3 and postera upload of artifacts.  (#375)
    * add start of szybki
    
    * improve szybki interface
    
    * final touches
    
    * fix dask output
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add in documentation
    
    * fix szybki output
    
    * fix output
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix again
    
    * hooray get szybki upload to work
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add proper start to manifold artefact
    
    * add in proper S3 push hoooooray
    
    * move the column renaming to data validation API
    
    * re-jig
    
    * add to single-target
    
    * almost there
    
    * finally working
    
    * finally fix everything up
    
    * add debug statement
    
    * update utils
    
    * add some docs
    
    * add gif upload
    
    * keep in needed column
    
    * fix merge issues
    
    * add white backgroun
    
    * Revert "add white backgroun"
    
    This reverts commit 162628cac3e50f95f3baa39e5e995262c7400f0a.
    
    * Revert "Revert "add white backgroun""
    
    This reverts commit d6303a56d234cd52a2d54c3560bd6cccbb466a6e.
    
    * add zoom to ligand back
    
    * fix for hooks
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Updates from review with @hmacdope
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix up arte vs arti fact
    
    * cloud_front -> cloudfront
    
    * make dataframe copy
    
    * docstring fixes
    
    * fold in renaming function
    
    * small fixes and rename ARTEFACT -> ARTIFACT
    
    * revert issue with GIF views
    
    * fix dataframe ops in workflow
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix for hooks
    
    * only drop if present
    
    * add in comment
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: David Dotson <dotsdl@gmail.com>

--- asapdiscovery-dataviz/asapdiscovery/dataviz/gif_viz.py
+++ asapdiscovery-dataviz/asapdiscovery/dataviz/gif_viz.py
@@ -375,10 +375,12 @@ def add_gif_progress_bar(
 
         # load the image.
         img = Image.open(filename)
-        draw = ImageDraw.Draw(img, "RGBA")
+        img2 = Image.new("RGBA", img.size, "WHITE")
+        img2.paste(img, mask=img)
+        draw = ImageDraw.Draw(img2, "RGBA")
 
         # get its dimensions (need these for coords); calculate progress bar width at this frame.
-        width, height = img.size
+        width, height = img2.size
         bar_width = frame_num / total_frames * width
 
         # draw the progress bar for this frame (black, fully opaque).
@@ -397,4 +399,4 @@ def add_gif_progress_bar(
         )  # align to RHS; this way if value increases it will grow into frame.
 
         # save the image.
-        img.save(filename)
+        img2.save(filename)
