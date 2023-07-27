d32a3a8edfeac03840449eb4635f563edc9b3efe
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-07-27 12:30:44 +1000

    Fix GIF and HTML angles + rework VizTargets + spruce up dataviz modules.  (#380)
    * add PSE dump
    
    * add in proper alignment to first minimized pose by re-aligning
    
    * make proper staticmethod
    
    * add in gif view for new ASAP structures
    
    * add pose to viz script
    
    * add viz target updates
    
    * update blocks and runners
    
    * partial fix of poses and viz module
    
    * re-indent
    
    * update NGL viewer version in HTML renderer
    
    * fix docs
    
    * add in proper docked view block for new MAC1 structures
    
    * add in visualisation for new MAC1 structures
    
    * add example of how to generate the angle
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix gif membership test
    
    * add in tests
    
    * test gif render
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix testing of docking run
    
    * move html getters to inside block class
    
    * change gif blocks logic also
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ asapdiscovery-dataviz/asapdiscovery/dataviz/scripts/pose_to_viz.py
@@ -0,0 +1,76 @@
+import argparse
+import logging
+from pathlib import Path
+
+from asapdiscovery.data.logging import FileLogger
+from asapdiscovery.dataviz.html_viz import HTMLVisualizer
+from asapdiscovery.dataviz.viz_targets import VizTargets
+
+parser = argparse.ArgumentParser(description="Turn a trajectory into a GIF")
+
+parser.add_argument(
+    "--viz-target",
+    type=str,
+    required=True,
+    choices=VizTargets.get_allowed_targets(),
+    help="Target to write visualizations for, one of (sars2_mpro, mers_mpro, 7ene_mpro, 272_mpro, sars2_mac1)",
+)
+
+parser.add_argument(
+    "--pose",
+    type=str,
+    required=True,
+    help="Path to the pose SDF file",
+)
+
+parser.add_argument(
+    "--protein",
+    type=str,
+    required=True,
+    help="Path to the protein file",
+)
+
+parser.add_argument(
+    "--out",
+    type=str,
+    required=True,
+    help="Path to the output HTML file",
+)
+
+
+def main():
+    args = parser.parse_args()
+
+    # setup logging
+    logger_cls = FileLogger("pose_to_viz", path="./", stdout=True, level=logging.DEBUG)
+    logger = logger_cls.getLogger()
+    logger.info("Running HTML visualization")
+
+    # check all the required files exist
+    pose = Path(args.pose)
+    if not pose.exists():
+        raise FileNotFoundError(f"Pose file {pose} does not exist")
+    protein = Path(args.protein)
+    if not protein.exists():
+        raise FileNotFoundError(f"Topology file {protein} does not exist")
+
+    out = Path(args.out)
+
+    logger.info(f"Pose file: {pose}")
+    logger.info(f"Protein file: {protein}")
+    logger.info(f"Output file: {out}")
+
+    html_visualizer = HTMLVisualizer(
+        poses=[str(pose)],
+        output_paths=[out],
+        target=args.viz_target,
+        protein=protein,
+        logger=logger,
+    )
+    html_visualizer.write_pose_visualizations()
+
+    logger.info("Done")
+
+
+if __name__ == "__main__":
+    main()
