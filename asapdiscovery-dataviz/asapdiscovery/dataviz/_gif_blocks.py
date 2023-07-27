95e2633cd49e48f8a3ae63d6531eb049c65b88c0
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-07-27 12:30:44 +1000

    Fix GIF and HTML angles + rework VizTargets + spruce up dataviz modules.  (#380)
    * add PSE dump
    
    * add in proper alignment to first minimized pose by re-aligning
    
    * make proper staticmethod
    
    * add in gif view for new ASAP structures
    
    * add pose to viz script
    
    * add viz target updates
    
    * update blocks and runners
    
    * partial fix of poses and viz module
    
    * re-indent
    
    * update NGL viewer version in HTML renderer
    
    * fix docs
    
    * add in proper docked view block for new MAC1 structures
    
    * add in visualisation for new MAC1 structures
    
    * add example of how to generate the angle
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix gif membership test
    
    * add in tests
    
    * test gif render
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix testing of docking run
    
    * move html getters to inside block class
    
    * change gif blocks logic also
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- asapdiscovery-dataviz/asapdiscovery/dataviz/_gif_blocks.py
+++ asapdiscovery-dataviz/asapdiscovery/dataviz/_gif_blocks.py
@@ -1,155 +1,197 @@
-# find these by going through GIF generation with `pse_share=True`, then inspecting
-# session_5.pse with pymol, orienting and running `get_view` in pymol terminal.
-# SARS2
-view_coords_sars2_mpro = (
-    -0.339127213,
-    -0.370405823,
-    -0.864748597,
-    0.307130545,
-    -0.912446380,
-    0.270390421,
-    -0.889192164,
-    -0.173893914,
-    0.423199117,
-    0.000072375,
-    0.000199302,
-    -80.242187500,
-    6.715160370,
-    92.460678101,
-    114.338867188,
-    -458.494476318,
-    618.980895996,
-    -20.000000000,
-)
+from .viz_targets import VizTargets
 
-# MERS
-view_coords_mers_mpro = (
-    -0.635950804,
-    -0.283323288,
-    -0.717838645,
-    -0.040723491,
-    -0.916550398,
-    0.397835642,
-    -0.770651817,
-    0.282238036,
-    0.571343124,
-    0.000061535,
-    0.000038342,
-    -58.079559326,
-    8.052228928,
-    0.619271040,
-    21.864795685,
-    -283.040344238,
-    399.190032959,
-    -20.000000000,
-)
-
-# 7ENE
-view_coords_7ene_mpro = (
-    0.710110664,
-    0.317291290,
-    -0.628544748,
-    -0.485409290,
-    -0.426031262,
-    -0.763462067,
-    -0.510019004,
-    0.847245276,
-    -0.148514912,
-    0.000047840,
-    0.000042381,
-    -58.146995544,
-    -1.610392451,
-    -9.301478386,
-    57.779785156,
-    13.662354469,
-    102.618484497,
-    -20.000000000,
-)
+"""
+Find these by going through GIF generation with `pse_share=True`, then inspecting
+session_5.pse with pymol, orienting and running `get_view` in pymol terminal.
+"""
 
 
-# 272
-view_coords_272_mpro = (
-    0.877147615,
-    0.019472033,
-    -0.479821414,
-    -0.101128228,
-    -0.969278216,
-    -0.224204898,
-    -0.469446748,
-    0.245185584,
-    -0.848235607,
-    0.000049642,
-    0.000095099,
-    -100.664413452,
-    6.686100960,
-    18.334394455,
-    41.254760742,
-    -59353.101562500,
-    59554.460937500,
-    -20.000000000,
-)
+class GIFBlockData:
+    @classmethod
+    def get_view_coords(cls, target):
+        target_ = VizTargets.get_name_underscore(target)
+        return getattr(cls, f"view_coords_{target_}")
 
-# mac1
-view_coords_sars2_mac1 = (
-    0.058132507,
-    -0.604144037,
-    -0.794748724,
-    -0.915980995,
-    0.284294397,
-    -0.283112556,
-    0.396988422,
-    0.744434655,
-    -0.536857843,
-    0.000011945,
-    -0.000066146,
-    -87.475425720,
-    13.680676460,
-    25.153524399,
-    -0.173997879,
-    -1329.627441406,
-    1504.635498047,
-    -20.000000000,
-)
+    @classmethod
+    def get_pocket_dict(cls, target):
+        target_name = VizTargets.get_target_name(target, underscore=True)
+        return getattr(cls, f"pocket_dict_{target_name}")
 
-# set colorings of subpockets by resn. This may change over time.,
-# first define the residues that span the subpockets
-# SARS2
-pocket_dict_sars2_mpro = {
-    "subP1": "140-145+163+172",
-    "subP1_prime": "25-27",
-    "subP2": "41+49+54",
-    "subP3_4_5": "165-168+189-192",
-    "sars_unique": "25+49+142+164+168+169+181+186+188+190+191",
-}
+    @classmethod
+    def get_color_dict(cls, target):
+        protein_name = VizTargets.get_protein_name(target, underscore=True)
+        return getattr(cls, f"color_dict_{protein_name}")
 
-# MERS
-pocket_dict_mers_mpro = {
-    "subP1": "143+144+145+146+147+148+166+175",
-    "subP1_prime": "25+26+27",
-    "subP2": "41+49+54",
-    "subP3_4_5": "168+169+170+171+192+193+194+195",
-    "sars_unique": "25+49+145+167+171+172+184+189+191+193+194",
-}
+    view_coords_SARS_CoV_2_Mpro = (
+        -0.339127213,
+        -0.370405823,
+        -0.864748597,
+        0.307130545,
+        -0.912446380,
+        0.270390421,
+        -0.889192164,
+        -0.173893914,
+        0.423199117,
+        0.000072375,
+        0.000199302,
+        -80.242187500,
+        6.715160370,
+        92.460678101,
+        114.338867188,
+        -458.494476318,
+        618.980895996,
+        -20.000000000,
+    )
 
-# MAC1
-pocket_dict_sars2_mac1 = {
-    "nucleotide": "154+156+22+23+24+52+49+125",
-    "bridge": "126+155",
-    "phosphate": "46+47+48+38+39+40+130+131+132+127+128+97",
-    "anion_hole": "129+157+160+136+164",
-}
+    # MERS
+    view_coords_MERS_CoV_Mpro = (
+        -0.635950804,
+        -0.283323288,
+        -0.717838645,
+        -0.040723491,
+        -0.916550398,
+        0.397835642,
+        -0.770651817,
+        0.282238036,
+        0.571343124,
+        0.000061535,
+        0.000038342,
+        -58.079559326,
+        8.052228928,
+        0.619271040,
+        21.864795685,
+        -283.040344238,
+        399.190032959,
+        -20.000000000,
+    )
 
-# now define the colors per subpocket
-color_dict_mpro = {
-    "subP1": "yellow",
-    "subP1_prime": "orange",
-    "subP2": "skyblue",
-    "subP3_4_5": "aquamarine",
-}
+    # 7ENE
+    view_coords_MERS_CoV_Mpro_7ene = (
+        0.710110664,
+        0.317291290,
+        -0.628544748,
+        -0.485409290,
+        -0.426031262,
+        -0.763462067,
+        -0.510019004,
+        0.847245276,
+        -0.148514912,
+        0.000047840,
+        0.000042381,
+        -58.146995544,
+        -1.610392451,
+        -9.301478386,
+        57.779785156,
+        13.662354469,
+        102.618484497,
+        -20.000000000,
+    )
 
-color_dict_mac1 = {
-    "nucleotide": "paleyellow",
-    "bridge": "darksalmon",
-    "phosphate": "brightorange",
-    "anion_hole": "slate",
-}
+    # 272
+    view_coords_MERS_CoV_Mpro_272 = (
+        0.781453907,
+        0.495069802,
+        -0.379777491,
+        0.348743975,
+        -0.851257741,
+        -0.392085701,
+        -0.517399728,
+        0.173951998,
+        -0.837877095,
+        0.000047162,
+        0.000063360,
+        -106.999412537,
+        6.953200340,
+        11.497907639,
+        42.910934448,
+        -49050.539062500,
+        49264.585937500,
+        -20.000000000,
+    )
+
+    # mac1
+    view_coords_SARS_CoV_2_Mac1 = (
+        0.199152365,
+        -0.560011566,
+        -0.804189026,
+        -0.606427014,
+        0.574200273,
+        -0.550032735,
+        0.769795358,
+        0.597223163,
+        -0.225250959,
+        0.000011945,
+        -0.000066146,
+        -74.581398010,
+        13.680676460,
+        25.153524399,
+        -0.173997879,
+        -2000.212524414,
+        2149.432861328,
+        -20.000000000,
+    )
+
+    # mac1 monomers from 07-19 release
+    view_coords_SARS_CoV_2_Mac1_monomer = (
+        0.088525243,
+        0.968614519,
+        0.232240111,
+        -0.460261583,
+        0.246554896,
+        -0.852860034,
+        -0.883353055,
+        -0.031390585,
+        0.467653960,
+        0.000071935,
+        -0.000032585,
+        -91.646080017,
+        2.006669521,
+        -8.887639046,
+        14.542367935,
+        64.035736084,
+        119.277297974,
+        -20.000000000,
+    )
+
+    # set colorings of subpockets by resn. This may change over time.,
+    # first define the residues that span the subpockets
+    # SARS2
+    pocket_dict_SARS_CoV_2_Mpro = {
+        "subP1": "140-145+163+172",
+        "subP1_prime": "25-27",
+        "subP2": "41+49+54",
+        "subP3_4_5": "165-168+189-192",
+        "sars_unique": "25+49+142+164+168+169+181+186+188+190+191",
+    }
+
+    # MERS
+    pocket_dict_MERS_CoV_Mpro = {
+        "subP1": "143+144+145+146+147+148+166+175",
+        "subP1_prime": "25+26+27",
+        "subP2": "41+49+54",
+        "subP3_4_5": "168+169+170+171+192+193+194+195",
+        "sars_unique": "25+49+145+167+171+172+184+189+191+193+194",
+    }
+
+    # MAC1
+    pocket_dict_SARS_CoV_2_Mac1 = {
+        "nucleotide": "154+156+22+23+24+52+49+125",
+        "bridge": "126+155",
+        "phosphate": "46+47+48+38+39+40+130+131+132+127+128+97",
+        "anion_hole": "129+157+160+136+164",
+    }
+
+    # now define the colors per subpocket
+    color_dict_Mpro = {
+        "subP1": "yellow",
+        "subP1_prime": "orange",
+        "subP2": "skyblue",
+        "subP3_4_5": "aquamarine",
+    }
+
+    color_dict_Mac1 = {
+        "nucleotide": "paleyellow",
+        "bridge": "darksalmon",
+        "phosphate": "brightorange",
+        "anion_hole": "slate",
+    }
--- asapdiscovery-dataviz/asapdiscovery/dataviz/_html_blocks.py
+++ asapdiscovery-dataviz/asapdiscovery/dataviz/_html_blocks.py
@@ -1,19 +1,22 @@
-# Description: HTML blocks for the visualisations in asapdiscovery/dataviz/html_vis.py
+from .viz_targets import VizTargets
+
+"""
+HTML blocks for visualising poses.
+
+The most important part of this is getting the correct orientation of the pose with the "orient_tail" block.
+To get the values for this run the following in a jupyter notbook with nglview installed:
+
+import nglview as nv
+view = nglview.show_structure_file("<>.pdb")
+
+# orient the view as you want it
+
+view._camera_orientation
+
+# copy the output and paste it into the relevant orient_tail block below as the value for the Matrix4.fromArray() call.
 
 
-visualisation_header = """\
-<div id="viewport"
-role="NGL"
-data-proteins='[{
-   "type": "data", "value": "mike_combined", "isVariable": true, "loadFx": "loadmike_combined"
-}]'
-data-backgroundcolor="white"></div>
-<script type="text/javascript">document.getElementById("blue").style.color = "blue";</script>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"
-              integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-              crossorigin="anonymous" type="text/javascript"></script>
-<script src="https://unpkg.com/ngl@2.0.0-dev.34/dist/ngl.js" type="text/javascript"></script>
-<script src="https://michelanglo.sgc.ox.ac.uk/michelanglo.js" type="text/javascript"></script>
+TODO: Make these load from a YAML file.
 """
 
 
@@ -33,7 +36,49 @@ def _vis_core(pdb_body: str) -> str:
     return core
 
 
-visualisation_tail = """\
+def make_core_html(pdb_body: str) -> str:
+    return (
+        HTMLBlockData.visualisation_header
+        + _vis_core(pdb_body)
+        + HTMLBlockData.visualisation_tail
+    )
+
+
+class HTMLBlockData:
+    @classmethod
+    def get_pocket_color(cls, target: str) -> str:
+        """
+        Get the color for the pocket of a target.
+        """
+        # need protein name
+        protein_name = VizTargets.get_target_name(target, underscore=True)
+        return getattr(cls, f"colour_{protein_name}")
+
+    @classmethod
+    def get_orient_tail(cls, target: str) -> str:
+        """
+        Get the orient tail for a target.
+        """
+        # need underscore full name
+        target_ = VizTargets.get_name_underscore(target)
+        return getattr(cls, f"orient_tail_{target_}")
+
+    visualisation_header = """\
+<div id="viewport"
+role="NGL"
+data-proteins='[{
+   "type": "data", "value": "mike_combined", "isVariable": true, "loadFx": "loadmike_combined"
+}]'
+data-backgroundcolor="white"></div>
+<script type="text/javascript">document.getElementById("blue").style.color = "blue";</script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+              integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+              crossorigin="anonymous" type="text/javascript"></script>
+<script src="https://unpkg.com/ngl@2.1.1/dist/ngl.js" type="text/javascript"></script>
+<script src="https://michelanglo.sgc.ox.ac.uk/michelanglo.js" type="text/javascript"></script>
+"""
+
+    visualisation_tail = """\
 <script type="text/javascript">
 
 
@@ -68,12 +113,7 @@ function loadmike_combined (protein) {
 
 """
 
-
-def make_core_html(pdb_body: str) -> str:
-    return visualisation_header + _vis_core(pdb_body) + visualisation_tail
-
-
-orient_tail_7ene_mpro = """\
+    orient_tail_MERS_CoV_Mpro_7ene = """\
     //orient
     stage.viewerControls.orient((new NGL.Matrix4).fromArray([-47.411552767443936, 18.160442129079684, 45.864598141236, 0.0, 33.734451290915786, -34.476765704662284, 48.52362967346835, 0.0, 35.99080588204288, 56.238428207759625, 14.936708193081358, 0.0, 1.14170241355896, 9.264076232910156, -58.5212287902832, 1.0]));
     stage.setParameters({ cameraFov: 20.0, fogNear: 45.0}); //clipFar: 44.402000427246094, clipNear: -44.39164352416992
@@ -83,8 +123,7 @@ orient_tail_7ene_mpro = """\
 
 </script> """
 
-
-orient_tail_mers_mpro = """\
+    orient_tail_MERS_CoV_Mpro = """\
     //orient
     stage.viewerControls.orient((new NGL.Matrix4).fromArray([27.892651485688475, -17.496836682191315, 27.842153246574526, 0.0, 4.645958152833828, -34.046145151871315, -26.049992638068943, 0.0, 32.55357881447708, 19.850633421900966, -20.137968094041753, 0.0, -9.070898056030273, 0.7458584308624268, -23.088354110717773, 1.0]));
     stage.setParameters({ cameraFov: 20.0, fogNear: 45.0}); //clipFar: 67.24873352050781, clipNear: -67.247802734375
@@ -94,8 +133,7 @@ orient_tail_mers_mpro = """\
 
 </script> """
 
-
-orient_tail_sars2_mpro = """\
+    orient_tail_SARS_CoV_2_Mpro = """\
     //orient
     stage.viewerControls.orient((new NGL.Matrix4).fromArray([32.700534186404866, -23.332587195321253, 28.280957904108163, 0.0, 2.9287225438600046, -36.11192126726678, -33.179842577844965, 0.0, 36.54640101226278, 23.77111378788311, -22.645942287727394, 0.0, -9.279577255249023, 1.1916427612304688, -23.425792694091797, 1.0]));
     stage.setParameters({ cameraFov: 20.0, fogNear: 45.0}); //clipFar: 28.732975006103516, clipNear: -28.735034942626953
@@ -105,9 +143,7 @@ orient_tail_sars2_mpro = """\
 
 </script> """
 
-
-# TODO: change to proper name once it has one
-orient_tail_272_mpro = """\
+    orient_tail_MERS_CoV_Mpro_272 = """\
     //orient
     stage.viewerControls.orient((new NGL.Matrix4).fromArray([-55.43715359724729, 19.154542778222776, 24.429636441852836, 0.0, -26.507044110580864, -55.2321834848874, -16.845374934225674, 0.0, 16.158030311277685, -24.889572286518273, 56.18196582121527, 0.0, 0.7490043640136719, 0.8194751739501953, -22.965221405029297, 1.0]));
     stage.setParameters({ cameraFov: 20.0, fogNear: 45.0}); //clipFar: 51.28336715698242, clipNear: -51.29174613952637
@@ -117,7 +153,7 @@ orient_tail_272_mpro = """\
 
 </script> """
 
-orient_tail_sars2_mac1 = """\
+    orient_tail_SARS_CoV_2_Mac1 = """\
     //orient
     stage.viewerControls.orient((new NGL.Matrix4).fromArray([-26.865335727331967, -33.96353780439949, 76.20488333720914, 0.0, 68.40799043278685, 36.8623095955993, 40.54563400173538, 0.0, -47.76036465635116, 71.90319471856219, 15.208982898829163, 0.0, -10.974757194519043, -20.811683654785156, 0.7359411716461182, 1.0]));
     stage.setParameters({ cameraFov: 20.0, fogNear: 45.0}); //clipFar: 308.4343490600586, clipNear: -308.38341522216797
@@ -126,7 +162,16 @@ orient_tail_sars2_mac1 = """\
 
 </script> """
 
-colour_mers_mpro = """\
+    orient_tail_SARS_CoV_2_Mac1_monomer = """\
+    //orient
+    stage.viewerControls.orient((new NGL.Matrix4).fromArray([-23.219745442858454, 55.838006663099, -28.902925722905522, 0, 40.964266876161005, 36.80909548255832, 38.20259167828506, 0, 47.69896165275294, -4.430144865672009, -46.878583122804734, 0, 1.8491380487657036, 13.741344709971262, -10.428263568518318, 1]));
+    stage.setParameters({ cameraFov: 20.0, fogNear: 45.0}); //clipFar: 308.4343490600586, clipNear: -308.38341522216797
+}
+
+
+</script> """
+
+    colour_MERS_CoV_Mpro = """\
         // Define the binding pocket.
         const data = {
         'color_dict': {
@@ -165,8 +210,7 @@ colour_mers_mpro = """\
 
 """
 
-
-colour_sars2_mpro = """\
+    colour_SARS_CoV_2_Mpro = """\
         // Define the binding pocket.
         const data = {
         'color_dict': {
@@ -205,9 +249,7 @@ colour_sars2_mpro = """\
 
 """
 
-colour_7ene_mpro = colour_sars2_mpro
-
-colour_sars2_mac1 = """\
+    colour_SARS_CoV_2_Mac1 = """\
         // Define the binding pocket.
         const data = {
         'color_dict': {
