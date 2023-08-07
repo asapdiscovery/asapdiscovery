183505c871900ac6e72b3779d114aa78b0e5c88c
Hugo MacDermott <hugomacdermott@gmail.com>
2023-08-07 11:56:24 +1000

    add testing of dask factory functions

---
+++ asapdiscovery-data/asapdiscovery/data/dask_utils.py
@@ -0,0 +1,77 @@
+from dask.distributed import Client, LocalCluster
+from dask_jobqueue import LSFCluster
+from pydantic import BaseModel, Field
+from typing import Optional
+from .execution_utils import guess_network_interface
+from enum import Enum
+from dask import config as cfg
+
+cfg.set({"distributed.scheduler.worker-ttl": None})
+cfg.set({"distributed.admin.tick.limit": "2h"})
+
+
+class GPU(str, Enum):
+    GTX1080TI = "GTX1080TI"
+
+_LILAC_GPU_GROUPS = {
+    GPU.GTX1080TI : "lt-gpu",
+}
+
+_LILAC_GPU_EXTRAS = {
+    GPU.GTX1080TI: ['-R "select[hname!=lt16]"'], # random node that has something wrong with its GPU
+}
+
+class DaskCluster(BaseModel):
+    class Config:
+        allow_mutation = False
+        extra = "forbid"
+
+    name: str = Field("dask-worker", description="Name of the dask worker")
+    cores: int = Field(1, description="Number of cores per job")
+    memory: str = Field("20 GB", description="Amount of memory per job")
+    death_timeout: int = Field(120, description="Timeout for a worker to be considered dead")
+
+    
+
+class LilacDaskCluster(DaskCluster):
+
+    shebang: str = Field("#!/usr/bin/env bash", description="Shebang for the job")
+    queue: str = Field("cpuqueue", description="LSF queue to submit jobs to")
+    project: str = Field(None, description="LSF project to submit jobs to")
+    walltime: str = Field("1:00", description="Walltime for the job")
+    use_stdin: bool = Field(True, description="Whether to use stdin for job submission")
+    job_extra_directives: Optional[list[str]] = Field(None, description="Extra directives to pass to LSF")
+
+    def to_cluster(self, exclude_interface: Optional[str]='lo'):
+        interface = guess_network_interface(exclude=[exclude_interface])
+        return LSFCluster(
+                interface=interface,
+                scheduler_options={"interface": interface},
+                **self.dict()
+            )
+
+
+class LilacGPUConfig(BaseModel):
+    gpu: GPU = Field(..., description="GPU type")
+    gpu_group: str = Field(..., description="GPU group")
+    extra: Optional[list[str]] = Field(None, description="Extra directives to pass to LSF")
+
+    def to_job_extra_directives(self):
+        return ['-gpu num=1:j_exclusive=yes:mode=shared', f'-m {self.gpu_group}', *self.extra]
+    
+    @classmethod
+    def from_gpu(cls, gpu: GPU):
+        return cls(gpu=gpu, gpu_group=_LILAC_GPU_GROUPS[gpu], extra=_LILAC_GPU_EXTRAS[gpu])
+
+
+
+
+class LilacGPUDaskCluster(LilacDaskCluster):
+    queue: str = "gpuqueue"
+    walltime = "24:00"
+    memory = "48 GB"
+
+    @classmethod
+    def from_gpu(cls, gpu: GPU=GPU.GTX1080TI):
+        gpu_config = LilacGPUConfig.from_gpu(gpu)
+        return cls(job_extra_directives=gpu_config.to_job_extra_directives())
---
+++ asapdiscovery-data/asapdiscovery/data/tests/test_dask_utils.py
@@ -0,0 +1,27 @@
+from asapdiscovery.data.dask_utils import LilacDaskCluster, LilacGPUDaskCluster
+
+def test_LilacDaskCluster():
+    opts = LilacDaskCluster()
+    cluster = opts.to_cluster()
+    js = cluster.job_script()
+    assert "dask-worker" in js
+
+    
+
+def test_LilacGPUDDaskCluster():
+    opts = LilacGPUDaskCluster()
+    cluster = opts.to_cluster()
+    js = cluster.job_script()
+    assert "dask-worker" in js
+    assert "gpuqueue" in js
+
+def test_LilacGPUDaskCluster_from_gpu():
+    opts = LilacGPUDaskCluster.from_gpu("GTX1080TI")
+    cluster = opts.to_cluster()
+    js = cluster.job_script()
+    assert "dask-worker" in js
+    assert "gpuqueue" in js
+    assert "lt-gpu" in js
+    assert '-R "select[hname!=lt16]"' in js
+
+
