182831ba3c839691b8c768c093c5e633bde9c53b
Hugo MacDermott <hugomacdermott@gmail.com>
2023-08-07 19:45:11 +1000

    add in metadata

---
+++ asapdiscovery-data/asapdiscovery/data/metadata/__init__.py
@@ -0,0 +1,1 @@
+from .resources import MERS_CoV_Mpro_SEQRES, SARS_CoV_2_Mpro_SEQRES, SARS_CoV_2_Mac1_SEQRES
---
+++ asapdiscovery-data/asapdiscovery/data/tests/test_metadata.py
@@ -0,0 +1,8 @@
+import os
+from asapdiscovery.data.metadata import MERS_CoV_Mpro_SEQRES, SARS_CoV_2_Mpro_SEQRES, SARS_CoV_2_Mac1_SEQRES
+
+def test_seqres():
+    assert os.path.exists(MERS_CoV_Mpro_SEQRES)
+    assert os.path.exists(SARS_CoV_2_Mpro_SEQRES)
+    assert os.path.exists(SARS_CoV_2_Mac1_SEQRES)
+
