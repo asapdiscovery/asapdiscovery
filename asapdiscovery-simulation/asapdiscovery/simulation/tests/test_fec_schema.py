79cbf59738224f902f72df732080159beae0632b
Josh Horton <Josh.Horton@newcastle.ac.uk>
2023-08-17 11:52:16 +0100

    rename tests to avoid duplicate names

--- asapdiscovery-simulation/asapdiscovery/simulation/tests/test_fec_schema.py
+++ asapdiscovery-simulation/asapdiscovery/simulation/tests/test_fec_schema.py
@@ -1,6 +1,8 @@
 import openfe
 import pytest
 from alchemiscale import Scope, ScopedKey
+from openff.units import unit as OFFUnit
+
 from asapdiscovery.simulation.schema.atom_mapping import (
     KartografAtomMapper,
     LomapAtomMapper,
@@ -13,7 +15,6 @@ from asapdiscovery.simulation.schema.fec import (
     TransformationResult,
 )
 from asapdiscovery.simulation.schema.network import NetworkPlanner
-from openff.units import unit as OFFUnit
 
 
 @pytest.mark.parametrize(
