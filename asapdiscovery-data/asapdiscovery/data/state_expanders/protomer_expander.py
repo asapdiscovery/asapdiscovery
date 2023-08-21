0a49af121d802bb7a4d04585941d05ec489caca7
hmacdope <hugomacdermott@gmail.com>
2023-08-21 13:16:14 +1000

    add protomer expander

---
+++ asapdiscovery-data/asapdiscovery/data/state_expanders/protomer_expander.py
@@ -0,0 +1,42 @@
+from typing import Literal
+
+from asapdiscovery.data.openeye import oechem, oequacpac
+from asapdiscovery.data.schema_v2.ligand import Ligand
+from asapdiscovery.data.state_expanders.state_expander import StateExpanderBase
+from pydantic import Field
+
+
+class ProtomerExpander(StateExpanderBase):
+    """
+    Expand a molecule to protomers
+    """
+
+    expander_type: Literal["StereoExpander"] = "StereoExpander"
+    stereo_expand_defined: bool = Field(
+        False,
+        description="Expand stereochemistry at defined centers as well as undefined centers",
+    )
+
+    def provenance(self) -> dict[str, str]:
+        return {
+            "expander": self.expander_type,
+            "oechem": oechem.OEChemGetVersion(),
+            "quacpac": oechem.OEQuacPacGetVersion(),
+        }
+
+    def _expand(self, ligands: list[Ligand]) -> list[Ligand]:
+       
+        expanded_states = []
+
+        for parent_ligand in ligands:
+            oemol = parent_ligand.to_oemol()
+            parent_ligand.make_parent_tag(provenance=self.provenance())
+            for protomer in oequacpac.OEGetReasonableProtomers(oemol):
+                fmol = oechem.OEMol(protomer)
+                # copy the ligand properties over to the new molecule, we may want to have more fine grained control over this
+                # down the track.
+                protomer_ligand = Ligand.from_oemol(fmol, **parent_ligand.dict())
+                protomer_ligand.set_parent(parent_ligand, provenance=self.provenance())
+                expanded_states.append(protomer_ligand)
+
+        return expanded_states
