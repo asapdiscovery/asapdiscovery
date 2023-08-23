3cd0e390e69aa50bcb9b42db40abdf3e4298db9d
Jenke Scheen <jenke.scheen@choderalab.org>
2023-08-23 15:36:03 +0200

    add mac1 and sars2 mpro views

--- asapdiscovery-dataviz/asapdiscovery/dataviz/_html_blocks.py
+++ asapdiscovery-dataviz/asapdiscovery/dataviz/_html_blocks.py
@@ -82,6 +82,31 @@ class HTMLBlockData:
             return f"""
         protein.addRepresentation( 'surface', {{color: 'blue', sele: '{missing_res_formatted}', opacity: 1, side: 'front', surfaceType: 'av', probeRadius: 4.0, scaleFactor: 2.0}} );
         """
+    @classmethod
+    def get_orient(cls, target: str) -> str:
+        """
+        Get the orient array for a target.
+        """
+        target_ = VizTargets.get_name_underscore(target)
+        return getattr(cls, f"HTML_orient_{target_}")
+           
+    HTML_orient_SARS_CoV_2_Mpro = """\
+    [19.43872410226519, 11.81781888855718, 13.596857815858694, 71.59651614705442, 0.8265984029795093, 0.2984942958173893, 0.4405725231221419, 0.18311768736524137]
+    """
+
+    HTML_orient_SARS_CoV_2_Mac1 = """\
+    [
+    -3.5200155623997147, 
+    -5.050560643099713, 
+    -12.108040862949323, 
+    71.69598666461106, 
+    0.3887976484684321, 
+    0.42408332663180826, 
+    -0.5833075994459622, 
+    -0.5733602402041021
+    ]
+    """
+
 
     color_method_subpockets = """\
         protein.removeAllRepresentations();
