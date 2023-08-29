51ee8ef6ece13902362560da97f9179ef6ed09cf
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-08-30 09:18:45 +1000

    Add selectors to sort ligands against complexes naievely or by MCS, protein prep and Pair schema. (#445)
    * start of selectors
    
    * test properly
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix for hooks
    
    * add comment
    
    * change n_select name
    
    * WIP
    
    * continue exploration
    
    * improvements
    
    * fix preservation of chain info in complex
    
    * verify expected output
    
    * finalse selection tests
    
    * add some docs
    
    * rename again to make neater
    
    * add in baseclasses
    
    * properly use pairs
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove unused import
    
    * add pair testing
    
    * fix var names
    
    * Update asapdiscovery-data/asapdiscovery/data/selectors/mcs_selector.py
    
    Co-authored-by: kaminow <51923685+kaminow@users.noreply.github.com>
    
    * Update asapdiscovery-data/asapdiscovery/data/tests/test_selectors.py
    
    Co-authored-by: kaminow <51923685+kaminow@users.noreply.github.com>
    
    * Update asapdiscovery-modeling/asapdiscovery/modeling/protein_prep_v2.py
    
    Co-authored-by: kaminow <51923685+kaminow@users.noreply.github.com>
    
    * Update asapdiscovery-modeling/asapdiscovery/modeling/protein_prep_v2.py
    
    Co-authored-by: kaminow <51923685+kaminow@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add check for complex types
    
    * add spruce ver also
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: kaminow <51923685+kaminow@users.noreply.github.com>

--- asapdiscovery-data/asapdiscovery/data/schema_v2/complex.py
+++ asapdiscovery-data/asapdiscovery/data/schema_v2/complex.py
@@ -3,22 +3,38 @@ from __future__ import annotations
 from pathlib import Path
 from typing import Any
 
-from asapdiscovery.data.openeye import load_openeye_pdb
+from asapdiscovery.data.openeye import combine_protein_ligand, load_openeye_pdb
 from asapdiscovery.data.schema_v2.ligand import Ligand
 from asapdiscovery.data.schema_v2.schema_base import DataModelAbstractBase
-from asapdiscovery.data.schema_v2.target import Target
+from asapdiscovery.data.schema_v2.target import PreppedTarget, Target
 from asapdiscovery.modeling.modeling import split_openeye_mol
+from asapdiscovery.modeling.protein_prep_v2 import ProteinPrepper
 from asapdiscovery.modeling.schema import MoleculeFilter
 from pydantic import Field
 
 
-class Complex(DataModelAbstractBase):
+class ComplexBase(DataModelAbstractBase):
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, ComplexBase):
+            return NotImplemented
+
+        # Just check that both Targets and Ligands are the same
+        return (self.target == other.target) and (self.ligand == other.ligand)
+
+    def __ne__(self, other: Any) -> bool:
+        return not self.__eq__(other)
+
+
+class Complex(ComplexBase):
     """
     Schema for a Complex, containing both a Target and Ligand
+    In this case the Target field is required to be protein only
+
     """
 
     target: Target = Field(description="Target schema object")
     ligand: Ligand = Field(description="Ligand schema object")
+    ligand_chain: str = Field(..., description="Chain ID of ligand in complex")
 
     # Overload from base class to check target and ligand individually
     def data_equal(self, other: Complex):
@@ -34,7 +50,7 @@ class Complex(DataModelAbstractBase):
         ligand_chain="",
         target_kwargs={},
         ligand_kwargs={},
-    ):
+    ) -> Complex:
         # First load full complex molecule
         complex_mol = load_openeye_pdb(pdb_file)
 
@@ -42,20 +58,67 @@ class Complex(DataModelAbstractBase):
         mol_filter = MoleculeFilter(
             protein_chains=target_chains, ligand_chain=ligand_chain
         )
-        split_dict = split_openeye_mol(complex_mol, mol_filter)
+        split_dict = split_openeye_mol(complex_mol, mol_filter, keep_one_lig=True)
 
         # Create Target and Ligand objects
         target = Target.from_oemol(split_dict["prot"], **target_kwargs)
         ligand = Ligand.from_oemol(split_dict["lig"], **ligand_kwargs)
 
-        return cls(target=target, ligand=ligand)
+        return cls(
+            target=target, ligand=ligand, ligand_chain=split_dict["keep_lig_chain"]
+        )
 
-    def __eq__(self, other: Any) -> bool:
-        if not isinstance(other, Complex):
-            return NotImplemented
+    def to_combined_oemol(self):
+        """
+        Combine the target and ligand into a single oemol
+        """
+        return combine_protein_ligand(
+            self.target.to_oemol(), self.ligand.to_oemol(), lig_chain=self.ligand_chain
+        )
 
-        # Just check that both Targets and Ligands are the same
-        return (self.target == other.target) and (self.ligand == other.ligand)
 
-    def __ne__(self, other: Any) -> bool:
-        return not self.__eq__(other)
+class PreppedComplex(ComplexBase):
+    """
+    Schema for a Complex, containing both a PreppedTarget and Ligand
+    In this case the PreppedTarget contains the protein and ligand.
+    """
+
+    target: PreppedTarget = Field(description="PreppedTarget schema object")
+    ligand: Ligand = Field(description="Ligand schema object")
+
+    # Overload from base class to check target and ligand individually
+    def data_equal(self, other: PreppedComplex):
+        return self.target.data_equal(other.target) and self.ligand.data_equal(
+            other.ligand
+        )
+
+    @classmethod
+    def from_complex(cls, complex: Complex, prep_kwargs={}) -> PreppedComplex:
+        """
+        Create a PreppedComplex from a Complex by running ProteinPrepper
+        on the combined oemol of the complex
+
+        Parameters
+        ----------
+        complex : Complex
+            Complex to create PreppedComplex from
+        prep_kwargs : dict
+            Keyword arguments to pass to ProteinPrepper
+
+        Returns
+        -------
+        PreppedComplex
+            PreppedComplex object
+        """
+        # overwrite ligand_chain with ligand_chain from complex if it exists
+        prep_kwargs.pop("ligand_chain", None)
+        prep_kwargs["ligand_chain"] = complex.ligand_chain
+        oedu = ProteinPrepper(**prep_kwargs).prep(complex.to_combined_oemol())
+        # copy over ids from complex
+        prepped_target = PreppedTarget.from_oedu(
+            oedu,
+            ids=complex.target.ids,
+            target_name=complex.target.target_name,
+            ligand_chain=complex.ligand_chain,
+        )
+        return cls(target=prepped_target, ligand=complex.ligand)
--- asapdiscovery-modeling/asapdiscovery/modeling/modeling.py
+++ asapdiscovery-modeling/asapdiscovery/modeling/modeling.py
@@ -303,7 +303,7 @@ def spruce_protein(
         build_loops_success and build_sidechains_success and place_hydrogens_success
     )
     # Re-percieve residues so that atom number and connect records dont get screwed up
-    initial_prot = openeye_perceive_residues(initial_prot, preserve_all=True)
+    initial_prot = openeye_perceive_residues(initial_prot, preserve_all=False)
     return success, spruce_error_msg, initial_prot
 
 
@@ -676,19 +676,30 @@ def split_openeye_mol(
     # Get rid of any straggling extra copies of the ligand
     prot_mol = trim_small_chains(prot_mol, prot_cutoff_len)
 
+    if molecule_filter.ligand_chain:
+        keep_lig_chain = molecule_filter.ligand_chain
+    else:
+        keep_lig_chain = None
     # Get rid of extra copies of the ligand
     if keep_one_lig:
         all_lig_chains = [res.GetChainID() for res in oechem.OEGetResidues(lig_mol)]
         # Handle the case where the input has no ligand, otherwise throws an IndexError
         if len(all_lig_chains) > 0:
             # Keep first alphabetically, for reproducibility
-            keep_lig_chain = sorted(all_lig_chains)[0]
+            if not keep_lig_chain:
+                keep_lig_chain = sorted(all_lig_chains)[0]
             for a in lig_mol.GetAtoms():
                 # Delete all atoms that don't match
                 if oechem.OEAtomGetResidue(a).GetChainID() != keep_lig_chain:
                     lig_mol.DeleteAtom(a)
 
-    return {"prot": prot_mol, "lig": lig_mol, "wat": water_mol, "oth": oth_mol}
+    return {
+        "prot": prot_mol,
+        "lig": lig_mol,
+        "wat": water_mol,
+        "oth": oth_mol,
+        "keep_lig_chain": keep_lig_chain,
+    }
 
 
 def save_design_unit(
