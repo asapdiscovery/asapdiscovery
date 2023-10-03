fb2f3750f996ee28f0343b14ed127e96c64612ee
hmacdope <hugomacdermott@gmail.com>
2023-10-03 12:23:54 +1100

    rework logging test to be less insanely useless

--- asapdiscovery-data/asapdiscovery/data/tests/test_logging.py
+++ asapdiscovery-data/asapdiscovery/data/tests/test_logging.py
@@ -5,38 +5,25 @@ from asapdiscovery.data.logging import FileLogger
 
 
 def run_func(name):
-    logger = logging.getLogger(f"internal_logger.{name}")
+    logger = logging.getLogger(name)
     logger.info("Default test")
 
 
-@pytest.fixture()
-def file_logger():
-    file_logger = FileLogger("top_logger", ".", level=logging.INFO)
+def test_toplevel(tmp_path):
+    file_logger = FileLogger("top_logger", tmp_path, level=logging.INFO)
     logger = file_logger.getLogger()
     logger.info("Top level test")
-    return file_logger
-
-
-@pytest.mark.parametrize("name", ["test1", "test2"])
-def test_toplevel(file_logger, name: str):
-    with open(file_logger.logfile) as f:
+    with open(tmp_path / file_logger.logfile) as f:
         fread = f.read()
         assert "Top level test" in fread
 
 
-@pytest.mark.parametrize("name", ["test1", "test2"])
-def test_set_as_default(file_logger, name: str):
-    file_logger.set_as_default()
-    assert file_logger
-
-
-@pytest.mark.parametrize("name", ["test1", "test2"])
-def test_internal_func(file_logger, name: str):
-    file_logger = FileLogger(f"internal_logger.{name}", ".", level=logging.INFO)
+def test_internal_func(tmp_path):
+    file_logger = FileLogger("top_logger", tmp_path, level=logging.INFO)
     logger = file_logger.getLogger()
-    logger.info("Running run_func")
-    run_func(name)
-    with open(file_logger.logfile) as f:
+    logger.info("Top level test")
+    run_func(file_logger.name)
+    with open(tmp_path / file_logger.logfile) as f:
         fread = f.read()
-        assert "Top level test" not in fread
+        assert "Top level test" in fread
         assert "Default test" in fread
