e1e63268dd27d948471876a9268c3a4d3a2d02fe
hmacdope <hugomacdermott@gmail.com>
2023-10-04 08:33:56 +1100

    Revert "remove cache file for GraphInferenceDataset"
    This reverts commit 73822e81a91d3e8199e87a11a2c5488abc36ea03.

--- asapdiscovery-ml/asapdiscovery/ml/dataset.py
+++ asapdiscovery-ml/asapdiscovery/ml/dataset.py
@@ -509,7 +509,7 @@ class GraphInferenceDataset(Dataset):
         exp_compounds,
         node_featurizer=None,
         edge_featurizer=None,
-        cache_file=None,
+        cache_file="./cache.bin",
     ):
         """
         Parameters
