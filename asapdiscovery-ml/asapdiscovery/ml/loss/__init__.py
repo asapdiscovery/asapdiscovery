12f32a857fbdde95b7b39c338bab0193c6547975
kaminow <benjamin.kaminow@choderalab.org>
2023-10-13 14:49:02 -0400

    Remove unneeded GroupedMSELoss.

--- asapdiscovery-ml/asapdiscovery/ml/loss/__init__.py
+++ asapdiscovery-ml/asapdiscovery/ml/loss/__init__.py
@@ -1,5 +1,4 @@
 from .GaussianNLLLoss import GaussianNLLLoss
-from .GroupedMSELoss import GroupedMSELoss
 from .MSELoss import MSELoss
 
-__all__ = ["MSELoss", "GroupedMSELoss", "GaussianNLLLoss"]
+__all__ = ["MSELoss", "GaussianNLLLoss"]
