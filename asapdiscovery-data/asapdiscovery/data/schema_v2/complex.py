ee1c29761b9ed8b9e78e290a44eb65639663627c
kaminow <benjamin.kaminow@choderalab.org>
2023-10-18 09:45:06 -0400

    Remove ligand mol manipulation.

--- asapdiscovery-data/asapdiscovery/data/schema_v2/complex.py
+++ asapdiscovery-data/asapdiscovery/data/schema_v2/complex.py
@@ -66,9 +66,6 @@ class Complex(ComplexBase):
 
         # Create Target and Ligand objects
         target = Target.from_oemol(split_dict["prot"], **target_kwargs)
-        lig_mol = split_dict["lig"]
-        if "compound_name" in ligand_kwargs:
-            lig_mol.SetTitle(ligand_kwargs["compound_name"])
         ligand = Ligand.from_oemol(split_dict["lig"], **ligand_kwargs)
 
         return cls(
