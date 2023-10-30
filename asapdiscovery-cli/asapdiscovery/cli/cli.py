56d4e810a847e6b4e28643b500d3f398bf972f4a
hmacdope <hugomacdermott@gmail.com>
2023-10-30 13:44:59 +1100

    docking works

---
+++ asapdiscovery-cli/asapdiscovery/cli/cli.py
@@ -0,0 +1,8 @@
+import click
+
+@click.group()
+def cli(help="Command-line interface for asapdiscovery"):
+    ...
+
+from asapdiscovery.data.cli import cli
+from asapdiscovery.docking.cli import cli
