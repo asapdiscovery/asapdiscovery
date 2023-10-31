9d8ad3db76b1d747857b3f31834fd72e8b384dd7
Jenke Scheen <jenke.scheen@choderalab.org>
2023-10-31 18:00:25 +0100

    vendor new stuff

--- asapdiscovery-data/asapdiscovery/data/metadata/resources.py
+++ asapdiscovery-data/asapdiscovery/data/metadata/resources.py
@@ -33,4 +33,10 @@ master_structures = {
     "EV-D68-3Cpro": pkg_resources.resource_filename(
         __name__, "master_structures/ev_d68_3cpro.pdb"
     ),
+    "ZIKV-NS2B-NS3pro": pkg_resources.resource_filename(
+        __name__, "master_structures/zikv_ns2b_ns3pro.pdb"
+    ),
+    "EV-A71-3Cpro": pkg_resources.resource_filename(
+        __name__, "master_structures/ev_a71_3cpro.pdb"
+    ),
 }
