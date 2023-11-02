bd37b9b3fb07b484997f0b7bb4d78e1c771e7574
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2023-11-02 15:11:11 +1100

    Update asapdiscovery-data/asapdiscovery/data/sequence.py
    Co-authored-by: kaminow <51923685+kaminow@users.noreply.github.com>

--- asapdiscovery-data/asapdiscovery/data/sequence.py
+++ asapdiscovery-data/asapdiscovery/data/sequence.py
@@ -3,8 +3,7 @@ from asapdiscovery.data.postera.manifold_data_validation import TargetTags
 
 
 def seqres_by_target(target: TargetTags):
-    res_prefix = target.value
-    resource = f"{res_prefix}_SEQRES.yaml"
+    resource = target.value
     try:
         res = seqres_data.get(resource)
     except KeyError:
