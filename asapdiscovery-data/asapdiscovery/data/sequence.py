21a52f970bb51d224f49f35a075dbe8a74652217
hmacdope <hugomacdermott@gmail.com>
2023-11-02 15:16:38 +1100

    trigger propper KeyError on read

--- asapdiscovery-data/asapdiscovery/data/sequence.py
+++ asapdiscovery-data/asapdiscovery/data/sequence.py
@@ -5,7 +5,7 @@ from asapdiscovery.data.postera.manifold_data_validation import TargetTags
 def seqres_by_target(target: TargetTags):
     resource = target.value
     try:
-        res = seqres_data.get(resource)
+        res = seqres_data[resource]
     except KeyError:
         raise ValueError(f"Resource {resource} not found.")
     return res
