2d2b64ecd3160f048a30aaad860799a29936a3c1
JenkeScheen <43140137+JenkeScheen@users.noreply.github.com>
2023-11-02 23:52:02 +0100

    Add new targets (#565)
    * add ev d68 3cpro high-level structure
    
    * add evd68 to yaml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * make master structures be in separate folder in metadata
    
    * cleanup
    
    * set seqres into folder as well
    
    * rename old seqres to canonical naming
    
    * more structuring
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * adjust pdb rotation to fit master view
    
    * add more structural refs from literature
    
    * vendor new stuff
    
    * add denv ns2b pdb
    
    * vendor denv ns2b
    
    * add the new virus/target tags to allowed targets
    
    * add seqres yamls
    
    * add script to convert fasta to yaml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add d68 subpocket coloring
    
    * d68 as monomer
    
    * add a71 subpocket resis
    
    * clarification
    
    * zikv and denv ns2b_ns3pro gif blocks
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * vendor new targets again after conflict resolve; add viz targets
    
    * cleanup
    
    * attempt to get views correct
    
    * remove waters from master structures
    
    * more attempts at view correction
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * cleanup
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- asapdiscovery-data/asapdiscovery/data/metadata/resources.py
+++ asapdiscovery-data/asapdiscovery/data/metadata/resources.py
@@ -1,38 +1,67 @@
 import pkg_resources
 
-# import each file
+# SEQRES in YAML format
 MERS_CoV_Mpro_SEQRES = pkg_resources.resource_filename(
-    __name__, "mpro_mers_seqres.yaml"
+    __name__, "master_seqres/mers_cov_mpro_seqres.yaml"
 )
 SARS_CoV_2_Mpro_SEQRES = pkg_resources.resource_filename(
-    __name__, "mpro_sars2_seqres.yaml"
+    __name__, "master_seqres/sars_cov_2_mpro_seqres.yaml"
 )
 SARS_CoV_2_Mac1_SEQRES = pkg_resources.resource_filename(
-    __name__, "mac1_sars2_seqres.yaml"
+    __name__, "master_seqres/sars_cov_2_mac1_seqres.yaml"
+)
+EV_D68_3Cpro_SEQRES = pkg_resources.resource_filename(
+    __name__, "master_seqres/ev_d68_3cpro.yaml"
+)
+EV_A71_3Cpro_SEQRES = pkg_resources.resource_filename(
+    __name__, "master_seqres/ev_a71_3cpro.yaml"
+)
+ZIKV_NS2B_NS3pro_SEQRES = pkg_resources.resource_filename(
+    __name__, "master_seqres/zikv_ns2b_ns3pro.yaml"
+)
+DENV_NS2B_NS3pro_SEQRES = pkg_resources.resource_filename(
+    __name__, "master_seqres/denv_ns2b_ns3pro.yaml"
 )
 
 seqres_data = {
     "MERS-CoV-Mpro": MERS_CoV_Mpro_SEQRES,
     "SARS-CoV-2-Mpro": SARS_CoV_2_Mpro_SEQRES,
     "SARS-CoV-2-Mac1": SARS_CoV_2_Mac1_SEQRES,
+    "EV-D68-3Cpro": EV_D68_3Cpro_SEQRES,
+    "EV-A71-3Cpro": EV_A71_3Cpro_SEQRES,
+    "ZIKV-NS2B-NS3pro": ZIKV_NS2B_NS3pro_SEQRES,
+    "DENV-NS2B-NS3pro": DENV_NS2B_NS3pro_SEQRES,
 }
 
-
+# Fitness data in JSON format
 SARS_CoV_2_fitness_data = pkg_resources.resource_filename(
     __name__, "aa_fitness_sars_cov_2.json"
 )
 
+# Reference PDB files to align targets to for consistent dataviz
 master_structures = {
     "SARS-CoV-2-Mpro": pkg_resources.resource_filename(
-        __name__, "master_sars_cov_2_mpro.pdb"
+        __name__, "master_structures/sars_cov_2_mpro.pdb"
     ),
     "SARS-CoV-2-Mac1": pkg_resources.resource_filename(
-        __name__, "master_sars_cov_2_mac1.pdb"
+        __name__, "master_structures/sars_cov_2_mac1.pdb"
     ),
     "SARS-CoV-2-Mac1-monomer": pkg_resources.resource_filename(
-        __name__, "master_sars_cov_2_mac1.pdb"
+        __name__, "master_structures/sars_cov_2_mac1.pdb"
     ),
     "MERS-CoV-Mpro": pkg_resources.resource_filename(
-        __name__, "master_mers_cov_mpro.pdb"
+        __name__, "master_structures/mers_cov_mpro.pdb"
+    ),
+    "EV-D68-3Cpro": pkg_resources.resource_filename(
+        __name__, "master_structures/ev_d68_3cpro.pdb"
+    ),
+    "EV-A71-3Cpro": pkg_resources.resource_filename(
+        __name__, "master_structures/ev_a71_3cpro.pdb"
+    ),
+    "ZIKV-NS2B-NS3pro": pkg_resources.resource_filename(
+        __name__, "master_structures/zikv_ns2b_ns3pro.pdb"
+    ),
+    "DENV-NS2B-NS3pro": pkg_resources.resource_filename(
+        __name__, "master_structures/denv_ns2b_ns3pro.pdb"
     ),
 }
