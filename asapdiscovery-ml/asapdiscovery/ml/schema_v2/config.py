cc61bdf3f04aceae455d1c468f245a10289d4c65
kaminow <benjamin.kaminow@choderalab.org>
2023-11-06 16:43:08 -0500

    Start adding Config schema for models.

--- asapdiscovery-ml/asapdiscovery/ml/schema_v2/config.py
+++ asapdiscovery-ml/asapdiscovery/ml/schema_v2/config.py
@@ -2,9 +2,9 @@ from __future__ import annotations
 
 from enum import Enum
 from pathlib import Path
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 import torch
-from typing import Iterator
+from typing import Callable, Iterator, List, Optional
 
 # from asapdiscovery.data.schema_v2.schema_base import DataModelAbstractBase
 
@@ -105,3 +105,135 @@ class OptimizerConfig(BaseModel):
             case optimizer_type:
                 # Shouldn't be possible but just in case
                 raise ValueError(f"Unknown value for optimizer_type: {optimizer_type}")
+
+
+class ModelType(str, Enum):
+    """
+    Enum for model types.
+    """
+
+    gat = "gat"
+    schnet = "schnet"
+    e3nn = "e3nn"
+
+
+class ModelConfig(BaseModel):
+    """
+    Class for constructing an ML model.
+    """
+
+    from dgllife.utils import CanonicalAtomFeaturizer
+
+    # GAT-specific params
+    in_feats: int = Field(
+        CanonicalAtomFeaturizer().feat_size(),
+        description=(
+            "Input node feature size. Defaults to size of the CanonicalAtomFeaturizer."
+        ),
+    )
+    num_layers: int = Field(
+        2,
+        description=(
+            "Number of GAT layers. Ignored if a list of values is passed for any "
+            "other argument."
+        ),
+    )
+    hidden_feats: int | List[int] = Field(
+        32,
+        description=(
+            "Output size of each GAT layer. If an int is passed, the value for "
+            "num_layers will be used to determine the size of the model. If a list of "
+            "ints is passed, the size of the model will be inferred from the length of "
+            "the list."
+        ),
+    )
+    num_heads: int | List[int] = Field(
+        4,
+        description=(
+            "Number of attention heads for each GAT layer. Passing an int or list of "
+            "ints functions similarly as for hidden_feats."
+        ),
+    )
+    feat_drops: float | List[float] = Field(
+        0,
+        description=(
+            "Dropout of input features for each GAT layer. Passing an float or list of "
+            "floats functions similarly as for hidden_feats."
+        ),
+    )
+    attn_drops: float | List[float] = Field(
+        0,
+        description=(
+            "Dropout of attention values for each GAT layer. Passing an float or list "
+            "of floats functions similarly as for hidden_feats."
+        ),
+    )
+    alphas: float | List[float] = Field(
+        0.2,
+        description=(
+            "Hyperparameter for LeakyReLU gate for each GAT layer. Passing an float or "
+            "list of floats functions similarly as for hidden_feats."
+        ),
+    )
+    residuals: bool | List[bool] = Field(
+        True,
+        description=(
+            "Whether to use residual connection for each GAT layer. Passing a bool or "
+            "list of bools functions similarly as for hidden_feats."
+        ),
+    )
+    agg_modes: str | List[str] = Field(
+        "flatten",
+        description=(
+            "Which aggregation mode [flatten, mean] to use for each GAT layer. "
+            "Passing a str or list of strs functions similarly as for hidden_feats."
+        ),
+    )
+    activations: Optional[Callable | List[Callable]] = Field(
+        None,
+        description=(
+            "Activation function for each GAT layer. Passing a function or "
+            "list of functions functions similarly as for hidden_feats."
+        ),
+    )
+    biases: bool | List[bool] = Field(
+        True,
+        description=(
+            "Whether to use bias for each GAT layer. Passing a bool or "
+            "list of bools functions similarly as for hidden_feats."
+        ),
+    )
+    allow_zero_in_degree: bool = Field(
+        False, description="Allow zero in degree nodes for all graph layers."
+    )
+
+    @model_validator(mode="after")
+    def massage_into_lists(self) -> "ModelConfig":
+        list_params = [
+            "hidden_feats",
+            "num_heads",
+            "feat_drops",
+            "attn_drops",
+            "alphas",
+            "residuals",
+            "agg_modes",
+            "activations",
+            "biases",
+        ]
+        # First check if any of the list-optional params are lists
+        if any([isinstance(getattr(self, p)), list] for p in list_params):
+            use_num_layers = False
+        else:
+            use_num_layers = True
+
+        # If all values are just ints/floats/bools (ie no lists), we can just make the
+        #  lists based on num_layers and return
+        if use_num_layers:
+            for p in list_params:
+                setattr(self, p, [getattr(self, p)] * self.num_layers)
+
+            return self
+
+        # Otherwise need to do a bit more logic to get things right
+
+        return self
