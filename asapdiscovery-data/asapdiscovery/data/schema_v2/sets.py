e5d6538eed744e8556d16135de30509827908423
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-11-07 11:12:16 -0500

    enable creation of one-to-many sets from list of pairs

--- asapdiscovery-data/asapdiscovery/data/schema_v2/sets.py
+++ asapdiscovery-data/asapdiscovery/data/schema_v2/sets.py
@@ -1,8 +1,9 @@
-from typing import Any
+from typing import Any, Union
 
 from asapdiscovery.data.schema_v2.complex import Complex, PreppedComplex
 from asapdiscovery.data.schema_v2.ligand import Ligand
 from asapdiscovery.data.schema_v2.schema_base import DataModelAbstractBase
+from asapdiscovery.data.schema_v2.pairs import CompoundStructurePair, DockingInputPair
 from pydantic import Field
 
 
@@ -20,14 +21,47 @@ class MultiStructureBase(DataModelAbstractBase):
             set(self.complex) == set(other.complex)
         )
 
+    @classmethod
+    def _from_pairs(
+        cls,
+        pair_list: list[Union[CompoundStructurePair, DockingInputPair]],
+    ) -> list:
+        """
+        Create a list of CompoundMultiStructures from a list of CompoundStructurePairs.
+        Automatically separates out the ligands.
+        """
+        ligand_complexes_dict = {}
 
-class LigandMultiStructure(MultiStructureBase):
+        for pair in pair_list:
+            ligand = pair.ligand
+            complex = pair.complex
+
+            if ligand in ligand_complexes_dict:
+                ligand_complexes_dict[ligand].append(complex)
+            else:
+                ligand_complexes_dict[ligand] = [complex]
+
+        compound_multi_structures = [
+            cls(ligand=ligand, complexes=complexes)
+            for ligand, complexes in ligand_complexes_dict.items()
+        ]
+
+        return compound_multi_structures
+
+
+class CompoundMultiStructure(MultiStructureBase):
     """
     Schema for one ligand to many possible reference structures.
     """
 
     ligand: Ligand = Field(description="Ligand schema object")
     complexes: list[Complex] = Field(description="List of reference structures")
+
+    @classmethod
+    def from_pairs(
+        cls, pair_list: list[CompoundStructurePair]
+    ) -> list["CompoundMultiStructure"]:
+        return cls._from_pairs(pair_list)
 
 
 class DockingInputMultiStructure(MultiStructureBase):
@@ -37,3 +71,9 @@ class DockingInputMultiStructure(MultiStructureBase):
 
     ligand: Ligand = Field(description="Ligand schema object")
     complexes: list[PreppedComplex] = Field(description="List of reference structures")
+
+    @classmethod
+    def from_pairs(
+        cls, pair_list: list[DockingInputPair]
+    ) -> list["DockingInputMultiStructure"]:
+        return cls._from_pairs(pair_list)
