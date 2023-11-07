d4f11d51eb0a6804fb2bf8d7c5c3c75231428392
Alexander Matthew Payne <alexpayne1997@gmail.com>
2023-11-07 11:30:35 -0500

    add enum of docking methods

---
+++ asapdiscovery-docking/asapdiscovery/docking/docking_methods.py
@@ -0,0 +1,13 @@
+"""
+This collects the docking methods together in a single enum
+"""
+from enum import Enum
+from asapdiscovery.docking.docking_v2 import POSITDocker
+
+
+class DockingMethod(Enum):
+    """
+    Enum of all docking methods.
+    """
+
+    POSIT = POSITDocker
