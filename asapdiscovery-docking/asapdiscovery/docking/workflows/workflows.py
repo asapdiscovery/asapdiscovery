f3df264747d0425dd238093c4d64e7cef4aa23a6
hmacdope <hugomacdermott@gmail.com>
2023-11-10 14:51:35 +1100

    fix postera in baseclass

--- asapdiscovery-docking/asapdiscovery/docking/workflows/workflows.py
+++ asapdiscovery-docking/asapdiscovery/docking/workflows/workflows.py
@@ -31,6 +31,13 @@ class DockingWorkflowInputsBase(BaseModel):
         False, description="Whether to use the Postera database as the query set."
     )
 
+    postera_upload: bool = Field(
+        False, description="Whether to upload the results to Postera."
+    )
+    postera_molset_name: Optional[str] = Field(
+        None, description="The name of the molecule set to upload to."
+    )
+
     cache_dir: Optional[str] = Field(
         None, description="Path to a directory where a cache has been generated"
     )
