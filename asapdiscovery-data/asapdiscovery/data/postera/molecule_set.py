8a773a9089a7d64d32ea6e61624322e2d9312c30
hmacdope <hugomacdermott@gmail.com>
2023-11-13 08:07:58 +1100

    add response printing from postera

--- asapdiscovery-data/asapdiscovery/data/postera/molecule_set.py
+++ asapdiscovery-data/asapdiscovery/data/postera/molecule_set.py
@@ -335,8 +335,10 @@ class MoleculeSetAPI(PostEraAPI):
         ).json()
         try:
             return response["moleculesUpdated"]
-        except KeyError:
-            raise ValueError(f"Update failed for molecule set {molecule_set_id}")
+
+        except Exception as e:
+            raise ValueError(f"Update failed for molecule set {molecule_set_id}, with response: {response}") from e
+
 
     def update_molecules_from_df_with_manifold_validation(
         self,
