77a02dc695b863f01d0b875401c18e6f4914fdb6
kaminow <benjamin.kaminow@choderalab.org>
2023-11-13 11:40:34 -0500

    Change all CLI arg defaults to None so we can actually know when an arg was passed.

--- asapdiscovery-ml/asapdiscovery/ml/cli.py
+++ asapdiscovery-ml/asapdiscovery/ml/cli.py
@@ -19,14 +19,12 @@ def ml():
 # Shared MTENN-related parameters
 @click.option(
     "--grouped",
-    is_flag=True,
-    default=False,
+    type=bool,
     help="Model is a grouped (multi-pose) model.",
 )
 @click.option(
     "--strategy",
     type=ascfg.MTENNStrategy,
-    default=ascfg.MTENNStrategy.delta,
     help=(
         "Which Strategy to use for combining complex, protein, and ligand "
         "representations in the MTENN Model."
@@ -56,7 +54,6 @@ def ml():
 @click.option(
     "--max-comb-neg",
     type=bool,
-    default=True,
     help=(
         "Whether to take the min instead of max when combining pose predictions "
         "with MaxCombination."
@@ -65,7 +62,6 @@ def ml():
 @click.option(
     "--max-comb-scale",
     type=float,
-    default=1000,
     help=(
         "Scaling factor for values when taking the max/min when combining pose "
         "predictions with MaxCombination. A value of 1 will approximate the "
@@ -110,16 +106,10 @@ def ml():
     ),
 )
 # GAT-specific parameters
-@click.option(
-    "--in-feats",
-    type=int,
-    default=CanonicalAtomFeaturizer().feat_size(),
-    help=("Input node feature size. Defaults to size of the CanonicalAtomFeaturizer."),
-)
+@click.option("--in-feats", type=int, help="Input node feature size.")
 @click.option(
     "--num-layers-gat",
     type=int,
-    default=2,
     help=(
         "Number of GAT layers. Ignored if multiple values are passed for any "
         "other GAT argument. To define a model with only one layer, this must be "
@@ -128,7 +118,6 @@ def ml():
 )
 @click.option(
     "--hidden-feats",
-    default="32",
     help=(
         "Output size of each GAT layer. This can either be a single value, which will "
         "be broadcasted to each layer, or a comma-separated list with each value "
@@ -137,7 +126,6 @@ def ml():
 )
 @click.option(
     "--num-heads",
-    default="4",
     help=(
         "Number of attention heads for each GAT layer. Passing a single value or "
         "multiple values functions similarly as for --hidden-feats."
@@ -145,7 +133,6 @@ def ml():
 )
 @click.option(
     "--feat-drops",
-    default="0",
     help=(
         "Dropout of input features for each GAT layer. Passing a single value or "
         "multiple values functions similarly as for --hidden-feats."
@@ -153,7 +140,6 @@ def ml():
 )
 @click.option(
     "--attn-drops",
-    default="0",
     help=(
         "Dropout of attention values for each GAT layer. Passing a single value or "
         "multiple values functions similarly as for --hidden-feats."
@@ -161,7 +147,6 @@ def ml():
 )
 @click.option(
     "--alphas",
-    default="0.2",
     help=(
         "Hyperparameter for LeakyReLU gate for each GAT layer. Passing a single value "
         "or multiple values functions similarly as for --hidden-feats."
@@ -169,7 +154,6 @@ def ml():
 )
 @click.option(
     "--residuals",
-    default="True",
     help=(
         "Whether to use residual connection for each GAT layer. Passing a single value "
         "or multiple values functions similarly as for --hidden-feats."
@@ -177,7 +161,6 @@ def ml():
 )
 @click.option(
     "--agg-modes",
-    default="flatten",
     help=(
         "Which aggregation mode [flatten, mean] to use for each GAT layer. Passing a "
         "single value or multiple values functions similarly as for --hidden-feats."
@@ -185,7 +168,6 @@ def ml():
 )
 @click.option(
     "--biases",
-    default="True",
     help=(
         "Whether to use bias for each GAT layer. Passing a single value "
         "or multiple values functions similarly as for --hidden-feats."
@@ -193,47 +175,40 @@ def ml():
 )
 @click.option(
     "--allow-zero-in-degree",
-    is_flag=True,
+    type=bool,
     help="Allow zero in degree nodes for all graph layers.",
 )
 # SchNet-specific parameters
-@click.option("--hidden-channels", type=int, default=128, help="Hidden embedding size.")
+@click.option("--hidden-channels", type=int, help="Hidden embedding size.")
 @click.option(
     "--num-filters",
     type=int,
-    default=128,
     help="Number of filters to use in the cfconv layers.",
 )
-@click.option(
-    "--num-interactions", type=int, default=6, help="Number of interaction blocks."
-)
+@click.option("--num-interactions", type=int, help="Number of interaction blocks.")
 @click.option(
     "--num-gaussians",
     type=int,
-    default=50,
     help="Number of gaussians to use in the interaction blocks.",
 )
 @click.option(
     "--cutoff",
     type=float,
-    default=10,
     help="Cutoff distance for interatomic interactions.",
 )
 @click.option(
     "--max-num-neighbors",
     type=int,
-    default=32,
     help="Maximum number of neighbors to collect for each node.",
 )
 @click.option(
     "--readout",
     type=click.Choice(["add", "mean"]),
-    default="add",
     help="Which global aggregation to use [add, mean].",
 )
 @click.option(
     "--dipole",
-    is_flag=True,
+    type=bool,
     help=(
         "Whether to use the magnitude of the dipole moment to make the final "
         "prediction."
@@ -261,7 +236,6 @@ def ml():
 @click.option(
     "--num-atom-types",
     type=int,
-    default=100,
     help=(
         "Number of different atom types. In general, this will just be the "
         "max atomic number of all input atoms."
@@ -269,175 +243,171 @@ def ml():
 )
 @click.option(
     "--irreps-hidden",
-    default="10x0o+10x0e+3x1o+3x1e+2x2o+2x2e+1x3o+1x3e",
     help="Irreps for the hidden layers of the network.",
 )
 @click.option(
-    "--lig", is_flag=True, help="Include ligand labels as a node attribute information."
+    "--lig", type=bool, help="Include ligand labels as a node attribute information."
 )
 @click.option(
     "--irreps-edge-attr",
     type=int,
-    default=3,
     help=(
         "Which level of spherical harmonics to use for encoding edge attributes "
         "internally."
     ),
 )
-@click.option(
-    "--num-layers-schnet", type=int, default=3, help="Number of network layers."
-)
+@click.option("--num-layers-schnet", type=int, help="Number of network layers.")
 @click.option(
     "--neighbor-dist",
     type=float,
-    default=10,
     help="Cutoff distance for including atoms as neighbors.",
 )
 @click.option(
     "--num-basis",
     type=int,
-    default=10,
     help="Number of bases on which the edge length are projected.",
 )
-@click.option(
-    "--num-radial-layers", type=int, default=1, help="Number of radial layers."
-)
+@click.option("--num-radial-layers", type=int, help="Number of radial layers.")
 @click.option(
     "--num-radial-neurons",
     type=int,
-    default=128,
     help="Number of neurons in each radial layer.",
 )
-@click.option(
-    "--num-neighbors", type=float, default=25, help="Typical number of neighbor nodes."
-)
-@click.option(
-    "--num-nodes", type=float, default=4700, help="Typical number of nodes in a graph."
-)
+@click.option("--num-neighbors", type=float, help="Typical number of neighbor nodes.")
+@click.option("--num-nodes", type=float, help="Typical number of nodes in a graph.")
 def test(
     model_type: ascfg.ModelType,
-    grouped: bool = False,
-    strategy: ascfg.MTENNStrategy = ascfg.MTENNStrategy.delta,
+    grouped: bool | None = None,
+    strategy: ascfg.MTENNStrategy | None = None,
     pred_readout: ascfg.MTENNReadout | None = None,
     combination: ascfg.MTENNCombination | None = None,
     comb_readout: ascfg.MTENNReadout | None = None,
-    max_comb_neg: bool = True,
-    max_comb_scale: float = 1000,
+    max_comb_neg: bool | None = None,
+    max_comb_scale: float | None = None,
     pred_substrate: float | None = None,
     pred_km: float | None = None,
     comb_substrate: float | None = None,
     comb_km: float | None = None,
-    in_feats: int = CanonicalAtomFeaturizer().feat_size(),
-    num_layers_gat: int = 2,
-    hidden_feats: str = "32",
-    num_heads: str = "4",
-    feat_drops: str = "0",
-    attn_drops: str = "0",
-    alphas: str = "0.2",
-    residuals: str = "True",
-    agg_modes: str = "flatten",
-    biases: str = "True",
-    allow_zero_in_degree: bool = False,
-    hidden_channels: int = 128,
-    num_filters: int = 128,
-    num_interactions: int = 6,
-    num_gaussians: int = 50,
-    cutoff: float = 10,
-    max_num_neighbors: int = 32,
-    readout: str = "add",
-    dipole: bool = False,
+    in_feats: int | None = None,
+    num_layers_gat: int | None = None,
+    hidden_feats: str | None = None,
+    num_heads: str | None = None,
+    feat_drops: str | None = None,
+    attn_drops: str | None = None,
+    alphas: str | None = None,
+    residuals: str | None = None,
+    agg_modes: str | None = None,
+    biases: str | None = None,
+    allow_zero_in_degree: bool | None = None,
+    hidden_channels: int | None = None,
+    num_filters: int | None = None,
+    num_interactions: int | None = None,
+    num_gaussians: int | None = None,
+    cutoff: float | None = None,
+    max_num_neighbors: int | None = None,
+    readout: str | None = None,
+    dipole: bool | None = None,
     mean: float | None = None,
     std: float | None = None,
-    num_atom_types: int = 100,
-    irreps_hidden: str = "10x0o+10x0e+3x1o+3x1e+2x2o+2x2e+1x3o+1x3e",
-    lig: bool = False,
-    irreps_edge_attr: int = 3,
-    num_layers_schnet: int = 3,
-    neighbor_dist: float = 10,
-    num_basis: int = 10,
-    num_radial_layers: int = 1,
-    num_radial_neurons: int = 128,
-    num_neighbors: float = 25,
-    num_nodes: float = 4700,
+    num_atom_types: int | None = None,
+    irreps_hidden: str | None = None,
+    lig: bool | None = None,
+    irreps_edge_attr: int | None = None,
+    num_layers_schnet: int | None = None,
+    neighbor_dist: float | None = None,
+    num_basis: int | None = None,
+    num_radial_layers: int | None = None,
+    num_radial_neurons: int | None = None,
+    num_neighbors: float | None = None,
+    num_nodes: float | None = None,
 ):
     # Build the model
     match model_type:
         case ascfg.ModelType.gat:
-            config = ascfg.GATModelConfig(
-                grouped=grouped,
-                strategy=strategy,
-                pred_readout=pred_readout,
-                combination=combination,
-                comb_readout=comb_readout,
-                max_comb_neg=max_comb_neg,
-                max_comb_scale=max_comb_scale,
-                pred_substrate=pred_substrate,
-                pred_km=pred_km,
-                comb_substrate=comb_substrate,
-                comb_km=comb_km,
-                in_feats=in_feats,
-                num_layers=num_layers_gat,
-                hidden_feats=hidden_feats,
-                num_heads=num_heads,
-                feat_drops=feat_drops,
-                attn_drops=attn_drops,
-                alphas=alphas,
-                residuals=residuals,
-                agg_modes=agg_modes,
-                biases=biases,
-                allow_zero_in_degree=allow_zero_in_degree,
-            )
+            config_class = ascfg.GATModelConfig
+            cli_config_vals = {
+                "grouped": grouped,
+                "strategy": strategy,
+                "pred_readout": pred_readout,
+                "combination": combination,
+                "comb_readout": comb_readout,
+                "max_comb_neg": max_comb_neg,
+                "max_comb_scale": max_comb_scale,
+                "pred_substrate": pred_substrate,
+                "pred_km": pred_km,
+                "comb_substrate": comb_substrate,
+                "comb_km": comb_km,
+                "in_feats": in_feats,
+                "num_layers": num_layers_gat,
+                "hidden_feats": hidden_feats,
+                "num_heads": num_heads,
+                "feat_drops": feat_drops,
+                "attn_drops": attn_drops,
+                "alphas": alphas,
+                "residuals": residuals,
+                "agg_modes": agg_modes,
+                "biases": biases,
+                "allow_zero_in_degree": allow_zero_in_degree,
+            }
         case ascfg.ModelType.schnet:
-            config = ascfg.SchNetModelConfig(
-                grouped=grouped,
-                strategy=strategy,
-                pred_readout=pred_readout,
-                combination=combination,
-                comb_readout=comb_readout,
-                max_comb_neg=max_comb_neg,
-                max_comb_scale=max_comb_scale,
-                pred_substrate=pred_substrate,
-                pred_km=pred_km,
-                comb_substrate=comb_substrate,
-                comb_km=comb_km,
-                hidden_channels=hidden_channels,
-                num_filters=num_filters,
-                num_interactions=num_interactions,
-                num_gaussians=num_gaussians,
-                cutoff=cutoff,
-                max_num_neighbors=max_num_neighbors,
-                readout=readout,
-                dipole=dipole,
-                mean=mean,
-                std=std,
-            )
+            config_class = ascfg.SchNetModelConfig
+            cli_config_vals = {
+                "grouped": grouped,
+                "strategy": strategy,
+                "pred_readout": pred_readout,
+                "combination": combination,
+                "comb_readout": comb_readout,
+                "max_comb_neg": max_comb_neg,
+                "max_comb_scale": max_comb_scale,
+                "pred_substrate": pred_substrate,
+                "pred_km": pred_km,
+                "comb_substrate": comb_substrate,
+                "comb_km": comb_km,
+                "hidden_channels": hidden_channels,
+                "num_filters": num_filters,
+                "num_interactions": num_interactions,
+                "num_gaussians": num_gaussians,
+                "cutoff": cutoff,
+                "max_num_neighbors": max_num_neighbors,
+                "readout": readout,
+                "dipole": dipole,
+                "mean": mean,
+                "std": std,
+            }
         case ascfg.ModelType.e3nn:
-            config = ascfg.E3NNModelConfig(
-                grouped=grouped,
-                strategy=strategy,
-                pred_readout=pred_readout,
-                combination=combination,
-                comb_readout=comb_readout,
-                max_comb_neg=max_comb_neg,
-                max_comb_scale=max_comb_scale,
-                pred_substrate=pred_substrate,
-                pred_km=pred_km,
-                comb_substrate=comb_substrate,
-                comb_km=comb_km,
-                num_atom_types=num_atom_types,
-                irreps_hidden=irreps_hidden,
-                lig=lig,
-                irreps_edge_attr=irreps_edge_attr,
-                num_layers=num_layers_schnet,
-                neighbor_dist=neighbor_dist,
-                num_basis=num_basis,
-                num_radial_layers=num_radial_layers,
-                num_radial_neurons=num_radial_neurons,
-                num_neighbors=num_neighbors,
-                num_nodes=num_nodes,
-            )
+            config_class = ascfg.E3NNModelConfig
+            cli_config_vals = {
+                "grouped": grouped,
+                "strategy": strategy,
+                "pred_readout": pred_readout,
+                "combination": combination,
+                "comb_readout": comb_readout,
+                "max_comb_neg": max_comb_neg,
+                "max_comb_scale": max_comb_scale,
+                "pred_substrate": pred_substrate,
+                "pred_km": pred_km,
+                "comb_substrate": comb_substrate,
+                "comb_km": comb_km,
+                "num_atom_types": num_atom_types,
+                "irreps_hidden": irreps_hidden,
+                "lig": lig,
+                "irreps_edge_attr": irreps_edge_attr,
+                "num_layers": num_layers_schnet,
+                "neighbor_dist": neighbor_dist,
+                "num_basis": num_basis,
+                "num_radial_layers": num_radial_layers,
+                "num_radial_neurons": num_radial_neurons,
+                "num_neighbors": num_neighbors,
+                "num_nodes": num_nodes,
+            }
         case unknown:
             raise ValueError(f"Unknown model type: {unknown}")
+
+    # Only keep values CLI config vals that actually had a value passed
+    cli_config_vals = {k: v for k, v in cli_config_vals.items() if v is not None}
+    print(cli_config_vals, flush=True)
+    config = config_class(**cli_config_vals)
+
     print(config, flush=True)
     model = config.build()
     print(model, flush=True)
