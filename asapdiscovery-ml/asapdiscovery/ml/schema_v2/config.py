d3ca6e957b1c892e16d84f834820ff92a6cc0f34
kaminow <benjamin.kaminow@choderalab.org>
2023-11-13 12:02:56 -0500

    Remove unused import.

--- asapdiscovery-ml/asapdiscovery/ml/schema_v2/config.py
+++ asapdiscovery-ml/asapdiscovery/ml/schema_v2/config.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import abc
 from collections.abc import Iterator
 from enum import Enum
-from pathlib import Path
 from typing import Callable, ClassVar
 
 import mtenn
