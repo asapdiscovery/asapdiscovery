b2bf5b99581b382437ec4e404fb0692232e13f20
kaminow <benjamin.kaminow@choderalab.org>
2023-11-15 11:43:10 -0500

    Add actual splitting methods.

--- asapdiscovery-ml/asapdiscovery/ml/schema_v2/config.py
+++ asapdiscovery-ml/asapdiscovery/ml/schema_v2/config.py
@@ -312,17 +312,20 @@ class DatasetSplitterConfig(BaseModel):
         0.1, description="Fraction of dataset to put in the test split."
     )
     enforce_1: bool = Field(
-        False, description="Make sure that all split fractions add up to 1."
+        True, description="Make sure that all split fractions add up to 1."
     )
 
     # Seed for randomly splitting data
-    rand_seed: int = Field(
-        42, description="Random seed to use if randomly splitting data."
+    rand_seed: int | None = Field(
+        None, description="Random seed to use if randomly splitting data."
     )
 
     @root_validator(pre=False)
     def check_frac_sum(cls, values):
         frac_sum = sum([values["train_frac"], values["val_frac"], values["test_frac"]])
+        if frac_sum < 0:
+            raise ValueError("Can't have negative split fractions.")
+
         if not np.isclose(frac_sum, 1):
             warn_msg = f"Split fractions add to {frac_sum:0.2f}, not 1."
             if values["enforce_1"]:
@@ -332,7 +335,227 @@ class DatasetSplitterConfig(BaseModel):
 
                 warn(warn_msg, RuntimeWarning)
 
+            if frac_sum > 1:
+                raise ValueError("Can't have split fractions adding to > 1.")
+
         return values
+
+    def split(self, ds: DockedDataset | GraphDataset | GroupedDockedDataset):
+        """
+        Dispatch method for spliting ds into train, val, and test splits.
+
+        Parameters
+        ----------
+        ds : DockedDataset | GraphDataset | GroupedDockedDataset
+            Full ML dataset to split
+
+        Returns
+        -------
+        torch.nn.Subset
+            Train split
+        torch.nn.Subset
+            Val split
+        torch.nn.Subset
+            Test split
+        """
+        match self.split_type:
+            case DatasetSplitterType.random:
+                return self._split_random(ds)
+            case DatasetSplitterType.temporal:
+                return self._split_temporal(ds)
+            case other:
+                raise ValueError(f"Unknown DatasetSplitterType {other}.")
+
+    @staticmethod
+    def _make_subsets(ds, idx_lists, split_lens):
+        """
+        Helper script for making subsets of a dataset.
+
+        Parameters
+        ----------
+        ds : Union[cml.data.DockedDataset, cml.data.GraphDataset]
+            Molecular dataset to split
+        idx_dict : List[List[int]]
+            List of lists of indices into `ds`
+        split_lens : List[int]
+            List of split lengths
+
+        Returns
+        -------
+        List[torch.utils.data.Subset]
+            List of Subsets of original dataset
+        """
+        # For each Subset, grab all molecules with the included compound_ids
+        all_subsets = []
+        # Keep track of which structure indices we've seen so we don't double count in the
+        #  end splits
+        seen_idx = set()
+        prev_idx = 0
+        # Go up to the last split so we can add anything that got left out from rounding
+        for i, n_mols in enumerate(split_lens[:-1]):
+            n_mols_cur = 0
+            subset_idx = []
+            cur_idx = prev_idx
+            # Keep adding groups until the split is as big as it needs to be, or we reach
+            #  the end of the array (making sure to save at least some molecules for the
+            #  rest of the splits)
+            while (n_mols_cur < n_mols) and (
+                cur_idx < (len(idx_lists) - (len(split_lens) - i))
+            ):
+                subset_idx.extend(idx_lists[cur_idx])
+                n_mols_cur += len(idx_lists[cur_idx])
+                cur_idx += 1
+
+            # Make sure we're not including something that's in another split
+            subset_idx = [i for i in subset_idx if i not in seen_idx]
+            seen_idx.update(subset_idx)
+            all_subsets.append(torch.utils.data.Subset(ds, subset_idx))
+
+            # Update counter
+            prev_idx = cur_idx
+
+        # Finish up anything leftover
+        subset_idx = [i for d in idx_lists[prev_idx:] for i in d if i not in seen_idx]
+        all_subsets.append(torch.utils.data.Subset(ds, subset_idx))
+
+        return all_subsets
+
+    def _split_random(self, ds: DockedDataset | GraphDataset | GroupedDockedDataset):
+        """
+        Random split.
+
+        Parameters
+        ----------
+        ds : DockedDataset | GraphDataset | GroupedDockedDataset
+            Full ML dataset to split
+
+        Returns
+        -------
+        torch.nn.Subset
+            Train split
+        torch.nn.Subset
+            Val split
+        torch.nn.Subset
+            Test split
+        """
+
+        if self.rand_seed is not None:
+            g = torch.Generator()
+        else:
+            g = torch.Generator().manual_seed(self.rand_seed)
+        print("splitting with random seed:", g.initial_seed(), flush=True)
+
+        if self.grouped:
+            # Grouped models are already grouped by compound, so don't need to do
+            #  anything fancy here
+            ds_train, ds_val, ds_test = torch.utils.data.random_split(
+                ds, [self.train_frac, self.val_frac, self.test_frac], g
+            )
+        else:
+            # Calculate how many molecules we want covered through each split
+            n_mols_split = np.floor(
+                np.asarray([self.train_frac, self.val_frac, self.test_frac]) * len(ds)
+            )
+
+            # First get all the unique compound_ids
+            compound_ids_dict = {}
+            for c, idx_list in ds.compounds.items():
+                try:
+                    compound_ids_dict[c[1]].extend(idx_list)
+                except KeyError:
+                    compound_ids_dict[c[1]] = idx_list
+            all_compound_ids = np.asarray(list(compound_ids_dict.keys()))
+
+            # Shuffle the indices
+            indices = torch.randperm(len(all_compound_ids), generator=g)
+            idx_lists = [compound_ids_dict[all_compound_ids[i]] for i in indices]
+
+            # For each Subset, grab all molecules with the included compound_ids
+            ds_train, ds_val, ds_test = DatasetSplitterConfig._make_subsets(
+                ds, idx_lists, n_mols_split
+            )
+
+        return ds_train, ds_val, ds_test
+
+    def _split_temporal(self, ds: DockedDataset | GraphDataset | GroupedDockedDataset):
+        """
+        Temporal split.
+
+        Parameters
+        ----------
+        ds : DockedDataset | GraphDataset | GroupedDockedDataset
+            Full ML dataset to split
+
+        Returns
+        -------
+        torch.nn.Subset
+            Train split
+        torch.nn.Subset
+            Val split
+        torch.nn.Subset
+            Test split
+        """
+        split_fracs = [self.train_frac, self.val_frac, self.test_frac]
+        # Check that split_fracs adds to 1, padding if it doesn't
+        # Add an allowance for floating point inaccuracies
+        total_splits = sum(split_fracs)
+        if not np.isclose(total_splits, 1):
+            sink_frac = 1 - total_splits
+            split_fracs = split_fracs[:2] + [sink_frac] + split_fracs[2:]
+            sink_split = True
+        else:
+            sink_split = False
+
+        # Calculate how many molecules we want covered through each split
+        n_mols_split = np.floor(np.asarray(split_fracs) * len(ds))
+
+        # First get all the unique created dates
+        dates_dict = {}
+        # If we have a grouped dataset, we want to iterate through compound_ids, which will
+        #  allow us to access a group of structures. Otherwise, loop through the structures
+        #  directly
+        if self.grouped:
+            iter_list = ds.compound_ids
+        else:
+            iter_list = ds.structures
+        for i, iter_item in enumerate(iter_list):
+            if self.grouped:
+                # Take the earliest date from all structures (they should all be the same,
+                #  but just in case)
+                all_dates = [
+                    s["date_created"]
+                    for s in ds.structures[iter_item]
+                    if "date_created" in s
+                ]
+                if len(all_dates) == 0:
+                    raise ValueError("Dataset doesn't contain dates.")
+                else:
+                    date_created = min(all_dates)
+            else:
+                try:
+                    date_created = iter_item["date_created"]
+                except KeyError:
+                    raise ValueError("Dataset doesn't contain dates.")
+            try:
+                dates_dict[date_created].append(i)
+            except KeyError:
+                dates_dict[date_created] = [i]
+        all_dates = np.asarray(list(dates_dict.keys()))
+
+        # Sort the dates
+        all_dates_sorted = sorted(all_dates)
+
+        # Make subsets
+        idx_lists = [dates_dict[d] for d in all_dates_sorted]
+        all_subsets = DatasetSplitterConfig._make_subsets(ds, idx_lists, n_mols_split)
+
+        # Take out the sink split
+        if sink_split:
+            ds_train, ds_val, _, ds_test = all_subsets
+        else:
+            ds_train, ds_val, ds_test = all_subsets
+
+        return ds_train, ds_val, ds_test
 
 
 class ModelType(str, Enum):
