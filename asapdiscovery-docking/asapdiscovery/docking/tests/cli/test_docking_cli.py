878bcd8def5117e4e9ce9c622967741e11d01429
hmacdope <hugomacdermott@gmail.com>
2023-11-16 09:59:58 +1100

    breaking issue with docking tests

--- asapdiscovery-docking/asapdiscovery/docking/tests/cli/test_docking_cli.py
+++ asapdiscovery-docking/asapdiscovery/docking/tests/cli/test_docking_cli.py
@@ -14,79 +14,53 @@ def click_success(result):
     return result.exit_code == 0
 
 
-@pytest.mark.parametrize("subcommand", ["large-scale", "small-scale", "cross-docking"])
+@pytest.mark.parametrize("use_dask", [True, False])
+@pytest.mark.parametrize("subcommand, posit_cutoff", [("large-scale", 0), ("small-scale", 0), ("cross-docking", False)])
 @pytest.mark.skipif(
     os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
 )
-def test_docking_cli_fragalysis(ligand_file, mpro_frag_dir, tmp_path, subcommand):
-    runner = CliRunner()
-
-    frag_parent_dir, _ = mpro_frag_dir
-
-    result = runner.invoke(
-        cli,
-        [
-            subcommand,
-            "--target",
-            "SARS-CoV-2-Mpro",
-            "--ligands",
-            ligand_file,
-            "--fragalysis-dir",
-            frag_parent_dir,
-            "--posit-confidence-cutoff",
-            0,
-            "--output-dir",
-            tmp_path,
-        ],
-    )
-    assert click_success(result)
-
-
-@pytest.mark.parametrize("subcommand", ["large-scale", "small-scale", "cross-docking"])
-@pytest.mark.skipif(
-    os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
-)
-def test_docking_cli_structure_directory(
-    ligand_file, structure_dir, tmp_path, subcommand
+def test_docking_cli_fragalysis(
+    ligand_file, mpro_frag_dir, tmp_path, subcommand, use_dask, posit_cutoff
 ):
     runner = CliRunner()
 
-    struct_dir, _ = structure_dir
+    frag_parent_dir, _ = mpro_frag_dir
+    cli_args = [
+        subcommand,
+        "--target",
+        "SARS-CoV-2-Mpro",
+        "--ligands",
+        ligand_file,
+        "--fragalysis-dir",
+        frag_parent_dir,
+        use_dask,
+        "--output-dir",
+        tmp_path,
+    ]
 
-    result = runner.invoke(
-        cli,
-        [
-            subcommand,
-            "--target",
-            "SARS-CoV-2-Mpro",
-            "--ligands",
-            ligand_file,
-            "--structure-dir",
-            struct_dir,
-            "--posit-confidence-cutoff",
-            0,
-            "--output-dir",
-            tmp_path,
-        ],
-    )
+    if use_dask:
+        cli_args += ["--use-dask"]
+
+    if posit_cutoff:
+        cli_args += ["--posit-confidence-cutoff", posit_cutoff]
+
+    result = runner.invoke(cli, cli_args)
     assert click_success(result)
 
 
-@pytest.mark.parametrize("subcommand", ["large-scale", "small-scale", "cross-docking"])
+@pytest.mark.parametrize("subcommand, posit_cutoff", [("large-scale", 0), ("small-scale", 0), ("cross-docking", False)])
 @pytest.mark.skipif(
     os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
 )
 def test_docking_cli_structure_directory_du_cache(
-    ligand_file, structure_dir, du_cache, tmp_path, subcommand
+    ligand_file, structure_dir, du_cache, tmp_path, subcommand, posit_cutoff
 ):
     runner = CliRunner()
 
     struct_dir, _ = structure_dir
     du_cache_dir, _ = du_cache
 
-    result = runner.invoke(
-        cli,
-        [
+    cli_args =  [
             subcommand,
             "--target",
             "SARS-CoV-2-Mpro",
@@ -94,27 +68,33 @@ def test_docking_cli_structure_directory_du_cache(
             ligand_file,
             "--structure-dir",
             struct_dir,
-            "--posit-confidence-cutoff",
-            0,
+            "--use-dask",
             "--cache-dir",
             du_cache_dir,
             "--output-dir",
             tmp_path,
-        ],
+        ]
+    
+    if posit_cutoff:
+        cli_args += ["--posit-confidence-cutoff", posit_cutoff]
+    
+
+    result = runner.invoke(
+        cli,
+        cli_args,
+       
     )
     assert click_success(result)
 
 
-@pytest.mark.parametrize("subcommand", ["large-scale", "small-scale", "cross-docking"])
+@pytest.mark.parametrize("subcommand, posit_cutoff", [("large-scale", 0), ("small-scale", 0), ("cross-docking", False)])
 @pytest.mark.skipif(
     os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
 )
-def test_docking_cli_pdb_file(ligand_file, pdb_file, tmp_path, subcommand):
+def test_docking_cli_pdb_file(ligand_file, pdb_file, tmp_path, subcommand, posit_cutoff):
     runner = CliRunner()
 
-    result = runner.invoke(
-        cli,
-        [
+    cli_args =  [
             subcommand,
             "--target",
             "SARS-CoV-2-Mpro",
@@ -126,6 +106,16 @@ def test_docking_cli_pdb_file(ligand_file, pdb_file, tmp_path, subcommand):
             0,
             "--output-dir",
             tmp_path,
-        ],
+        ]
+
+    
+    if posit_cutoff:
+        cli_args += ["--posit-confidence-cutoff", posit_cutoff]
+    
+
+    result = runner.invoke(
+        cli,
+        cli_args,
+
     )
     assert click_success(result)
