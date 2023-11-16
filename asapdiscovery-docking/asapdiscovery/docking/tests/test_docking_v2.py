72f59adb076181c29c97b5fbf55f7e79d7801286
hmacdope <hugomacdermott@gmail.com>
2023-11-16 11:35:44 +1100

    add omega dense

--- asapdiscovery-docking/asapdiscovery/docking/tests/test_docking_v2.py
+++ asapdiscovery-docking/asapdiscovery/docking/tests/test_docking_v2.py
@@ -8,10 +8,26 @@ from asapdiscovery.docking.openeye import POSITDocker
     os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
 )
 def test_docking(docking_input_pair):
-    docker = POSITDocker()
+    docker = POSITDocker(use_omega=False)  # save compute
     results = docker.dock([docking_input_pair])
     assert len(results) == 1
     assert results[0].probability > 0.0
+
+
+@pytest.mark.parametrize("omega_dense", [True, False])
+@pytest.mark.skipif(
+    os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
+)
+def test_docking_omega(docking_input_pair, omega_dense):
+    docker = POSITDocker(use_omega=True, omega_dense=omega_dense)
+    results = docker.dock([docking_input_pair])
+    assert len(results) == 1
+    assert results[0].probability > 0.0
+
+
+def test_docking_omega_dense_fails_no_omega(docking_input_pair):
+    with pytest.raises(ValueError, match="Cannot use omega_dense without use_omega"):
+        _ = POSITDocker(use_omega=False, omega_dense=True)
 
 
 @pytest.mark.skipif(
@@ -19,7 +35,7 @@ def test_docking(docking_input_pair):
 )
 @pytest.mark.parametrize("use_dask", [True, False])
 def test_docking_dask(docking_input_pair, docking_input_pair_simple, use_dask):
-    docker = POSITDocker()
+    docker = POSITDocker(use_omega=False)  # save compute
     results = docker.dock(
         [docking_input_pair, docking_input_pair_simple], use_dask=use_dask
     )
@@ -31,7 +47,7 @@ def test_docking_dask(docking_input_pair, docking_input_pair_simple, use_dask):
     os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
 )
 def test_docking_with_file_write(docking_input_pair_simple, tmp_path):
-    docker = POSITDocker()
+    docker = POSITDocker(use_omega=False)  # save compute
     results = docker.dock([docking_input_pair_simple])
     assert results[0].probability > 0.0
     docker.write_docking_files(results, tmp_path)
@@ -46,7 +62,7 @@ def test_docking_with_file_write(docking_input_pair_simple, tmp_path):
 )
 def test_multireceptor_docking(docking_multi_structure):
     assert len(docking_multi_structure.complexes) == 2
-    docker = POSITDocker()
+    docker = POSITDocker(use_omega=False)  # save compute
     results = docker.dock([docking_multi_structure])
     assert len(results) == 1
     assert results[0].input_pair.complex.target.target_name == "Mpro-x0354"
