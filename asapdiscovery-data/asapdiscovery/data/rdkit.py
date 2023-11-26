4de00b9e85d1ec0c487a73671ae4cc9dad196f88
hmacdope <hugomacdermott@gmail.com>
2023-11-27 09:33:33 +1100

    add docstring

--- asapdiscovery-data/asapdiscovery/data/rdkit.py
+++ asapdiscovery-data/asapdiscovery/data/rdkit.py
@@ -4,6 +4,16 @@ from rdkit import Chem
 def rdkit_smiles_roundtrip(smi: str) -> str:
     """
     Roundtrip a SMILES string through RDKit to canonicalize it
+
+    Parameters
+    ----------
+    smi : str
+        SMILES string to canonicalize
+
+    Returns
+    -------
+    str
+        Canonicalized SMILES string
     """
     mol = Chem.MolFromSmiles(smi)
     return Chem.MolToSmiles(mol)
