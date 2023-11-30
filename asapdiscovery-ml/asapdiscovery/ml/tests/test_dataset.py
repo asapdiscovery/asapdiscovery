13ca2fac4f03f709cbfbacf2045e2f92a92cbe36
kaminow <benjamin.kaminow@choderalab.org>
2023-11-30 11:52:32 -0500

    Add tests for GroupedDockedDataset.

--- asapdiscovery-ml/asapdiscovery/ml/tests/test_dataset.py
+++ asapdiscovery-ml/asapdiscovery/ml/tests/test_dataset.py
@@ -1,7 +1,7 @@
 import pytest
 from asapdiscovery.data.schema_v2.complex import Complex
 from asapdiscovery.data.testing.test_resources import fetch_test_file
-from asapdiscovery.ml.dataset import DockedDataset
+from asapdiscovery.ml.dataset import DockedDataset, GroupedDockedDataset
 
 
 @pytest.fixture(scope="session")
@@ -10,7 +10,7 @@ def complex_pdb():
     return pdb
 
 
-def test_docked_dataset_from_config(complex_pdb):
+def test_docked_dataset_from_complexes(complex_pdb):
     c1 = Complex.from_pdb(
         complex_pdb,
         target_kwargs={"target_name": "test1"},
@@ -30,10 +30,12 @@ def test_docked_dataset_from_config(complex_pdb):
     (xtal_id, compound_id), pose = next(it)
     assert xtal_id == compound_id == "test1"
     assert pose["pos"].shape[0] == len(pose["z"]) == len(pose["lig"])
+    assert pose["pos"].shape[0] > 0
 
     (xtal_id, compound_id), pose = next(it)
     assert xtal_id == compound_id == "test2"
     assert pose["pos"].shape[0] == len(pose["z"]) == len(pose["lig"])
+    assert pose["pos"].shape[0] > 0
 
 
 def test_docked_dataset_from_files(complex_pdb):
@@ -48,7 +50,76 @@ def test_docked_dataset_from_files(complex_pdb):
     (xtal_id, compound_id), pose = next(it)
     assert xtal_id == compound_id == "test1"
     assert pose["pos"].shape[0] == len(pose["z"]) == len(pose["lig"])
+    assert pose["pos"].shape[0] > 0
 
     (xtal_id, compound_id), pose = next(it)
     assert xtal_id == compound_id == "test2"
     assert pose["pos"].shape[0] == len(pose["z"]) == len(pose["lig"])
+    assert pose["pos"].shape[0] > 0
+
+
+def test_grouped_docked_dataset_from_complexes(complex_pdb):
+    c1 = Complex.from_pdb(
+        complex_pdb,
+        target_kwargs={"target_name": "test1"},
+        ligand_kwargs={"compound_name": "test"},
+    )
+    c2 = Complex.from_pdb(
+        complex_pdb,
+        target_kwargs={"target_name": "test2"},
+        ligand_kwargs={"compound_name": "test"},
+    )
+
+    ds = GroupedDockedDataset.from_complexes([c1, c2])
+
+    assert len(ds) == 1
+
+    compound_id, pose_list = next(iter(ds))
+    assert compound_id == "test"
+    assert len(pose_list) == 2
+
+    assert pose_list[0]["compound"] == ("test1", "test")
+    assert (
+        pose_list[0]["pos"].shape[0]
+        == len(pose_list[0]["z"])
+        == len(pose_list[0]["lig"])
+    )
+    assert pose_list[0]["pos"].shape[0] > 0
+
+    assert pose_list[1]["compound"] == ("test2", "test")
+    assert (
+        pose_list[1]["pos"].shape[0]
+        == len(pose_list[1]["z"])
+        == len(pose_list[1]["lig"])
+    )
+    assert pose_list[1]["pos"].shape[0] > 0
+
+
+def test_grouped_docked_dataset_from_files(complex_pdb):
+    ds = GroupedDockedDataset.from_files(
+        str_fns=[complex_pdb, complex_pdb],
+        compounds=[("test1", "test"), ("test2", "test")],
+        ignore_h=False,
+    )
+
+    assert len(ds) == 1
+
+    compound_id, pose_list = next(iter(ds))
+    assert compound_id == "test"
+    assert len(pose_list) == 2
+
+    assert pose_list[0]["compound"] == ("test1", "test")
+    assert (
+        pose_list[0]["pos"].shape[0]
+        == len(pose_list[0]["z"])
+        == len(pose_list[0]["lig"])
+    )
+    assert pose_list[0]["pos"].shape[0] > 0
+
+    assert pose_list[1]["compound"] == ("test2", "test")
+    assert (
+        pose_list[1]["pos"].shape[0]
+        == len(pose_list[1]["z"])
+        == len(pose_list[1]["lig"])
+    )
+    assert pose_list[1]["pos"].shape[0] > 0
