df8f378a929e9d61b368ad7cb324c87f1b44afd2
kaminow <benjamin.kaminow@choderalab.org>
2024-01-02 15:18:02 -0500

    Add integration test for building a graph DatasetConfig.

---
+++ asapdiscovery-ml/asapdiscovery/ml/tests/test_cli.py
@@ -0,0 +1,67 @@
+import json
+import pickle as pkl
+
+from click.testing import CliRunner
+import pytest
+from asapdiscovery.data.testing.test_resources import fetch_test_file
+from asapdiscovery.ml.cli import cli
+from asapdiscovery.ml.schema_v2.config import DatasetConfig, DatasetType
+
+
+@pytest.fixture(scope="session")
+def exp_file():
+    exp_file = fetch_test_file("ml_testing/exp_file.json")
+    return exp_file
+
+
+@pytest.fixture(scope="session")
+def docked_files():
+    docked_files = fetch_test_file(
+        [
+            "ml_testing/docked/AAR-POS-5507155c-1_Mpro-P0018_0A_0_bound_best.pdb",
+            "ml_testing/docked/AAR-RCN-390aeb1f-1_Mpro-P3074_0A_0_bound_best.pdb",
+            "ml_testing/docked/AAR-RCN-67438d21-1_Mpro-P3074_0A_0_bound_best.pdb",
+            "ml_testing/docked/AAR-POS-d2a4d1df-27_Mpro-P0238_0A_0_bound_best.pdb",
+            "ml_testing/docked/AAR-POS-8a4e0f60-7_Mpro-P0053_0A_0_bound_best.pdb",
+            "ml_testing/docked/AAR-RCN-28a8122f-1_Mpro-P2005_0A_0_bound_best.pdb",
+            "ml_testing/docked/AAR-RCN-37d0aa00-1_Mpro-P3074_0A_0_bound_best.pdb",
+            "ml_testing/docked/AAR-RCN-748c104b-1_Mpro-P3074_0A_0_bound_best.pdb",
+            "ml_testing/docked/AAR-RCN-521d1733-1_Mpro-P2005_0A_0_bound_best.pdb",
+            "ml_testing/docked/AAR-RCN-845f9611-1_Mpro-P2005_0A_0_bound_best.pdb",
+        ]
+    )
+    return docked_files
+
+
+def test_build_ds_graph(exp_file, tmp_path):
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "build-dataset",
+            "gat",
+            "--exp-file",
+            exp_file,
+            "--ds-cache",
+            tmp_path / "ds_cache.pkl",
+            "--ds-config-cache",
+            tmp_path / "ds_config_cache.json",
+        ],
+    )
+    assert result.exit_code == 0
+
+    # Make sure files exist
+    ds_cache = tmp_path / "ds_cache.pkl"
+    ds_config_cache = tmp_path / "ds_config_cache.json"
+    assert ds_cache.exists()
+    assert ds_config_cache.exists()
+
+    # Load and check stuff
+    ds = pkl.loads(ds_cache.read_bytes())
+    assert len(ds) == 10
+
+    ds_config = DatasetConfig(**json.loads(ds_config_cache.read_text()))
+    assert ds_config.ds_type == "graph"
+    assert len(ds_config.input_data) == 10
+    assert ds_config.cache_file == ds_cache
+    assert not ds_config.overwrite
