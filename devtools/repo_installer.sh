d73cc6de9e895d611526fe63f8ccd5df7f2fb82a
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-01-10 14:59:59 -0500

    add bash script that installs the subpackages in the right order

---
+++ devtools/repo_installer.sh
@@ -0,0 +1,12 @@
+# Edited 2024.01.10 Alex Payne
+# I can never remember the order to install the asapdiscovery packages in,
+# so I'm writing a script to do it for me.
+# this isn't necessarily the *only* correct order, but it is an order that works
+pip install -e asapdiscovery-data
+pip install -e asapdiscovery-cli
+pip install -e asapdiscovery-simulation
+pip install -e asapdiscovery-modeling
+pip install -e asapdiscovery-dataviz
+pip install -e asapdiscovery-docking
+pip install -e asapdiscovery-ml
+pip install -e asapdiscovery-alchemy
