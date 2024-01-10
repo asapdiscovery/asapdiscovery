fe0bf80f71ba4eb022cfd32d114fcd557e22a1f5
David Dotson <dotsdl@gmail.com>
2024-01-10 11:55:22 -0700

    Added guardrail to CLI for ligands requiring names

--- asapdiscovery-alchemy/asapdiscovery/alchemy/cli/alchemy.py
+++ asapdiscovery-alchemy/asapdiscovery/alchemy/cli/alchemy.py
@@ -107,6 +107,12 @@ def plan(
             openfe.SmallMoleculeComponent.from_sdf_string(mol.to_sdf_str())
             for mol in alchemy_ds.posed_ligands
         ]
+
+        # if any ligands lack a name, then raise an exception; important for
+        # ligands to have names for human-readable result gathering downstream
+        if missing := len([l for l in input_ligands if not l.name]):
+            raise ValueError(f"{missing} of {len(input_ligands)} ligands do not have names; names are required for ligands for downstream results handling")
+
         # write to a temp pdb file and read back in
         with tempfile.NamedTemporaryFile(suffix=".pdb") as fp:
             alchemy_ds.reference_complex.target.to_pdb_file(fp.name)
