2b8641fff6cfee19f45cd77d09d8eab3005c0dd0
kaminow <benjamin.kaminow@choderalab.org>
2024-01-16 15:36:40 -0500

    Add new Jitter class that adds noise based on a temperature/B factor tensor.

--- asapdiscovery-ml/asapdiscovery/ml/data_augmentation.py
+++ asapdiscovery-ml/asapdiscovery/ml/data_augmentation.py
@@ -89,3 +89,95 @@ class JitterFixed:
             return dict_copy
         else:
             return coords_copy
+
+
+class JitterVariable:
+    """
+    Jitter an input pose by drawing noise from a normal distribution that is based on a
+    property in the passed pose. Noise is generated per-atom.
+    """
+
+    def __init__(
+        self,
+        mean: callable,
+        std: callable,
+        rand_seed: int | None = None,
+        pos_dict_key: str = "pos",
+        temp_dict_key: str = "b",
+    ):
+        """
+        Parameters
+        ----------
+        mean : callable
+            Mean of noise distribution. The function will be used to transform the value
+            stored in the temperature information into a mean for a normal distribution
+        std : callable
+            Standard deviation of noise distribution. The function will be used to
+            transform the value stored in the temperature information into a standard
+            deviation for a normal distribution
+        rand_seed : int, optional
+            Random seed for noise generation
+        pos_dict_key : str, default="pos"
+            Key to access the coords in input dict
+        temp_dict_key : str, default="b"
+            Key to access temperature/B factors in input dict
+        """
+
+        self.mean = mean
+        self.std = std
+        self.rand_seed = rand_seed
+        if rand_seed is not None:
+            self.g = torch.Generator().manual_seed(rand_seed)
+        else:
+            self.g = torch.Generator().manual_seed(torch.random.seed())
+        self.pos_dict_key = pos_dict_key
+        self.temp_dict_key = temp_dict_key
+
+    def __call__(self, pose, inplace=False):
+        """
+        Apply noise to each atom. Unless inplace is True, this method will create a copy
+        of the input coordinate Tensor.
+
+        Parameters
+        ----------
+        pose : dict
+            Pose dict containing (at minimum) the initial coordinates to be jittered and
+            the temperature information. Noise will be generated independently for each
+            atom
+        inplace : bool, default=False
+            Modify the passed dict in place, rather than first copying
+
+        Returns
+        -------
+        dict
+            Copy of the input pose with jittered coordinates
+        """
+
+        # Fist make a copy of the input pose (if inplace is False)
+        if not inplace:
+            pose_copy = deepcopy(pose)
+        else:
+            # Should just be a reference so inputs should get modified
+            pose_copy = pose
+
+        # Calculate mean and std values. Need to do some reshaping/broadcasting to make
+        #  the shapes line up
+        mean = (
+            self.mean(pose_copy[self.temp_dict_key])
+            .reshape((-1, 1))
+            .broadcast_to(pose_copy[self.pos_dict_key].shape)
+            .to("cpu")
+        )
+        std = (
+            self.std(pose_copy[self.temp_dict_key])
+            .reshape((-1, 1))
+            .broadcast_to(pose_copy[self.pos_dict_key].shape)
+            .to("cpu")
+        )
+
+        # Generate noise (the std will be broadcast to the same shape as mean)
+        noise = torch.normal(mean=mean, std=std, generator=self.g)
+        # Add the noise
+        pose_copy[self.pos_dict_key] += noise.to(pose_copy[self.pos_dict_key].device)
+
+        return pose_copy
