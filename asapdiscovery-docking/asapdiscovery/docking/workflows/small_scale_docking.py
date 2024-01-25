a434cbe3270c31342f52e7d7ffebba96d80055e5
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2024-01-25 15:46:28 -0500

    fix import

--- asapdiscovery-docking/asapdiscovery/docking/workflows/small_scale_docking.py
+++ asapdiscovery-docking/asapdiscovery/docking/workflows/small_scale_docking.py
@@ -47,8 +47,7 @@ from asapdiscovery.docking.scorer_v2 import ChemGauss4Scorer, MetaScorer, MLMode
 from asapdiscovery.docking.workflows.workflows import PosteraDockingWorkflowInputs
 from asapdiscovery.ml.models import ASAPMLModelRegistry
 from asapdiscovery.modeling.protein_prep_v2 import ProteinPrepper
-from asapdiscovery.simulation.simulate import OpenMMPlatform
-from asapdiscovery.simulation.simulate_v2 import VanillaMDSimulatorV2
+from asapdiscovery.simulation.simulate_v2 import VanillaMDSimulatorV2, OpenMMPlatform
 from distributed import Client
 from pydantic import Field, PositiveInt, root_validator, validator
 
