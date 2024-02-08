9d41c6c6692b2a7ef93d55d2d2d4a1c9e0ed88c9
hmacdope <hugomacdermott@gmail.com>
2024-02-08 21:52:56 +1100

    skip sim test on Mac

--- asapdiscovery-simulation/asapdiscovery/simulation/tests/test_simulate.py
+++ asapdiscovery-simulation/asapdiscovery/simulation/tests/test_simulate.py
@@ -1,6 +1,9 @@
+import pytest
 from asapdiscovery.simulation.simulate import VanillaMDSimulator
 
-
+@pytest.mark.skipif(
+    os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
+)
 def test_actual_simulation(results, tmp_path):
     vs = VanillaMDSimulator(
         num_steps=1, equilibration_steps=1, output_dir=tmp_path, truncate_steps=False
