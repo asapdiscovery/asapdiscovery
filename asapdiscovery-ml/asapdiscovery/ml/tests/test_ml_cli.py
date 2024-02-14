89a260d819970daea7a1454a830a195d95085e12
fyng <feiyangthegoat@gmail.com>
2024-02-14 12:45:54 -0500

    add visnet tests

--- asapdiscovery-ml/asapdiscovery/ml/tests/test_ml_cli.py
+++ asapdiscovery-ml/asapdiscovery/ml/tests/test_ml_cli.py
@@ -147,6 +147,45 @@ def test_build_ds_e3nn(exp_file, docked_files, tmp_path):
     assert ds_config.for_e3nn
     assert not ds_config.overwrite
 
+def test_build_ds_visnet(exp_file, docked_files, tmp_path):
+    docked_dir = docked_files[0].parent
+
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "build-dataset",
+            "visnet",
+            "--exp-file",
+            exp_file,
+            "--structures",
+            str(docked_dir),
+            "--ds-cache",
+            tmp_path / "ds_cache.pkl",
+            "--ds-config-cache",
+            tmp_path / "ds_config_cache.json",
+        ],
+    )
+    assert result.exit_code == 0
+
+    # Make sure files exist
+    ds_cache = tmp_path / "ds_cache.pkl"
+    ds_config_cache = tmp_path / "ds_config_cache.json"
+    assert ds_cache.exists()
+    assert ds_config_cache.exists()
+
+    # Load and check stuff
+    ds = pkl.loads(ds_cache.read_bytes())
+    assert len(ds) == 10
+
+    ds_config = DatasetConfig(**json.loads(ds_config_cache.read_text()))
+    assert ds_config.ds_type == "structural"
+    assert len(ds_config.input_data) == 10
+    assert ds_config.cache_file == ds_cache
+    assert not ds_config.grouped
+    assert ds_config.for_e3nn
+    assert not ds_config.overwrite
+
 
 def test_build_trainer_graph(exp_file, tmp_path):
     runner = CliRunner()
@@ -329,6 +368,67 @@ def test_build_trainer_e3nn(exp_file, docked_files, tmp_path):
     assert t.model_config.irreps_hidden == "5x0o+5x0e"
 
 
+def test_build_trainer_visnet(exp_file, docked_files, tmp_path):
+    docked_dir = docked_files[0].parent
+
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "build",
+            "visnet",
+            "--output-dir",
+            tmp_path / "model_out",
+            "--trainer-config-cache",
+            tmp_path / "trainer.json",
+            "--ds-split-type",
+            "temporal",
+            "--exp-file",
+            exp_file,
+            "--structures",
+            str(docked_dir),
+            "--ds-cache",
+            tmp_path / "ds_cache.pkl",
+            "--ds-config-cache",
+            tmp_path / "ds_config_cache.json",
+            "--loss-type",
+            "mse_step",
+            "--device",
+            "cpu",
+            "--n-epochs",
+            "1",
+            "--use-wandb",
+            "False",
+        ],
+    )
+    assert result.exit_code == 0
+
+    # Make sure the right files exist
+    trainer_config_cache = tmp_path / "trainer.json"
+    output_dir = tmp_path / "model_out"
+    assert trainer_config_cache.exists()
+    assert not output_dir.exists()
+    assert not (tmp_path / "ds_cache.pkl").exists()
+    assert (tmp_path / "ds_config_cache.json").exists()
+
+    # Load and check stuff
+    t = Trainer(**json.loads(trainer_config_cache.read_text()))
+    assert t.n_epochs == 1
+    assert t.output_dir == output_dir
+    assert not t.use_wandb
+    assert not t._is_initialized
+    assert not hasattr(t, "model")
+    assert not hasattr(t, "optimizer")
+    assert not hasattr(t, "es")
+    assert not hasattr(t, "ds")
+    assert not hasattr(t, "ds_train")
+    assert not hasattr(t, "ds_val")
+    assert not hasattr(t, "ds_test")
+    assert not hasattr(t, "loss_func")
+
+    assert not t.ds_config.for_e3nn
+
+
 def test_build_and_train_graph(exp_file, tmp_path):
     runner = CliRunner()
     result = runner.invoke(
@@ -452,6 +552,67 @@ def test_build_and_train_e3nn(exp_file, docked_files, tmp_path):
         [
             "build-and-train",
             "e3nn",
+            "--output-dir",
+            tmp_path / "model_out",
+            "--trainer-config-cache",
+            tmp_path / "trainer.json",
+            "--ds-split-type",
+            "temporal",
+            "--exp-file",
+            exp_file,
+            "--structures",
+            str(docked_dir),
+            "--ds-cache",
+            tmp_path / "ds_cache.pkl",
+            "--ds-config-cache",
+            tmp_path / "ds_config_cache.json",
+            "--irreps-hidden",
+            "0:5",
+            "--loss-type",
+            "mse_step",
+            "--device",
+            "cpu",
+            "--n-epochs",
+            "1",
+            "--use-wandb",
+            "False",
+        ],
+    )
+    # assert result.exit_code == 0
+    if result.exit_code:
+        raise result.exception
+
+    # Make sure the right files exist
+    trainer_config_cache = tmp_path / "trainer.json"
+    output_dir = tmp_path / "model_out"
+    loss_dict_file = output_dir / "loss_dict.json"
+    assert trainer_config_cache.exists()
+    assert output_dir.exists()
+    assert (output_dir / "init.th").exists()
+    for i in range(1):
+        assert (output_dir / f"{i}.th").exists()
+    assert (output_dir / "final.th").exists()
+    assert loss_dict_file.exists()
+    assert (tmp_path / "ds_cache.pkl").exists()
+    assert (tmp_path / "ds_config_cache.json").exists()
+
+    # Load and check stuff
+    loss_dict = json.loads(loss_dict_file.read_text())
+    assert {"train", "test", "val"} == set(loss_dict.keys())
+    print({k: len(v) for k, v in loss_dict.items()}, flush=True)
+    assert len(loss_dict["train"]) == 8
+    assert len(loss_dict["val"]) == 1
+    assert len(loss_dict["test"]) == 1
+
+def test_build_and_train_visnet(exp_file, docked_files, tmp_path):
+    docked_dir = docked_files[0].parent
+
+    runner = CliRunner()
+    result = runner.invoke(
+        cli,
+        [
+            "build-and-train",
+            "visnet",
             "--output-dir",
             tmp_path / "model_out",
             "--trainer-config-cache",
