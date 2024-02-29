da9337153a16869a0d679b7fb4f675e2a600a563
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-02-29 11:39:36 -0500

    remove unused import

--- asapdiscovery-docking/asapdiscovery/docking/schema/pose_generation.py
+++ asapdiscovery-docking/asapdiscovery/docking/schema/pose_generation.py
@@ -5,7 +5,6 @@ from typing import Any, Literal, Optional
 from asapdiscovery.data.backend.openeye import (
     _get_SD_data,
     _set_SD_data,
-    get_SD_data,
     oechem,
     oedocking,
     oeff,
