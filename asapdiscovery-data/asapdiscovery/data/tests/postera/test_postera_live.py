77883f5e034999c4dcc53aeb62ef1490cac75982
hmacdope <hugomacdermott@gmail.com>
2024-03-20 08:21:54 +1100

    try fix for changed API backend

--- asapdiscovery-data/asapdiscovery/data/tests/postera/test_postera_live.py
+++ asapdiscovery-data/asapdiscovery/data/tests/postera/test_postera_live.py
@@ -3,6 +3,7 @@ import random
 from hashlib import sha256
 from time import sleep
 from uuid import uuid4
+from requests.exceptions import HTTPError
 
 import numpy as np
 import pandas as pd
@@ -19,6 +20,7 @@ from asapdiscovery.data.services.postera.molecule_set import (
 from asapdiscovery.data.services.postera.postera_factory import PosteraFactory
 from asapdiscovery.data.services.postera.postera_uploader import PosteraUploader
 from asapdiscovery.data.services.services_config import PosteraSettings
+
 
 # WARNING IMPORTANT: - this is a live test and will make real requests to the POSTERA API
 # A sanboxed API key is required to run this test, DO NOT USE A PRODUCTION API KEY
@@ -102,7 +104,8 @@ class TestPosteraLive:
     def test_garbage_api_key(self):
         postera_settings_garbage = PosteraSettings(POSTERA_API_KEY="garbage_api")
         ms_api = MoleculeSetAPI.from_settings(postera_settings_garbage)
-        assert ms_api.list_available() == {}
+        with pytest.raises(HTTPError):
+            ms_api.list_available()
 
     def test_get(self, live_postera_ms_api_instance, simple_moleculeset):
         molecule_set_name, uuid = simple_moleculeset
