e16aaee606cc37c0dc2388a7f379fb05cf10570b
hmacdope <hugomacdermott@gmail.com>
2024-03-20 09:34:54 +1100

    first attempt

---
+++ asapdiscovery-workflows/asapdiscovery/workflows/tests/cli/test_docking_cli.py
@@ -0,0 +1,216 @@
+import os
+import traceback
+from unittest import mock
+
+import pytest
+from asapdiscovery.docking.cli import docking as cli
+from asapdiscovery.docking.docking import DockingResult
+from asapdiscovery.simulation.simulate import SimulationResult, VanillaMDSimulator
+from click.testing import CliRunner
+
+
+def click_success(result):
+    if result.exit_code != 0:  # -no-cov-  (only occurs on test error)
+        print(result.output)
+        traceback.print_tb(result.exc_info[2])
+        print(result.exc_info[0], result.exc_info[1])
+    return result.exit_code == 0
+
+
+@pytest.mark.skipif(
+    os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
+)
+@pytest.mark.skipif(os.getenv("SKIP_EXPENSIVE_TESTS"), reason="Expensive tests skipped")
+@pytest.mark.parametrize("subcommand", ["large-scale", "small-scale"])
+def test_project_support_docking_cli_fragalysis(
+    ligand_file, mpro_frag_dir, tmp_path, subcommand
+):
+    runner = CliRunner()
+    frag_parent_dir, _ = mpro_frag_dir
+    args = [
+        subcommand,
+        "--target",
+        "SARS-CoV-2-Mpro",
+        "--ligands",
+        ligand_file,
+        "--fragalysis-dir",
+        frag_parent_dir,
+        "--posit-confidence-cutoff",
+        0,
+        "--output-dir",
+        tmp_path,
+    ]
+    if (
+        subcommand == "small-scale"
+    ):  # turn off dask cuda overrides for CI runners which lack GPUs
+        args.extend(["--no-allow-dask-cuda"])
+    result = runner.invoke(cli, args)
+    assert click_success(result)
+
+
+@pytest.mark.skipif(
+    os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
+)
+@pytest.mark.skipif(os.getenv("SKIP_EXPENSIVE_TESTS"), reason="Expensive tests skipped")
+@pytest.mark.parametrize("subcommand", ["large-scale", "small-scale"])
+def test_project_support_docking_cli_structure_directory_dask(
+    ligand_file, structure_dir, tmp_path, subcommand
+):
+    runner = CliRunner()
+    struct_dir, _ = structure_dir
+    args = [
+        subcommand,
+        "--target",
+        "SARS-CoV-2-Mpro",
+        "--ligands",
+        ligand_file,
+        "--structure-dir",
+        struct_dir,
+        "--use-dask",  # add dask
+        "--posit-confidence-cutoff",
+        0,
+        "--output-dir",
+        tmp_path,
+    ]
+    if (
+        subcommand == "small-scale"
+    ):  # turn off dask cuda overrides for CI runners which lack GPUs
+        args.extend(["--no-allow-dask-cuda"])
+    result = runner.invoke(cli, args)
+    assert click_success(result)
+
+
+@pytest.mark.skipif(
+    os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
+)
+@pytest.mark.skipif(os.getenv("SKIP_EXPENSIVE_TESTS"), reason="Expensive tests skipped")
+@pytest.mark.parametrize("subcommand", ["large-scale", "small-scale"])
+def test_project_support_docking_cli_structure_directory_du_cache_dask(
+    ligand_file, structure_dir, du_cache, tmp_path, subcommand
+):
+    runner = CliRunner()
+    struct_dir, _ = structure_dir
+    du_cache_dir, _ = du_cache
+    args = [
+        subcommand,
+        "--target",
+        "SARS-CoV-2-Mpro",
+        "--ligands",
+        ligand_file,
+        "--structure-dir",
+        struct_dir,
+        "--use-dask",
+        "--posit-confidence-cutoff",
+        0,
+        "--cache-dir",
+        du_cache_dir,
+        "--output-dir",
+        tmp_path,
+    ]
+    if (
+        subcommand == "small-scale"
+    ):  # turn off dask cuda overrides for CI runners which lack GPUs
+        args.extend(["--no-allow-dask-cuda"])
+    result = runner.invoke(cli, args)
+    assert click_success(result)
+
+
+@pytest.mark.skipif(
+    os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
+)
+@pytest.mark.skipif(os.getenv("SKIP_EXPENSIVE_TESTS"), reason="Expensive tests skipped")
+@pytest.mark.parametrize("subcommand", ["large-scale", "small-scale"])
+def test_project_support_docking_cli_pdb_file_dask(
+    ligand_file, pdb_file, tmp_path, subcommand
+):
+    runner = CliRunner()
+    args = [
+        subcommand,
+        "--target",
+        "SARS-CoV-2-Mpro",
+        "--ligands",
+        ligand_file,
+        "--pdb-file",
+        pdb_file,
+        "--use-dask",
+        "--posit-confidence-cutoff",
+        0,
+        "--output-dir",
+        tmp_path,
+    ]
+    if (
+        subcommand == "small-scale"
+    ):  # turn off dask cuda overrides for CI runners which lack GPUs
+        args.extend(["--no-allow-dask-cuda"])
+    result = runner.invoke(cli, args)
+    assert click_success(result)
+
+
+@pytest.mark.skipif(
+    os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
+)
+@pytest.mark.skipif(os.getenv("SKIP_EXPENSIVE_TESTS"), reason="Expensive tests skipped")
+def test_small_scale_docking_md(ligand_file, pdb_file, tmp_path, simulation_results):
+    runner = CliRunner()
+
+    def _simulate_patch(
+        self, docking_results: list[DockingResult]
+    ) -> list[SimulationResult]:
+        return [simulation_results]
+
+    # NB: cannot use dask for below test as patch will not survive pickling and transfer to worker
+
+    with mock.patch.object(VanillaMDSimulator, "_simulate", _simulate_patch):
+        result = runner.invoke(
+            cli,
+            [
+                "small-scale",
+                "--target",
+                "SARS-CoV-2-Mpro",
+                "--ligands",
+                ligand_file,
+                "--pdb-file",
+                pdb_file,
+                "--no-allow-dask-cuda",
+                "--posit-confidence-cutoff",
+                0,
+                "--output-dir",
+                tmp_path,
+                "--md",
+                "--md-steps",
+                1,
+                "--md-openmm-platform",
+                "CPU",
+            ],
+        )
+    assert click_success(result)
+
+
+@pytest.mark.skipif(
+    os.getenv("RUNNER_OS") == "macOS", reason="Docking tests slow on GHA on macOS"
+)
+@pytest.mark.skipif(os.getenv("SKIP_EXPENSIVE_TESTS"), reason="Expensive tests skipped")
+def test_cross_docking_cli_structure_directory_du_cache(
+    ligand_file, structure_dir, du_cache, tmp_path
+):
+    runner = CliRunner()
+    struct_dir, _ = structure_dir
+    du_cache_dir, _ = du_cache
+    result = runner.invoke(
+        cli,
+        [
+            "cross-docking",
+            "--target",
+            "SARS-CoV-2-Mpro",
+            "--ligands",
+            ligand_file,
+            "--structure-dir",
+            struct_dir,
+            "--use-dask",
+            "--cache-dir",
+            du_cache_dir,
+            "--output-dir",
+            tmp_path,
+        ],
+    )
+    assert click_success(result)
---
+++ asapdiscovery-workflows/asapdiscovery/workflows/tests/conftest.py
@@ -0,0 +1,97 @@
+import pytest
+from asapdiscovery.data.backend.openeye import oechem
+from asapdiscovery.data.schema.complex import PreppedComplex
+from asapdiscovery.data.schema.ligand import Ligand
+from asapdiscovery.data.testing.test_resources import fetch_test_file
+from asapdiscovery.docking.docking import DockingInputMultiStructure, DockingInputPair
+from asapdiscovery.docking.openeye import POSITDockingResults
+
+
+@pytest.fixture()
+def ligand():
+    return Ligand.from_sdf(
+        fetch_test_file("Mpro-P0008_0A_ERI-UCB-ce40166b-17.sdf"), compound_name="test"
+    )
+
+
+@pytest.fixture()
+def ligand_simple():
+    return Ligand.from_smiles("CCCOCO", compound_name="test2")
+
+
+@pytest.fixture()
+def prepped_complex():
+    return PreppedComplex.from_oedu_file(
+        fetch_test_file("Mpro-P2660_0A_bound-prepped_receptor.oedu"),
+        ligand_kwargs={"compound_name": "test"},
+        target_kwargs={"target_name": "test", "target_hash": "mock_hash"},
+    )
+
+
+@pytest.fixture()
+def prepped_complexes():
+    cached_dus = {
+        "Mpro-x1002": "du_cache/Mpro-x1002_0A_bound.oedu",
+        "Mpro-x0354": "du_cache/Mpro-x0354_0A_bound.oedu",
+    }
+    return [
+        PreppedComplex.from_oedu_file(
+            fetch_test_file(cached_du),
+            ligand_kwargs={"compound_name": "test"},
+            target_kwargs={"target_name": name, "target_hash": "mock_hash"},
+        )
+        for name, cached_du in cached_dus.items()
+    ]
+
+
+@pytest.fixture()
+def docking_input_pair(ligand, prepped_complex):
+    return DockingInputPair(complex=prepped_complex, ligand=ligand)
+
+
+@pytest.fixture()
+def docking_input_pair_simple(ligand_simple, prepped_complex):
+    return DockingInputPair(complex=prepped_complex, ligand=ligand_simple)
+
+
+@pytest.fixture()
+def docking_multi_structure(prepped_complexes, ligand):
+    return DockingInputMultiStructure(complexes=prepped_complexes, ligand=ligand)
+
+
+@pytest.fixture()
+def results():  # precomputed results
+    return [POSITDockingResults.from_json_file(fetch_test_file("docking_results.json"))]
+
+
+@pytest.fixture()
+def results_simple():
+    return [
+        POSITDockingResults.from_json_file(
+            fetch_test_file("docking_results_simple.json")
+        )
+    ]
+
+
+@pytest.fixture()
+def results_multi(results, results_simple):
+    return results + results_simple
+
+
+@pytest.fixture()
+def mol_with_constrained_confs() -> oechem.OEMol:
+    """Load a multiconformer OEMol from an sdf"""
+    mol = oechem.OEMol()
+    ifs = oechem.oemolistream(
+        str(fetch_test_file("constrained_conformer/ASAP-0008650.sdf"))
+    )
+    ifs.SetConfTest(oechem.OEIsomericConfTest())
+    oechem.OEReadMolecule(ifs, mol)
+    return mol
+
+
+@pytest.fixture()
+def mac1_complex():
+    return PreppedComplex.parse_file(
+        fetch_test_file("constrained_conformer/complex.json")
+    )
