ed0189104212b044ad46d4e0c1a36ff1776bd1a9
hmacdope <hugomacdermott@gmail.com>
2024-04-08 10:54:56 +1000

    dispatch working on gif viz

--- asapdiscovery-dataviz/asapdiscovery/dataviz/tests/test_gif_viz_v2.py
+++ asapdiscovery-dataviz/asapdiscovery/dataviz/tests/test_gif_viz_v2.py
@@ -7,3 +7,10 @@ def test_gif_viz_v2(tmp_path, simulation_results, use_dask):
     gv = GIFVisualizerV2(target="SARS-CoV-2-Mpro", output_dir=tmp_path)
     vizs = gv.visualize(inputs=simulation_results, use_dask=use_dask)
     assert len(vizs) == 1
+
+
+@pytest.mark.parametrize("use_dask", [True, False])
+def test_gif_viz_v2_paths(tmp_path, traj, top, use_dask):
+    gv = GIFVisualizerV2(target="SARS-CoV-2-Mpro", output_dir=tmp_path)
+    vizs = gv.visualize(inputs=[(traj, top)], use_dask=use_dask)
+    assert len(vizs) == 1
--- asapdiscovery-dataviz/asapdiscovery/dataviz/viz_v2/gif_viz.py
+++ asapdiscovery-dataviz/asapdiscovery/dataviz/viz_v2/gif_viz.py
@@ -1,6 +1,8 @@
 import logging
 import shutil
 from pathlib import Path
+from typing import Tuple, Any
+from multimethod import multimethod
 
 from asapdiscovery.data.metadata.resources import master_structures
 from asapdiscovery.data.services.postera.manifold_data_validation import (
@@ -8,6 +10,8 @@ from asapdiscovery.data.services.postera.manifold_data_validation import (
     TargetTags,
 )
 from asapdiscovery.data.util.dask_utils import dask_vmap
+from asapdiscovery.data.schema.complex import Complex
+from asapdiscovery.data.schema.pairs import CompoundStructurePair
 from asapdiscovery.dataviz._gif_blocks import GIFBlockData
 from asapdiscovery.dataviz.gif_viz import add_gif_progress_bar
 from asapdiscovery.dataviz.show_contacts import show_contacts
@@ -53,215 +57,236 @@ class GIFVisualizerV2(VisualizerBase):
         arbitrary_types_allowed = True
 
     @dask_vmap(["inputs"])
-    def _visualize(
-        self, inputs: list[SimulationResult], **kwargs
-    ) -> list[dict[str, str]]:
+    def _visualize(self, inputs: list[Any], **kwargs) -> list[dict[str, str]]:
+        return self._dispatch(inputs, **kwargs)
+
+    def provenance(self):
+        return {}
+
+    @staticmethod
+    def pymol_traj_viz(
+        target: str,
+        traj: Path,
+        system: Path,
+        out_dir: Path,
+        static_view_only: bool,
+        pse: bool,
+        pse_share: bool,
+        start: int,
+        stop: int,
+        interval: int,
+        smooth: int,
+        contacts: bool,
+        frames_per_ns: int,
+    ):
         view_coords = GIFBlockData.get_view_coords()
 
-        pocket_dict = GIFBlockData.get_pocket_dict(self.target)
+        pocket_dict = GIFBlockData.get_pocket_dict(target)
 
-        color_dict = GIFBlockData.get_color_dict(self.target)
+        color_dict = GIFBlockData.get_color_dict(target)
+        import pymol2
 
-        data = []
-        for res in inputs:
-            traj = res.traj_path
-            system = res.minimized_pdb_path
-            # NOTE very important, need to spawn a new pymol proc for each trajectory
-            # when working in parallel, otherwise they will trip over each other and not work.
+        p = pymol2.PyMOL()
+        p.start()
+        out_dir.mkdir(parents=True, exist_ok=True)
+        path = out_dir / "trajectory.gif"
 
-            import pymol2
+        tmpdir = out_dir / "tmp"
+        tmpdir.mkdir(parents=True, exist_ok=True)
 
-            p = pymol2.PyMOL()
-            p.start()
+        complex_name = "complex"
+        p.cmd.load(str(system), object=complex_name)
 
-            out_dir = self.output_dir / res.input_docking_result.unique_name
-            out_dir.mkdir(parents=True, exist_ok=True)
-            path = out_dir / "trajectory.gif"
+        if static_view_only:
+            # this may be unprepped/unaligned, so need to align to master structure before writing out.
+            reference_structure = master_structures[target]
+            p.cmd.load(reference_structure, object="reference_master")
+            p.cmd.align(complex_name, "reference_master")
+            p.cmd.delete("reference_master")
 
-            tmpdir = out_dir / "tmp"
-            tmpdir.mkdir(parents=True, exist_ok=True)
+        if pse:
+            p.cmd.save(str(out_dir / "session_1_loaded_system.pse"))
 
-            complex_name = "complex"
-            p.cmd.load(str(system), object=complex_name)
-
-            if self.static_view_only:
-                # this may be unprepped/unaligned, so need to align to master structure before writing out.
-                reference_structure = master_structures[self.target]
-                p.cmd.load(reference_structure, object="reference_master")
-                p.cmd.align(complex_name, "reference_master")
-                p.cmd.delete("reference_master")
-
-            if self.pse:
-                p.cmd.save(str(out_dir / "session_1_loaded_system.pse"))
-
-            # now select the residues, name them and color them.
-            for subpocket_name, residues in pocket_dict.items():
-                p.cmd.select(
-                    subpocket_name,
-                    f"{complex_name} and resi {residues} and polymer.protein",
-                )
-
-            for subpocket_name, color in color_dict.items():
-                p.cmd.set("surface_color", color, f"({subpocket_name})")
-
-            if self.pse:
-                p.cmd.save(str(out_dir / "session_2_colored_subpockets.pse"))
-
-            # Select ligand and receptor
-            p.cmd.select("ligand", "resn UNK or resn LIG")
+        # now select the residues, name them and color them.
+        for subpocket_name, residues in pocket_dict.items():
             p.cmd.select(
-                "receptor", "chain A or chain B or chain 1 or chain 2"
-            )  # TODO: Modify this to generalize to dimer
-            p.cmd.select(
-                "binding_site",
-                "name CA within 7 of resn UNK or name CA within 7 resn LIG",
-            )  # automate selection of the binding site
+                subpocket_name,
+                f"{complex_name} and resi {residues} and polymer.protein",
+            )
 
-            # set a bunch of stuff for visualization
-            p.cmd.set("bg_rgb", "white")
-            p.cmd.set("surface_color", "grey90")
-            p.cmd.bg_color("white")
-            p.cmd.hide("everything")
-            p.cmd.show("cartoon")
-            p.cmd.show("surface", "receptor")
-            p.cmd.set("surface_mode", 3)
-            p.cmd.set("cartoon_color", "grey")
-            p.cmd.set("transparency", 0.3)
-            p.cmd.hide(
-                "surface", "ligand"
-            )  # for some reason sometimes a ligand surface is applied - hide this.
+        for subpocket_name, color in color_dict.items():
+            p.cmd.set("surface_color", color, f"({subpocket_name})")
 
-            # select the ligand and subpocket residues, show them as sticks w/o nonpolar Hs
-            p.cmd.select("resn UNK or resn LIG")
+        if pse:
+            p.cmd.save(str(out_dir / "session_2_colored_subpockets.pse"))
+
+        # Select ligand and receptor
+        p.cmd.select("ligand", "resn UNK or resn LIG")
+        p.cmd.select(
+            "receptor", "chain A or chain B or chain 1 or chain 2"
+        )  # TODO: Modify this to generalize to dimer
+        p.cmd.select(
+            "binding_site",
+            "name CA within 7 of resn UNK or name CA within 7 resn LIG",
+        )  # automate selection of the binding site
+
+        # set a bunch of stuff for visualization
+        p.cmd.set("bg_rgb", "white")
+        p.cmd.set("surface_color", "grey90")
+        p.cmd.bg_color("white")
+        p.cmd.hide("everything")
+        p.cmd.show("cartoon")
+        p.cmd.show("surface", "receptor")
+        p.cmd.set("surface_mode", 3)
+        p.cmd.set("cartoon_color", "grey")
+        p.cmd.set("transparency", 0.3)
+        p.cmd.hide(
+            "surface", "ligand"
+        )  # for some reason sometimes a ligand surface is applied - hide this.
+
+        # select the ligand and subpocket residues, show them as sticks w/o nonpolar Hs
+        p.cmd.select("resn UNK or resn LIG")
+        p.cmd.show("sticks", "sele")
+        p.cmd.show("sticks", "subP*")
+        p.cmd.hide("sticks", "(elem C extend 1) and (elem H)")
+        p.cmd.color("pink", "elem C and sele")
+
+        for subpocket_name, color in color_dict.items():
+            # set non-polar sticks for this subpocket, color the backbone by subpocket color.
+            p.cmd.select(subpocket_name)
             p.cmd.show("sticks", "sele")
-            p.cmd.show("sticks", "subP*")
+            p.cmd.set("stick_color", color, f"({subpocket_name})")
             p.cmd.hide("sticks", "(elem C extend 1) and (elem H)")
-            p.cmd.color("pink", "elem C and sele")
 
-            for subpocket_name, color in color_dict.items():
-                # set non-polar sticks for this subpocket, color the backbone by subpocket color.
-                p.cmd.select(subpocket_name)
-                p.cmd.show("sticks", "sele")
-                p.cmd.set("stick_color", color, f"({subpocket_name})")
-                p.cmd.hide("sticks", "(elem C extend 1) and (elem H)")
+        if pse:
+            p.cmd.save(str(out_dir / "session_3_set_ligand_view.pse"))
 
-            if self.pse:
-                p.cmd.save(str(out_dir / "session_3_set_ligand_view.pse"))
-
-            if not self.static_view_only:
-                # load trajectory; center the system in the simulation and smoothen between frames.
-                p.cmd.load_traj(
-                    str(traj),
-                    object=complex_name,
-                    start=self.start,
-                    stop=self.stop,
-                    interval=self.interval,
-                )
-                if self.pse:
-                    p.cmd.save(str(out_dir / "session_4_loaded_trajectory.pse"))
-
-                # center the system to the minimized structure
-                # reload
-                complex_name_min = "complex_min"
-                p.cmd.load(str(system), object=complex_name_min)
-                p.cmd.align(complex_name, complex_name_min)
-                p.cmd.delete(complex_name_min)
-
-                if self.smooth:
-                    p.cmd.smooth(
-                        "all", window=int(self.smooth)
-                    )  # perform some smoothing of frames
-
-            if self.contacts:
-                p.cmd.extract("ligand_obj", "ligand")
-
-                show_contacts(p, "ligand_obj", "receptor")
-
-            p.cmd.set_view(view_coords)  # sets general orientation
-            if self.static_view_only:
-                p.cmd.save(str(path))
-                # remove tmpdir
-                shutil.rmtree(tmpdir)
-                return path  # for static view we can end the function here.
-
-            # turn on depth cueing
-            p.cmd.set("depth_cue", 1)
-
-            # now, select stuff to hide; we select everything that is
-            # farther than 15 Ang from our ligand.
-            p.cmd.select("th", "(all) and not ( (all) within 15 of ligand_obj)")
-            # hide it to save rendering time.
-            p.cmd.hide("everything", "th")
-
-            # capsid needs clipping planes as the ligand is encapsulated
-            if TargetProteinMap[self.target] == "Capsid":
-                p.cmd.clip("near", -25)
-
-            if self.pse or self.pse_share:
-                p.cmd.save(str(out_dir / "session_5_selections.pse"))
-
-            # Process the trajectory in a temporary directory
-            from pygifsicle import gifsicle
-
-            # now make the movie.
-            p.cmd.set(
-                "ray_trace_frames", 0
-            )  # ray tracing with surface representation is too expensive.
-            p.cmd.set(
-                "defer_builds_mode", 1
-            )  # this saves memory for large trajectories
-            p.cmd.set("cache_frames", 0)
-            p.cmd.set(
-                "max_threads", 1
-            )  # limit to 1 threads to prevent PyMOL from oversubscribing
-            p.cmd.mclear()  # clears cache
-            prefix = str(tmpdir / "frame")
-            p.cmd.mpng(
-                prefix
-            )  # saves png of each frame as "frame001.png, frame002.png, .."
-
-            # stop pymol instance
-            if self.pse or self.pse_share:
-                p.cmd.save(str(out_dir / "session_6_final.pse"))
-            p.stop()
-
-            # TODO: higher resolution on the pngs.
-            # TODO: Find way to improve writing speed by e.g. removing atoms not in view. Currently takes ~80sec per .png
-            # use imagio to create a gif from the .png files generated by pymol
-
-            from glob import glob
-
-            import imageio.v2 as iio
-
-            png_files = glob(f"{prefix}*.png")
-            if len(png_files) == 0:
-                raise OSError(
-                    f"No {prefix}*.png files found - did PyMol not generate any?"
-                )
-            png_files.sort()  # for some reason *sometimes* this list is scrambled messing up the GIF. Sorting fixes the issue.
-
-            # add progress bar to each frame
-
-            add_gif_progress_bar(
-                png_files, frames_per_ns=self.frames_per_ns, start_frame=self.start
+        if not static_view_only:
+            # load trajectory; center the system in the simulation and smoothen between frames.
+            p.cmd.load_traj(
+                str(traj),
+                object=complex_name,
+                start=start,
+                stop=stop,
+                interval=interval,
             )
+            if pse:
+                p.cmd.save(str(out_dir / "session_4_loaded_trajectory.pse"))
 
-            with iio.get_writer(str(path), mode="I") as writer:
-                for filename in png_files:
-                    image = iio.imread(filename)
-                    writer.append_data(image)
+            # center the system to the minimized structure
+            # reload
+            complex_name_min = "complex_min"
+            p.cmd.load(str(system), object=complex_name_min)
+            p.cmd.align(complex_name, complex_name_min)
+            p.cmd.delete(complex_name_min)
 
-            # now compress the GIF with the method that imagio recommends (https://imageio.readthedocs.io/en/stable/examples.html).
-            gifsicle(
-                sources=str(path),  # happens in-place
-                optimize=True,
-                colors=256,
-                options=["--loopcount"],  # this makes sure the GIF loops
-            )
+            if smooth:
+                p.cmd.smooth(
+                    "all", window=int(smooth)
+                )  # perform some smoothing of frames
 
+        if contacts:
+            p.cmd.extract("ligand_obj", "ligand")
+
+            show_contacts(p, "ligand_obj", "receptor")
+
+        p.cmd.set_view(view_coords)  # sets general orientation
+        if static_view_only:
+            p.cmd.save(str(path))
             # remove tmpdir
             shutil.rmtree(tmpdir)
+            return path  # for static view we can end the function here.
 
+        # turn on depth cueing
+        p.cmd.set("depth_cue", 1)
+
+        # now, select stuff to hide; we select everything that is
+        # farther than 15 Ang from our ligand.
+        p.cmd.select("th", "(all) and not ( (all) within 15 of ligand_obj)")
+        # hide it to save rendering time.
+        p.cmd.hide("everything", "th")
+
+        # capsid needs clipping planes as the ligand is encapsulated
+        if TargetProteinMap[target] == "Capsid":
+            p.cmd.clip("near", -25)
+
+        if pse or pse_share:
+            p.cmd.save(str(out_dir / "session_5_selections.pse"))
+
+        # Process the trajectory in a temporary directory
+        from pygifsicle import gifsicle
+
+        # now make the movie.
+        p.cmd.set(
+            "ray_trace_frames", 0
+        )  # ray tracing with surface representation is too expensive.
+        p.cmd.set("defer_builds_mode", 1)  # this saves memory for large trajectories
+        p.cmd.set("cache_frames", 0)
+        p.cmd.set(
+            "max_threads", 1
+        )  # limit to 1 threads to prevent PyMOL from oversubscribing
+        p.cmd.mclear()  # clears cache
+        prefix = str(tmpdir / "frame")
+        p.cmd.mpng(
+            prefix
+        )  # saves png of each frame as "frame001.png, frame002.png, .."
+
+        # stop pymol instance
+        if pse or pse_share:
+            p.cmd.save(str(out_dir / "session_6_final.pse"))
+        p.stop()
+
+        from glob import glob
+
+        import imageio.v2 as iio
+
+        png_files = glob(f"{prefix}*.png")
+        if len(png_files) == 0:
+            raise OSError(f"No {prefix}*.png files found - did PyMol not generate any?")
+        png_files.sort()  # for some reason *sometimes* this list is scrambled messing up the GIF. Sorting fixes the issue.
+
+        # add progress bar to each frame
+
+        add_gif_progress_bar(png_files, frames_per_ns=frames_per_ns, start_frame=start)
+
+        with iio.get_writer(str(path), mode="I") as writer:
+            for filename in png_files:
+                image = iio.imread(filename)
+                writer.append_data(image)
+
+        # now compress the GIF with the method that imagio recommends (https://imageio.readthedocs.io/en/stable/examples.html).
+        gifsicle(
+            sources=str(path),  # happens in-place
+            optimize=True,
+            colors=256,
+            options=["--loopcount"],  # this makes sure the GIF loops
+        )
+
+        # remove tmpdir
+        shutil.rmtree(tmpdir)
+
+        return path
+
+    @multimethod
+    def _dispatch(self, inputs: list[SimulationResult], **kwargs):
+        data = []
+        for res in inputs:
+            path = self.pymol_traj_viz(
+                target=self.target,
+                traj=res.traj_path,
+                system=res.minimized_pdb_path,
+                out_dir=self.output_dir
+                / res.input_docking_result.posed_ligand.compound_name,
+                static_view_only=self.static_view_only,
+                pse=self.pse,
+                pse_share=self.pse_share,
+                start=self.start,
+                stop=self.stop,
+                interval=self.interval,
+                smooth=self.smooth,
+                contacts=self.contacts,
+                frames_per_ns=self.frames_per_ns,
+            )
             row = {}
             row[DockingResultCols.LIGAND_ID.value] = (
                 res.input_docking_result.posed_ligand.compound_name
@@ -276,5 +301,38 @@ class GIFVisualizerV2(VisualizerBase):
             data.append(row)
         return data
 
-    def provenance(self):
-        return {}
+    @_dispatch.register
+    def _dispatch(self, inputs: list[Tuple[Path, Path]], **kwargs):
+        data = []
+        for i, tup in enumerate(inputs):
+            # unpack the tuple
+            traj, top = tup
+            # find with the unique identifier for the ligand would be
+            complex = Complex.from_pdb(
+                top,
+                target_kwargs={"target_name": f"unknown_target_{i}"},
+                ligand_kwargs={"compound_name": f"unknown_ligand_{i}"},
+            )
+            csp = CompoundStructurePair(complex=complex, ligand=complex.ligand)
+            path = self.pymol_traj_viz(
+                target=self.target,
+                traj=traj,
+                system=top,
+                out_dir=self.output_dir / csp.unique_name,
+                static_view_only=self.static_view_only,
+                pse=self.pse,
+                pse_share=self.pse_share,
+                start=self.start,
+                stop=self.stop,
+                interval=self.interval,
+                smooth=self.smooth,
+                contacts=self.contacts,
+                frames_per_ns=self.frames_per_ns,
+            )
+            row = {}
+            row[DockingResultCols.LIGAND_ID.value] = complex.ligand.compound_name
+            row[DockingResultCols.TARGET_ID.value] = complex.target.target_name
+            row[DockingResultCols.SMILES.value] = complex.ligand.smiles
+            row[DockingResultCols.GIF_PATH.value] = path
+            data.append(row)
+        return data
