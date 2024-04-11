aa4806988ea8073647a68c096f21a8736c23a4ed
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-04-11 07:23:01 -0400

    add a bunch of __init__.py files

---
+++ asapdiscovery-modeling/asapdiscovery/__init__.py
@@ -0,0 +1,6 @@
+"""Computational chemistry packages developed by the ASAP Discovery Consortium."""
+
+from importlib.metadata import version
+import data
+
+__version__ = version("asapdiscovery-data")
---
+++ asapdiscovery-simulation/asapdiscovery/__init__.py
@@ -0,0 +1,6 @@
+"""Computational chemistry packages developed by the ASAP Discovery Consortium."""
+
+from importlib.metadata import version
+
+
+__version__ = version("asapdiscovery-data")
