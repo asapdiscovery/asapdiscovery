6a61de23787be6f1b1c167c944c696bbbddcb2fc
Alex <alex.payne@choderalab.org>
2024-04-18 17:07:25 -0400

    change openeye set_SD_data backend to allow None

--- asapdiscovery-data/asapdiscovery/data/backend/openeye.py
+++ asapdiscovery-data/asapdiscovery/data/backend/openeye.py
@@ -829,6 +829,11 @@ def set_SD_data(mol: oechem.OEMol, data: dict[str, str | list]) -> oechem.OEMol:
     oechem.OEMol
         OpenEye OEMol with SD data set
     """
+
+    # Don't both if the ligand doesn't actually have any data
+    if not mol:
+        return
+
     from asapdiscovery.data.util.data_conversion import get_first_value_of_dict_of_lists
 
     # convert to dict of lists first
