284d1f31756c4e60ae8287f15b55ea413e53698e
jthorton <Josh.Horton@newcastle.ac.uk>
2024-04-18 16:57:44 +0100

    add extra possible covalent patterns

--- asapdiscovery-alchemy/asapdiscovery/alchemy/cli/utils.py
+++ asapdiscovery-alchemy/asapdiscovery/alchemy/cli/utils.py
@@ -185,6 +185,8 @@ def has_warhead(ligand: "Ligand") -> bool:
         "vinylsulfonamide_adduct": "NS(=O)(C[C:1]S)=O",
         "nitrile": "N#[C:1]-[*]",
         "nitrile_adduct": "C-S-[C:1](=N)",
+        "propiolamide": "NC(=O)C#C",
+        "sulfamate": "NS(=O)(=O)O"
     }
     rdkit_mol = ligand.to_rdkit()
     for smarts in covalent_warhead_smarts.values():
