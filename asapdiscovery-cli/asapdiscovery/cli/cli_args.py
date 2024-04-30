47558dc3e39e6588c0492e2611b7ddf5e04274d6
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2024-04-30 15:46:24 +1000

    Add resilience to non-dask computation (#1018)
    * add resilience to non-dask computation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix cruft
    
    * working
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix undef
    
    * get the try-except right
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- asapdiscovery-cli/asapdiscovery/cli/cli_args.py
+++ asapdiscovery-cli/asapdiscovery/cli/cli_args.py
@@ -1,6 +1,6 @@
 import click
 from asapdiscovery.data.services.postera.manifold_data_validation import TargetTags
-from asapdiscovery.data.util.dask_utils import DaskFailureMode, DaskType
+from asapdiscovery.data.util.dask_utils import DaskType, FailureMode
 from asapdiscovery.ml.models import ASAPMLModelRegistry
 from asapdiscovery.simulation.simulate import OpenMMPlatform
 
@@ -54,18 +54,18 @@ def dask_type(func):
     )(func)
 
 
-def dask_failure_mode(func):
+def failure_mode(func):
     return click.option(
-        "--dask-failure-mode",
-        type=click.Choice(DaskFailureMode.get_values(), case_sensitive=False),
-        default=DaskFailureMode.SKIP,
+        "--failure-mode",
+        type=click.Choice(FailureMode.get_values(), case_sensitive=False),
+        default=FailureMode.SKIP,
         help="The failure mode for dask. Can be 'raise' or 'skip'.",
         show_default=True,
     )(func)
 
 
 def dask_args(func):
-    return use_dask(dask_type(dask_failure_mode(func)))
+    return use_dask(dask_type(failure_mode(func)))
 
 
 def target(func):
