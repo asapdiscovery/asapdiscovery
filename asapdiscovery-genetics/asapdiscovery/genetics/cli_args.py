fcc6e10fe078530cfd8d678506e089452e123406
Maria Castellanos <castelm3@mskcc.org>
2024-05-03 18:02:52 -0400

    add genetics to cli

---
+++ asapdiscovery-genetics/asapdiscovery/genetics/cli_args.py
@@ -0,0 +1,78 @@
+import click
+
+def seq_file(func):
+    return click.option(
+        "-f",
+        "--seq-file",
+        type=click.Path(resolve_path=True, exists=True, file_okay=True, dir_okay=False),
+        help="File containing reference sequences",
+    )(func)
+
+def output_dir(func):
+    return click.option(
+        "--output-dir",
+        type=click.Path(
+            resolve_path=True, exists=False, file_okay=False, dir_okay=True
+        ),
+        help="The directory to output results to.",
+        default="output",
+    )(func)
+
+def seq_type(func):
+    return click.option(
+        "-t",
+        "--seq_type",
+        type=click.Choice(["fasta", "pdb", "pre-calc"]),
+        help="Type of input from which the sequence will be read.",
+        default="fasta",
+        show_default=True,
+    )(func)
+
+def blast_json(func):
+    return click.option(
+        "--blast-json",
+        type=click.Path(resolve_path=True, exists=True, file_okay=True, dir_okay=False),
+        help="Path to a json file containing parameters for the blast search.",
+    )(func)
+
+def email(func):
+    return click.option(
+        "--email",
+        type=str,
+        default="",
+        help="Email for Entrez search.",
+    )(func)
+
+
+def gen_ref_pdb(func):
+    return click.option(
+        "--gen-ref-pdb",
+        is_flag=True,
+        default=False,
+        help="Whether to retrieve a pdb file for the query structure.",
+    )(func)
+
+def multimer(func):
+    return click.option(
+        "--multimer",
+        is_flag=True,
+        default=False,
+        help="Store the output sequences for a multimer ColabFold run (from identical chains)."
+        'If not set, "--n-chains" will not be used. ',
+    )(func)
+
+def n_chains(func):
+    return click.option(
+        "--n-chains",
+        type=int,
+        default=None,
+        help="Number of repeated chains that will be saved in csv file."
+        'Requires calling the "--multimer" option first.',
+    )(func)
+
+def pdb_file(func):
+    return click.option(
+        "--pdb-file",
+        type=click.Path(resolve_path=True, exists=True, file_okay=True, dir_okay=False),
+        help="Path to a pdb file containing a structure",
+    )(func)
