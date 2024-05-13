141647a0db453035e987581041db82da6e8049fc
kaminow <benjamin.kaminow@choderalab.org>
2024-05-13 17:45:28 -0400

    Add class to organize TrainingPrediction values.

--- asapdiscovery-ml/asapdiscovery/ml/schema.py
+++ asapdiscovery-ml/asapdiscovery/ml/schema.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 from asapdiscovery.ml.config import LossFunctionConfig
 
 
 class TrainingPrediction(BaseModel):
     """
-    Class to organize predictions made during training.
+    Class to store predictions made during training.
     """
 
     # Identifiers
@@ -38,3 +38,45 @@ class TrainingPrediction(BaseModel):
         ..., description="Config describing loss function."
     )
     loss_vals: list[float] = Field(..., description="Loss value of model prediction.")
+
+
+class TrainingPredictionTracker(BaseModel):
+    """
+    Class to organize predictions.
+    """
+
+    split_dict: dict[str, list[TrainingPrediction]] = Field(
+        None, description="Internal dict storing all TrainingPredictions."
+    )
+
+    @validator("split_dict")
+    def init_split_dict(cls, split_dict):
+        # If nothing was passed, just init an empty dict
+        if not split_dict:
+            return {"train": [], "val": [], "test": []}
+
+        # Make sure that the format is correct
+        if split_dict.keys() != {"train", "val", "test"}:
+            raise ValueError(f"Received unexpected dict keys: {split_dict.keys()}")
+
+        # Make sure that each split has a list
+        if not all([isinstance(sp_list, list) for sp_list in split_dict.values()]):
+            raise ValueError(
+                "All dict values must be lists, got "
+                f"{[type(v) for v in split_dict.values()]}"
+            )
+
+        # Make sure all the lists have the right types in them
+        if not all(
+            [
+                (len(sp_list) == 0)
+                or ({type(v) for v in sp_list} == {TrainingPrediction})
+                for sp_list in split_dict.values()
+            ]
+        ):
+            raise ValueError(
+                "All dict values must only contain TrainingPredictions, got "
+                f"{[{type(v) for v in sp_list} for sp_list in split_dict.values()]}"
+            )
+
+        return split_dict
