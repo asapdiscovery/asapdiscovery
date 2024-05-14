030d9dd4fbfd814c13366c373d589c49e16521fe
kaminow <benjamin.kaminow@choderalab.org>
2024-05-14 11:14:45 -0400

    Add tests for get_values.

--- asapdiscovery-ml/asapdiscovery/ml/tests/test_training_prediction_schema.py
+++ asapdiscovery-ml/asapdiscovery/ml/tests/test_training_prediction_schema.py
@@ -85,13 +85,48 @@ def test_find_value_idxs(identifiers, loss_configs):
     idxs = tp_tracker._find_value_idxs()
     assert idxs == {"train": [0], "val": [0], "test": []}
 
+    idxs = tp_tracker._find_value_idxs(compound_id="bad_id")
+    assert idxs == {"train": [], "val": [], "test": []}
 
-def test_get_values_split():
-    pass
+
+def test_get_values_split(identifiers, loss_configs):
+    tp1 = TrainingPrediction(**identifiers, loss_config=loss_configs[0])
+    tp2 = TrainingPrediction(**identifiers, loss_config=loss_configs[1])
+
+    tp_tracker = TrainingPredictionTracker(
+        split_dict={"train": [tp1], "val": [tp2], "test": []}
+    )
+
+    vals = tp_tracker.get_values(split="train")
+    assert vals == [tp1]
+
+    vals = tp_tracker.get_values(split="train", compound_id="bad_id")
+    assert vals == []
 
 
-def test_get_values_no_split():
-    pass
+def test_get_values_no_split(identifiers, loss_configs):
+    tp1 = TrainingPrediction(**identifiers, loss_config=loss_configs[0])
+    tp2 = TrainingPrediction(**identifiers, loss_config=loss_configs[1])
+
+    tp_tracker = TrainingPredictionTracker(
+        split_dict={"train": [tp1], "val": [tp2], "test": []}
+    )
+
+    vals = tp_tracker.get_values()
+    assert vals == {"train": [tp1], "val": [tp2], "test": []}
+
+    vals = tp_tracker.get_values(loss_config=loss_configs[0])
+    assert vals == {"train": [tp1], "val": [], "test": []}
+
+    vals = tp_tracker.get_values(
+        compound_id=identifiers["compound_id"],
+        xtal_id=identifiers["xtal_id"],
+        target_prop=identifiers["target_prop"],
+    )
+    assert vals == {"train": [tp1], "val": [tp2], "test": []}
+
+    vals = tp_tracker.get_values(compound_id="bad_id")
+    assert vals == {"train": [], "val": [], "test": []}
 
 
 def test_update_values_existing():
