5eb5ecbd1d0a12913209ad573c80301701ec6b11
kaminow <benjamin.kaminow@choderalab.org>
2024-05-23 16:05:55 -0400

    Switch from argparse to click.

--- asapdiscovery-data/asapdiscovery/data/scripts/cdd_to_schema.py
+++ asapdiscovery-data/asapdiscovery/data/scripts/cdd_to_schema.py
@@ -12,38 +12,62 @@ python cdd_to_schema.py \
 -json cdd_downloaded_filtered.json
 """
 
-import argparse
+import click
+from pathlib import Path
 
-from asapdiscovery.data.util.utils import cdd_to_schema  # noqa: E402
-from asapdiscovery.data.util.utils import cdd_to_schema_pair  # noqa: E402
-
-
-def get_args():
-    parser = argparse.ArgumentParser(description="")
-
-    parser.add_argument("-i", required=True, help="CSV file input from CDD.")
-    parser.add_argument("-json", required=True, help="Output JSON file.")
-    parser.add_argument("-csv", help="Output CSV file.")
-    parser.add_argument(
-        "-type",
-        default="std",
-        help=(
-            "What type of data is " "being loaded (std: standard, ep: enantiomer pairs)"
-        ),
-    )
-
-    return parser.parse_args()
+from asapdiscovery.data.util.utils import cdd_to_schema, cdd_to_schema_pair
 
 
-def main():
-    args = get_args()
-
-    if args.type.lower() == "std":
-        _ = cdd_to_schema(args.i, args.json, args.csv)
-    elif args.type.lower() == "ep":
-        _ = cdd_to_schema_pair(args.i, args.json, args.csv)
+@click.command()
+@click.option(
+    "-i",
+    "--in-file",
+    required=True,
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, path_type=Path),
+    help="CSV file input from CDD.",
+)
+@click.option(
+    "-json",
+    "--out-json",
+    required=True,
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    help="Output JSON file.",
+)
+@click.option(
+    "-csv",
+    "--out-csv",
+    required=True,
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    help="Output CSV file.",
+)
+@click.option(
+    "-type",
+    "--data-type",
+    default="std",
+    type=click.Choice(["std", "ep"], case_sensitive=False),
+    help="What type of data is being loaded (std: standard, ep: enantiomer pairs).",
+)
+@click.option(
+    "--frag-dir",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path),
+    help=(
+        "Fragalysis directory. If passed, any compounds that are found to have a "
+        "crystal structure will have a Ligand object attached to their exp_data."
+    ),
+)
+def main(
+    in_file: Path,
+    out_json: Path,
+    out_csv: Path | None = None,
+    data_type: str = "std",
+    frag_dir: Path | None = None,
+):
+    if data_type.lower() == "std":
+        _ = cdd_to_schema(in_file, out_json, out_csv)
+    elif data_type.lower() == "ep":
+        _ = cdd_to_schema_pair(in_file, out_json, out_csv)
     else:
-        raise ValueError(f"Unknown value for -type: {args.type}.")
+        raise ValueError(f"Unknown value for --data-type: {data_type}.")
 
 
 if __name__ == "__main__":
