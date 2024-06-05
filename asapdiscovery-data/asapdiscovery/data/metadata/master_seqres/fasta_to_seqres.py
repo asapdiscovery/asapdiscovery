1d32c93de4b103b541cb510c0839e6afe3fcf545
hmacdope <hugomacdermott@gmail.com>
2024-06-05 11:24:56 +1000

    add fasta seqres

--- asapdiscovery-data/asapdiscovery/data/metadata/master_seqres/fasta_to_seqres.py
+++ asapdiscovery-data/asapdiscovery/data/metadata/master_seqres/fasta_to_seqres.py
@@ -24,7 +24,6 @@ def main():
 
     seqres_str = "SEQRES: |\n"
     for r in SeqIO.parse(args.in_file.open('r'), "fasta"):
-        print(r)
         # Based on the format Andre used in Slack
         rec_chains = r.description.split("|")[1]
         rec_chains = [c.strip(",") for c in rec_chains.split()[1:]]
@@ -48,7 +47,6 @@ def main():
             rec_str += "\n"
 
         rec_str = "".join([rec_str.format(*([c] * len(line_res))) for c in rec_chains])
-        print(rec_str)
         seqres_str += rec_str
 
     with args.out_file.open("w") as fp:
