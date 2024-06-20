e7c089b402db870f3a81a2412acad9f4734beff0
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2024-06-20 11:01:15 +1000

    framework

---
+++ asapdiscovery-data/asapdiscovery/data/operators/symmetry_expander.py
@@ -0,0 +1,130 @@
+from asapdiscovery.data.schema.complex import Complex
+from asapdiscovery.data.backend.openeye import save_openeye_pdb, oechem
+from asapdiscovery.data.schema.target import Target
+from asapdiscovery.data.util.dask_utils import FailureMode, dask_vmap
+from pydantic import BaseModel
+import MDAnalysis as mda
+from MDAnalysis.lib.util import NamedStream
+from io import StringIO
+import warnings
+import pymol2
+import tempfile
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class SymmetryExpander(BaseModel):
+    """
+    Expand symmetry of a unit cell to include multiple copies.
+    """
+
+    expand_ligand: bool = False
+    n_repeats: int = 8
+
+    def expand(
+        self,
+        complexes: list[Complex],
+        use_dask: bool = False,
+        dask_client=None,
+        failure_mode=FailureMode.SKIP,
+        **kwargs,
+    ):
+
+        return self._expand(
+            complexes=complexes,
+            use_dask=use_dask,
+            dask_client=dask_client,
+            failure_mode=failure_mode,
+            **kwargs,
+        )
+
+    @dask_vmap(["complexes"], has_failure_mode=True)
+    def _expand(
+        self,
+        complexes: list[Complex],
+        failure_mode: str = "skip",
+    ) -> list[Complex]:
+        new_complexs = []
+        for complex in complexes:
+            try:
+                target_oemol = complex.target.to_oemol()
+                if oechem.OEGetCrystalSymmetry(target_oemol) is None:
+                    raise ValueError("No crystal symmetry found in target")
+                new = oechem.OEMol()
+                oechem.OEExpandCrystalSymmetry(new, target_oemol)
+                # combine
+                combined = oechem.OEMol()
+                for mol in new.GetConfs():
+                    oechem.OEAddMols(combined, mol)
+
+                t = Target.from_oemol(combined, target_name=complex.target.target_name, ids=complex.target.ids)
+                c = Complex(target=t, ligand=complex.ligand, ligand_chain=complex.ligand_chain)
+                new_complexs.append(c)
+      
+            # try:
+            #     p = pymol2.PyMOL()
+            #     p.start()
+            #     # check if PDB has a box
+            #     with warnings.catch_warnings():
+            #         warnings.simplefilter(
+            #             "ignore"
+            #         )  # hides MDA RunTimeWarning that complains about string IO
+            #         u = mda.Universe(
+            #             NamedStream(StringIO(complex.target.data), "complex.pdb")
+            #         )
+
+            #         # check for a box
+            #         logger.info(f"symmetry expansion with box:  {u.trajectory.ts.dimensions}")
+            #         if u.trajectory.ts.dimensions is None:
+            #             raise ValueError("No box found in PDB")
+            #         elif not all(u.trajectory.ts.dimensions[:3]):
+            #             raise ValueError("Box has zero volume")
+
+            #     # load each component into PyMOL
+            #     p.cmd.read_pdbstr(complex.target.data, "protein_obj")
+            #     p.cmd.read_pdbstr(
+            #         oemol_to_pdb_string(complex.ligand.to_oemol()), "lig_obj"
+            #     )
+
+            #     # remove some solvent stuff to prevent occasional clashes with neighbors
+            #     p.cmd.remove("solvent")
+            #     p.cmd.remove("inorganic")
+
+            #     # reconstruct neighboring asymmetric units from the crystallographic experiment
+            #     p.cmd.symexp(
+            #         "sym", "protein_obj", "(protein_obj)", self.n_repeats
+            #     )  # do a big expansion just to be sure
+
+            #     if self.expand_ligand:
+            #         p.cmd.symexp(
+            #             "sym", "lig_obj", "(lig_obj)", self.n_repeats
+            #         )  # do a big expansion just to be sure
+
+            #     string = p.cmd.get_pdbstr(
+            #         "all", 0
+            #     )  # writes all states, so should be able to handle multi-ligand
+            #     p.stop()
+
+            #     tmp = tempfile.NamedTemporaryFile()
+            #     with open(tmp.name, 'w') as f:
+            #         f.write(string)
+            #         cnew = Complex.from_pdb(
+            #             "tst.pdb",
+            #             target_kwargs={"target_name": "test"},
+            #             ligand_kwargs={"compound_name": "test"},
+            #         )
+
+            #     new_complexs.append(cnew)
+
+            except Exception as e:
+                if failure_mode == "skip":
+                    logger.error(f"Error processing {complex.unique_name}: {e}")
+                elif failure_mode == "raise":
+                    raise e
+                else:
+                    raise ValueError(
+                        f"Unknown error mode: {failure_mode}, must be 'skip' or 'raise'"
+                    )
+        return new_complexs
