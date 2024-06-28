0340b217fa7f024dd4054370032237031300e131
hmacdope <hugomacdermott@gmail.com>
2024-06-28 15:45:04 +1000

    add to meta cli

--- asapdiscovery-cli/asapdiscovery/cli/cli.py
+++ asapdiscovery-cli/asapdiscovery/cli/cli.py
@@ -29,3 +29,7 @@ cli.add_command(ml)
 from asapdiscovery.genetics.cli import genetics  # noqa: F401, E402, F811
 
 cli.add_command(genetics)
+
+from asapdiscovery.simulation.cli import simulation  # noqa: F401, E402, F811
+
+cli.add_command(simulation)
--- asapdiscovery-simulation/asapdiscovery/simulation/cli.py
+++ asapdiscovery-simulation/asapdiscovery/simulation/cli.py
@@ -1,13 +1,21 @@
 import click
 import itertools
+import logging 
+from typing import Optional, Union
+
 from asapdiscovery.cli.cli_args import (
-    dask_args,
+    use_dask,
     ligands,
-    md_args,
+    md_steps,
+    md_openmm_platform,
     output_dir,
     pdb_file,
+    loglevel,
 )
-from asapdiscovery.simulation.simulate import VanillaMDSimulator
+from asapdiscovery.simulation.simulate import OpenMMPlatform, VanillaMDSimulator
+from asapdiscovery.data.util.dask_utils import DaskType, make_dask_client_meta, BackendType
+from asapdiscovery.data.util.logging import FileLogger
+from asapdiscovery.data.readers.molfile import MolFileFactory
 
 @click.group()
 def simulation():
@@ -18,24 +26,63 @@ def simulation():
 @simulation.command()
 @ligands
 @pdb_file
-@md_args
+@md_steps
+@md_openmm_platform
 @output_dir
+@loglevel   
+@use_dask
 def vanilla_md(
+    ligands: Optional[str] = None,
+    pdb_file: Optional[str] = None,
+    md_steps: int = 2500000,  # 10 ns @ 4.0 fs timestep
+    md_openmm_platform: OpenMMPlatform = OpenMMPlatform.Fastest,
+    output_dir: str = "vanilla_md_output",
+    loglevel: Union[int, str] = logging.INFO,
+    use_dask: bool = False
     
 ):
     print("Running vanilla MD simulation")
-    simulator = VanillaMDSimulator()
-    ligs = MolfileFactory.from_file(ligands).read()
-    combo = list(itertools.product([pdb_file], ligs))
-    print("running simulations ..., please wait")
-    simulator.simulate(combo)
-    print("done")
+    
+    logger = FileLogger(
+        "",  # default root logger so that dask logging is forwarded
+        path=output_dir,
+        logfile="vanilla_md.log",
+        stdout=True,
+        level=loglevel,
+    ).getLogger()
+
+    if use_dask:
+        logger.info("Using dask")
+        dask_client = make_dask_client_meta(
+                DaskType.LOCAL_GPU, loglevel=loglevel
+            )
+    else:
+        dask_client = None
+
+    # in case its a multisdf
+    ligs = MolFileFactory.from_file(ligands).read()
+    logger.info(f"Read {len(ligs)} ligands")
+
+    # save each ligand to a file
+    lig_paths = []
+    lig_dir = output_dir / "ligands"
+    for lig in ligs:
+        path = lig_dir / f"{lig.compound_name}.sdf"
+        lig.to_sdf(path)
+        lig_paths.append(path)
+    
+    combo = list(itertools.product([pdb_file], lig_paths))
+
+    simulator = VanillaMDSimulator(output_dir=output_dir, openmm_platform=md_openmm_platform, num_steps=md_steps)
+
+    logger.info("running simulations ..., please wait")
+    simulator.simulate(combo, use_dask=use_dask, dask_client=dask_client)
+    logger.info("done")
 
 
 
 @simulation.command()
 @ligands
 @pdb_file
-@md_args
 def szybki():
-    print("Running szybki simulation")
+    raise NotImplementedError("Szybki simulation not yet implemented")
