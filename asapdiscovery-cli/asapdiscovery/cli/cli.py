8f2127771184a2c06f144fa3d6b90999e6062e3e
hmacdope <hugomacdermott@gmail.com>
2024-07-05 09:55:58 +1000

    start

--- asapdiscovery-cli/asapdiscovery/cli/cli.py
+++ asapdiscovery-cli/asapdiscovery/cli/cli.py
@@ -29,3 +29,8 @@ cli.add_command(ml)
 from asapdiscovery.genetics.cli import genetics  # noqa: F401, E402, F811
 
 cli.add_command(genetics)
+
+
+from asapdiscovery.dataviz.cli import visualization  # noqa: F401, E402, F811
+
+cli.add_command(visualization)
