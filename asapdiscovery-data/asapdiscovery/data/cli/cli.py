152126c736af883fb91b6a72ddcd9c64a9e12e44
Hugo MacDermott-Opeskin <hugomacdermott@gmail.com>
2024-07-26 13:08:44 +1000

    Fix fragalysis download and add `data` cli point. (#1146)
    * fix fragalysis dl
    
    * remove old script
    
    * remove from pyproject
    
    * working
    
    * fix D68
    
    * fix testing
    
    * almost
    
    * fix test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove unused imports
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ asapdiscovery-data/asapdiscovery/data/cli/cli.py
@@ -0,0 +1,48 @@
+import copy
+from typing import Optional
+
+import click
+from asapdiscovery.data.services.fragalysis.fragalysis_download import (  # noqa: E402
+    API_CALL_BASE_LEGACY,
+    BASE_URL_LEGACY,
+    FragalysisTargets,
+    download,
+)
+
+
+def target(func):
+    return click.option(
+        "-t",
+        "--fragalysis-target",
+        type=click.Choice(FragalysisTargets.get_values(), case_sensitive=True),
+        help="The target for the workflow",
+        required=True,
+    )(func)
+
+
+@click.group()
+def data():
+    """Do data processing tasks"""
+    pass
+
+
+@data.command()
+@target
+@click.option("-o", "--output", required=True, help="Output file name.")
+@click.option(
+    "-x", "--extract", is_flag=True, help="Extract file after downloading it."
+)
+def download_fragalysis(
+    fragalysis_target: Optional[str] = "Mpro",
+    output: Optional[str] = "output.zip",
+    extract: Optional[bool] = False,
+):
+
+    # NOTE currently most of the targets we care about in fragalysis have been shifted to the "legacy" stack
+    # hence the use of the legacy base url and api call, this may change in the future
+
+    # Copy the base call and update the base target with the cli-specified target
+    api_call = copy.deepcopy(API_CALL_BASE_LEGACY)
+    api_call["target_name"] = fragalysis_target
+
+    download(output, api_call, extract=extract, base_url=BASE_URL_LEGACY)
