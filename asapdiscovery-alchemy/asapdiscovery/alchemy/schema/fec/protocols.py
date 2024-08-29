9627916bb5d38da0f3aa42c33604636fb68cb62b
David Dotson <dotsdl@gmail.com>
2024-08-28 20:44:04 -0700

    Trimming down protocol settings objects; seeking to avoid duplication and drift from original objects

--- asapdiscovery-alchemy/asapdiscovery/alchemy/schema/fec/protocols.py
+++ asapdiscovery-alchemy/asapdiscovery/alchemy/schema/fec/protocols.py
@@ -12,42 +12,16 @@ from openfe.protocols.openmm_rfe.equil_rfe_settings import (
     IntegratorSettings,
     OpenMMEngineSettings,
     SimulationSettings,
-    SolvationSettings,
+    OpenMMSolvationSettings,
     SystemSettings,
 )
 from openff.units import unit as OFFUnit
+from openff.models.types import FloatQuantity
 from pydantic import Field
 
 
-class SolventSettings(_SchemaBase):
-    """
-    A settings class to encode the solvent used in the OpenFE FEC calculations.
-    """
-
-    type: Literal["SolventSettings"] = "SolventSettings"
-
-    smiles: str = Field("O", description="The smiles pattern of the solvent.")
-    positive_ion: str = Field(
-        "Na+",
-        description="The positive monoatomic ion which should be used to neutralize the system and to adjust the ionic concentration.",
-    )
-    negative_ion: str = Field(
-        "Cl-",
-        description="The negative monoatomic ion which should be used to neutralize the system and to adjust the ionic concentration.",
-    )
-    neutralize: bool = Field(
-        True,
-        description="If the net charge of the chemical system should be neutralized by the ions defined by `positive_ion` and `negative_ion`.",
-    )
-    ion_concentration: FloatQuantity["molar"] = Field(  # noqa: F821
-        0.15 * OFFUnit.molar,
-        description="The ionic concentration required in molar units.",
-    )
-
-    def to_solvent_component(self) -> gufe.SolventComponent:
-        return gufe.SolventComponent(**self.dict(exclude={"type"}))
-
-
+# DD: I think we'll want to try and avoid duplicating the attribute structure of Protocol settings here;
+# though we don't expect them to change often, it would be cleaner to avoid duplication
 class _ProtocolBase(_SchemaBase, abc.ABC):
     """
     A base class for openfe protocol settings to work around the serialisation issues with
@@ -56,43 +30,11 @@ class _ProtocolBase(_SchemaBase, abc.ABC):
 
     type: Literal["_ProtocolBase"] = "_ProtocolBase"
 
-    solvent_settings: SolventSettings = Field(
-        SolventSettings(),
-        description="The solvent settings which should be used during the free energy calculations.",
-    )
-    forcefield_settings: settings.OpenMMSystemGeneratorFFSettings = Field(
-        settings.OpenMMSystemGeneratorFFSettings(
-            small_molecule_forcefield="openff-2.1.0"
-        ),
-        description="The force field settings used to parameterize the systems.",
-    )
-    thermo_settings: settings.ThermoSettings = Field(
-        settings.ThermoSettings(
-            temperature=298.15 * OFFUnit.kelvin, pressure=1 * OFFUnit.bar
-        ),
-        description="The settings for thermodynamic parameters.",
-    )
-    system_settings: SystemSettings = Field(
-        SystemSettings(), description="The nonbonded system settings."
-    )
-    solvation_settings: SolvationSettings = Field(
-        SolvationSettings(),
-        description="Settings controlling how the systems should be solvated.",
-    )
-    alchemical_settings: AlchemicalSettings = Field(
-        AlchemicalSettings(), description="The alchemical protocol settings."
-    )
-    engine_settings: OpenMMEngineSettings = Field(
-        OpenMMEngineSettings(), description="Openmm platform settings."
-    )
-    integrator_settings: IntegratorSettings = Field(
-        IntegratorSettings(),
-        description="Settings for the LangevinSplittingDynamicsMove integrator.",
-    )
-
     @abc.abstractmethod
     def to_openfe_protocol(self): ...
 
+
+# DD TODO: extract defaults from below and make sure these make it into our implementations
 
 class RelativeHybridTopologySettings(_ProtocolBase):
     """A settings class for the RelativeHybridTopologyProtocol in OpenFE."""
