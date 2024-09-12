ed3f00af70ba1f6eb6c681bca29eac0bdf800f93
JenkeScheen <43140137+JenkeScheen@users.noreply.github.com>
2024-09-13 01:52:10 +0200

    Add zikv rdrp (#1201)
    * add metadata and vendor it
    
    * more stuff
    
    * add to manifold tags
    
    * can just leave these empty?
    
    * nop
    
    * add chain A to fitness data
    
    * enable in fitness - small refactor on pulling fitness data
    
    * working version of viz
    
    * mvp
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- asapdiscovery-dataviz/asapdiscovery/dataviz/_gif_blocks.py
+++ asapdiscovery-dataviz/asapdiscovery/dataviz/_gif_blocks.py
@@ -71,6 +71,7 @@ class GIFBlockData:
         "EV-D68-Capsid": "A",
         "DENV-NS2B-NS3pro": "B",
         "ZIKV-NS2B-NS3pro": "B",
+        "ZIKV-RdRppro": "A",
     }
 
     # define the residues that span the subpockets
@@ -133,6 +134,7 @@ class GIFBlockData:
         "subP2": "152+83+75+81+82+72",
         "subP3": "153+154+155",
     }
+    pocket_dict_ZIKV_RdRppro = {"undecided": "1"}
 
     pocket_dict_DENV_NS2B_NS3pro = {
         "subP1": "135+151+161+159+129+150+130+132+131",
@@ -188,6 +190,9 @@ class GIFBlockData:
         "subP1_prime": "orange",
         "subP2": "blue",
         "subP3": "cyan",
+    }
+    color_dict_RdRppro = {
+        "undecided": "grey",
     }
     color_dict_Capsid = {
         "hydrophic_trap": "orange",
