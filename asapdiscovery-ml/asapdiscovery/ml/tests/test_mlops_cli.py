1198d88633ccf049db76d19853a6d11523dd5777
hmacdope <hugomacdermott@gmail.com>
2024-09-17 09:32:32 +1000

    mock up envs

--- asapdiscovery-ml/asapdiscovery/ml/tests/test_mlops_cli.py
+++ asapdiscovery-ml/asapdiscovery/ml/tests/test_mlops_cli.py
@@ -2,11 +2,38 @@ import pytest
 from asapdiscovery.data.testing.test_resources import fetch_test_file
 from asapdiscovery.ml.cli_mlops import mlops as cli
 from click.testing import CliRunner
+import os
+import traceback
+
+
+def click_success(result):
+    if result.exit_code != 0:  # -no-cov-  (only occurs on test error)
+        print(result.output)
+        traceback.print_tb(result.exc_info[2])
+        print(result.exc_info[0], result.exc_info[1])
+    return result.exit_code == 0
+
+
+
+
 
 
 def test_mlops_run(tmp_path):
 
     runner = CliRunner()
+    # mock AWS credentials
+    os.environ["AWS_ACCESS_KEY_ID"] = "dummy"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "dummy"
+    os.environ["BUCKET_NAME"] = "dummy"
+    os.environ["BUCKET_PREFIX"] = "dummy"
+    
+    # mock WANDB credentials
+    os.environ["WANDB_PROJECT"] = "dummy"
+
+    # mock CDD credentials
+    os.environ["CDD_API_KEY"] = "dummy"
+    os.environ["CDD_VAULT_NUMBER"] = "1"
+    
     result = runner.invoke(
         cli,
         [
@@ -21,4 +48,5 @@ def test_mlops_run(tmp_path):
             tmp_path,
         ],
     )
-    assert result.exit_code == 0
+    
+    assert click_success(result)
