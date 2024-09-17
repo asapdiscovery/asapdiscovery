3ee29895252ca1c3d11ea099e64ac5a51ddb8f3c
hmacdope <hugomacdermott@gmail.com>
2024-09-17 10:39:16 +1000

    set offline

--- asapdiscovery-ml/asapdiscovery/ml/tests/test_mlops_cli.py
+++ asapdiscovery-ml/asapdiscovery/ml/tests/test_mlops_cli.py
@@ -34,7 +34,7 @@ def test_mlops_run(tmp_path):
 
     # mock WANDB credentials
     os.environ["WANDB_PROJECT"] = "dummy"
-    os.environ["WANDB_MODE"] = "offline"
+    os.environ["WANDB_MODE"] = "disabled"
     os.environ["WANDB_DIR"] = str(tmp_path)
 
     # mock CDD credentials
