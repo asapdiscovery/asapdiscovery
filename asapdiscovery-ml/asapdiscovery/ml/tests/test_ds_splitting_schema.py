64e99b0abe8e8295bf7aaef2adbf0de5a6ff3b98
kaminow <benjamin.kaminow@choderalab.org>
2024-10-01 16:56:16 -0400

    Add test for graph dataset.

--- asapdiscovery-ml/asapdiscovery/ml/tests/test_ds_splitting_schema.py
+++ asapdiscovery-ml/asapdiscovery/ml/tests/test_ds_splitting_schema.py
@@ -57,3 +57,26 @@ def test_manual_split_docked_dataset(complex_pdb):
     assert len(ds_train) == 1
     assert len(ds_val) == 1
     assert len(ds_test) == 1
+
+
+def test_manual_split_graph_dataset(ligand_sdf):
+    lig1 = Ligand.from_sdf(ligand_sdf, compound_name="test1")
+    lig2 = Ligand.from_sdf(ligand_sdf, compound_name="test2")
+    lig3 = Ligand.from_sdf(ligand_sdf, compound_name="test3")
+
+    ds = DatasetConfig(ds_type=DatasetType.graph, input_data=[lig1, lig2, lig3]).build()
+
+    ds_splitter = DatasetSplitterConfig(
+        split_type="manual",
+        split_dict={
+            "train": [("NA", "test1")],
+            "val": [("NA", "test2")],
+            "test": [("NA", "test3")],
+        },
+    )
+
+    ds_train, ds_val, ds_test = ds_splitter.split(ds)
+
+    assert len(ds_train) == 1
+    assert len(ds_val) == 1
+    assert len(ds_test) == 1
