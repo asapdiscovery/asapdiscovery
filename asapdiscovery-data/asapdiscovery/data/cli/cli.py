5973fce43473414ed7ae6e11ba45a56a29a8b354
kaminow <51923685+kaminow@users.noreply.github.com>
2024-10-15 17:59:06 -0400

    Add CDD download to CLI (#1251)
    * Add empty function to CLI.
    
    * Add CLI args.
    
    * Add code for downloading.
    
    * Logging for Cheng-Prusoff values.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- asapdiscovery-data/asapdiscovery/data/cli/cli.py
+++ asapdiscovery-data/asapdiscovery/data/cli/cli.py
@@ -1,8 +1,11 @@
 import copy
+import logging
+import os
 from pathlib import Path
 from typing import Optional
 
 import click
+from asapdiscovery.data.services.cdd.cdd_download import download_molecules
 from asapdiscovery.data.services.fragalysis.fragalysis_download import (  # noqa: E402
     API_CALL_BASE_LEGACY,
     BASE_URL_LEGACY,
@@ -48,6 +51,174 @@ def download_fragalysis(
     api_call["target_name"] = fragalysis_target
 
     download(output, api_call, extract=extract, base_url=BASE_URL_LEGACY)
+
+
+@data.command()
+@click.option(
+    "-o",
+    "--output",
+    required=True,
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    help="Output CSV file.",
+)
+@click.option(
+    "-tok",
+    "--token",
+    help=(
+        "File containing CDD token. Not used if the CDDTOKEN "
+        "environment variable is set."
+    ),
+)
+@click.option(
+    "--cache",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, path_type=Path),
+    help="Cache CSV file.",
+)
+@click.option(
+    "-v",
+    "--vault",
+    help="Which CDD vault to download from (defaults to Moonshot vault).",
+)
+@click.option(
+    "-s",
+    "--search",
+    default="sars_fluorescence_noncovalent_w_dates",
+    help=(
+        "Either a search id or entry in MOONSHOT_SEARCH_DICT "
+        "(see asapdiscovery.data.cdd for more details). Defaults to search "
+        "with all noncovalent molecules in the SARS-CoV-2 dose response assay."
+    ),
+)
+@click.option(
+    "-smi",
+    "--smiles-fieldname",
+    default="suspected_SMILES",
+    help="Which column in the downloaded CSV file to use as SMILES.",
+)
+@click.option(
+    "-id",
+    "--id-fieldname",
+    default="Canonical PostEra ID",
+    help="Which column in the downloaded CSV file to use as the molecule id.",
+)
+@click.option("--retain-achiral", is_flag=True, help="Keep achiral molecules.")
+@click.option("--retain-racemic", is_flag=True, help="Keep racemic molecules.")
+@click.option(
+    "--retain-enantiopure", is_flag=True, help="Keep chirally resolved molecules."
+)
+@click.option(
+    "--retain-semiquant",
+    is_flag=True,
+    help="Keep molecules whose IC50 values are out of range.",
+)
+@click.option(
+    "--retain-all",
+    is_flag=True,
+    help=(
+        "Automatically sets retain_achiral, retain_racemic, retain_enantiopure, "
+        "and retain_semiquant."
+    ),
+)
+@click.option(
+    "-an",
+    "--assay-name",
+    default="ProteaseAssay_Fluorescence_Dose-Response_Weizmann",
+    help="Assay name to parse as IC50.",
+)
+@click.option(
+    "-T",
+    "--temp",
+    type=float,
+    default=298.0,
+    help="Temperature in K to use for delta G conversion.",
+)
+@click.option(
+    "-cp",
+    "--cheng-prusoff",
+    default="0.375,9.5",
+    help=(
+        "Comma separated values for [S] and Km to use in the Cheng-Prusoff equation "
+        "(assumed to be in the same units). Default values are those used in the "
+        "SARS-CoV-2 fluorescence experiments from the COVID Moonshot project (in uM "
+        "here). Pass 0 for both values to disable and use the pIC50 approximation."
+    ),
+)
+def download_cdd(
+    output: Path,
+    token: str | None = None,
+    cache: Path | None = None,
+    vault: str | None = None,
+    search: str = "sars_fluorescence_noncovalent_w_dates",
+    smiles_fieldname: str = "suspected_SMILES",
+    id_fieldname: str = "Canonical PostEra ID",
+    retain_achiral: bool = False,
+    retain_racemic: bool = False,
+    retain_enantiopure: bool = False,
+    retain_semiquant: bool = False,
+    retain_all: bool = False,
+    assay_name: str = "ProteaseAssay_Fluorescence_Dose-Response_Weizmann",
+    temp: float = 298.0,
+    cheng_prusoff: str = "0.375,9.5",
+):
+    # Check retain-all shortcut
+    if retain_all:
+        retain_achiral = True
+        retain_racemic = True
+        retain_enantiopure = True
+        retain_semiquant = True
+
+    # Parse Cheng-Prusoff args
+    cheng_prusoff = list(map(float, cheng_prusoff.split(",")))
+    if (len(cheng_prusoff) != 2) or (cheng_prusoff == [0, 0]):
+        print(
+            "No Cheng-Prusoff parameters passed, using pIC50=pKi approximation.",
+            flush=True,
+        )
+        cheng_prusoff = None
+    else:
+        print(
+            f"Using Cheng-Prusoff values of [S]={cheng_prusoff[0]} and",
+            f"Km={cheng_prusoff[1]}",
+            flush=True,
+        )
+
+    # Set up logging
+    logging.basicConfig(level=logging.DEBUG)
+
+    # Set up CDD token
+    if "CDDTOKEN" in os.environ:
+        header = {"X-CDD-token": os.environ["CDDTOKEN"]}
+    elif token and token.exists():
+        header = {"X-CDD-token": "".join(token.open().readlines()).strip()}
+    else:
+        raise ValueError(
+            "Must pass a file for --token if the CDDTOKEN environment "
+            "variable is not set."
+        )
+
+    # Get vault number from environment if not given
+    if not vault:
+        try:
+            vault = os.environ["MOONSHOT_CDD_VAULT_NUMBER"]
+        except KeyError:
+            raise ValueError("No value specified for vault.")
+
+    _ = download_molecules(
+        header,
+        vault=vault,
+        search=search,
+        fn_out=output,
+        fn_cache=cache,
+        id_fieldname=id_fieldname,
+        smiles_fieldname=smiles_fieldname,
+        retain_achiral=retain_achiral,
+        retain_racemic=retain_racemic,
+        retain_enantiopure=retain_enantiopure,
+        retain_semiquantitative_data=retain_semiquant,
+        assay_name=assay_name,
+        dG_T=temp,
+        cp_values=cheng_prusoff,
+    )
 
 
 @data.command(name="cdd-to-schema")
